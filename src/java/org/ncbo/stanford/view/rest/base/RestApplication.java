7df99c3cd80b996421ef7fd3bcef0754e861e7d0
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-10-31 00:45:36 +0000

    removed hard-wiring of Spring configuration files

--- src/java/org/ncbo/stanford/view/rest/base/RestApplication.java
+++ src/java/org/ncbo/stanford/view/rest/base/RestApplication.java
@@ -1,14 +1,17 @@
 package org.ncbo.stanford.view.rest.base;
 
+import javax.servlet.ServletContext;
+
 import org.restlet.Application;
 import org.restlet.Context;
 import org.restlet.Restlet;
 import org.restlet.Router;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
-import org.restlet.ext.spring.SpringContext;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.noelios.restlet.ext.servlet.ServletContextAdapter;
 
 /**
  * @author Michael Dorf
@@ -22,18 +25,13 @@ public class RestApplication extends Application {
 	public Restlet createRoot() {
 		Router router = new Router(getContext());
 
-		SpringContext springContext = new SpringContext(getContext());
-		XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(springContext);
-        
-		ClassPathResource[] resources = {
-				new ClassPathResource("applicationContext-datasources.xml"),
-				new ClassPathResource("applicationContext-services.xml"),
-				new ClassPathResource("applicationContext-rest.xml"), 
-				new ClassPathResource("applicationContext-security.xml") };
-		xmlReader.loadBeanDefinitions(resources);		
-		springContext.refresh();
-		
-		RestManager manager = (RestManager)springContext.getBean("restManager");
+		ServletContextAdapter adapter = (ServletContextAdapter) getContext();
+		ServletContext servletContext = adapter.getServletContext();
+		WebApplicationContext springApplicationContext = WebApplicationContextUtils
+				.getWebApplicationContext(servletContext);
+
+		RestManager manager = (RestManager) springApplicationContext
+				.getBean("restManager");
 		manager.init(router);
 
 		return router;
@@ -47,5 +45,4 @@ public class RestApplication extends Application {
 		}
 		super.handle(request, response);
 	}
-
 }
