07f5fb2927d2fca3501c11c75ee76e07231b21be
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-10-31 00:44:07 +0000

    class to store application constants

---
+++ src/java/org/ncbo/stanford/util/constants/ApplicationConstants.java
@@ -0,0 +1,17 @@
+package org.ncbo.stanford.util.constants;
+
+import org.ncbo.stanford.util.MessageUtils;
+
+public interface ApplicationConstants {
+
+	/**
+	 * Standard "NO VALUE" constant
+	 */
+	public static final int NO_VAL = -1;
+
+	/**
+	 * Default encoding
+	 */
+	public static final String DEFAULT_ENCODING = MessageUtils
+			.getMessage("default.encoding");
+}
