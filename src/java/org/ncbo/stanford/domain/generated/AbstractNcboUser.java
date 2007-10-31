4f0784bb38e8fb3109f47832c3e66fd702c733b6
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-10-31 01:39:54 +0000

    hibernate generated classes

---
+++ src/java/org/ncbo/stanford/domain/generated/AbstractNcboUser.java
@@ -0,0 +1,143 @@
+package org.ncbo.stanford.domain.generated;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * AbstractNcboUser entity provides the base persistence definition of the
+ * NcboUser entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+
+public abstract class AbstractNcboUser implements java.io.Serializable {
+
+	// Fields
+
+	private Integer id;
+	private String username;
+	private String password;
+	private String email;
+	private String firstname;
+	private String lastname;
+	private String phone;
+	private Date dateCreated;
+	private Set ncboOntologies = new HashSet(0);
+	private Set ncboUserRoles = new HashSet(0);
+
+	// Constructors
+
+	/** default constructor */
+	public AbstractNcboUser() {
+	}
+
+	/** minimal constructor */
+	public AbstractNcboUser(String username, String password, String email,
+			String firstname, String lastname, Date dateCreated) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.dateCreated = dateCreated;
+	}
+
+	/** full constructor */
+	public AbstractNcboUser(String username, String password, String email,
+			String firstname, String lastname, String phone, Date dateCreated,
+			Set ncboOntologies, Set ncboUserRoles) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.phone = phone;
+		this.dateCreated = dateCreated;
+		this.ncboOntologies = ncboOntologies;
+		this.ncboUserRoles = ncboUserRoles;
+	}
+
+	// Property accessors
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstname() {
+		return this.firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return this.lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getPhone() {
+		return this.phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getDateCreated() {
+		return this.dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Set getNcboOntologies() {
+		return this.ncboOntologies;
+	}
+
+	public void setNcboOntologies(Set ncboOntologies) {
+		this.ncboOntologies = ncboOntologies;
+	}
+
+	public Set getNcboUserRoles() {
+		return this.ncboUserRoles;
+	}
+
+	public void setNcboUserRoles(Set ncboUserRoles) {
+		this.ncboUserRoles = ncboUserRoles;
+	}
+
+}
