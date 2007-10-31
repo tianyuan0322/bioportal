83866c8e03418873453e87225b8b51867c1ff683
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-10-31 00:43:52 +0000

    utility for request manipulation

---
+++ src/java/org/ncbo/stanford/util/RequestUtils.java
@@ -0,0 +1,71 @@
+package org.ncbo.stanford.util;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ncbo.stanford.util.helper.StringHelper;
+
+
+public class RequestUtils {
+
+	/**
+	 * Sends a redirect call to a specified URL
+	 * 
+	 * @param request
+	 * @param response
+	 * @param url
+	 * @throws IOException
+	 */
+	public static void sendRedirect(HttpServletRequest request, 
+			HttpServletResponse response, String url) throws IOException {
+		if (!url.startsWith("http://") && !url.startsWith("https://")) {
+			url = request.getContextPath() + url;
+		}
+	
+		response.sendRedirect(response.encodeRedirectURL(url));
+	}
+
+	/**
+	 * Parses a query string and puts parameters
+	 * in the HashMap of <Key, Value> pairs
+	 * 
+	 * @param queryString
+	 * @return
+	 */
+	public static HashMap<String, String> parseQueryString(
+			String queryString) {
+		HashMap<String, String> parsed = new HashMap<String, String>(1);		
+		StringTokenizer	stAmpersand = new StringTokenizer(queryString, "&");
+		
+		while (stAmpersand.hasMoreTokens()) {
+			String anItem = stAmpersand.nextToken();
+	
+			if (anItem != null) {
+				// iterate for the "=" in the Name=Value pair
+				StringTokenizer stEqual = new StringTokenizer(anItem, "=");
+	
+				if (stEqual.hasMoreTokens()) {
+					String aName = stEqual.nextToken();
+	
+					if (!StringHelper.isNullOrNullString(aName)
+							&& stEqual.hasMoreTokens()) {
+						// add the variable to orderElements vector
+						String aValue = null;
+	
+						if (stEqual.hasMoreTokens()) {
+							aValue = stEqual.nextToken();
+						}
+						
+						parsed.put(aName, aValue);
+					}
+				}
+			}
+		}
+		
+		return parsed;
+	}
+}
