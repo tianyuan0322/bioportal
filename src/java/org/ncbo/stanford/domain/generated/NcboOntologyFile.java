932da4ced7175ffdc71bdde39e6395d55813a118
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-11-02 01:10:44 +0000

    regenerated according to the updated database structure

---
+++ src/java/org/ncbo/stanford/domain/generated/NcboOntologyFile.java
@@ -0,0 +1,22 @@
+package org.ncbo.stanford.domain.generated;
+
+/**
+ * NcboOntologyFile entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+public class NcboOntologyFile extends AbstractNcboOntologyFile implements
+		java.io.Serializable {
+
+	// Constructors
+
+	/** default constructor */
+	public NcboOntologyFile() {
+	}
+
+	/** full constructor */
+	public NcboOntologyFile(NcboOntology ncboOntology, String filename) {
+		super(ncboOntology, filename);
+	}
+
+}
---
+++ src/java/org/ncbo/stanford/domain/generated/NcboOntologyMetadata.java
@@ -0,0 +1,32 @@
+package org.ncbo.stanford.domain.generated;
+
+/**
+ * NcboOntologyMetadata entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+public class NcboOntologyMetadata extends AbstractNcboOntologyMetadata
+		implements java.io.Serializable {
+
+	// Constructors
+
+	/** default constructor */
+	public NcboOntologyMetadata() {
+	}
+
+	/** minimal constructor */
+	public NcboOntologyMetadata(NcboOntology ncboOntology, String displayLabel,
+			String format, Byte isFoundry) {
+		super(ncboOntology, displayLabel, format, isFoundry);
+	}
+
+	/** full constructor */
+	public NcboOntologyMetadata(NcboOntology ncboOntology, String displayLabel,
+			String format, String contactName, String contactEmail,
+			String homepage, String documentation, String publication,
+			String urn, Byte isFoundry) {
+		super(ncboOntology, displayLabel, format, contactName, contactEmail,
+				homepage, documentation, publication, urn, isFoundry);
+	}
+
+}
