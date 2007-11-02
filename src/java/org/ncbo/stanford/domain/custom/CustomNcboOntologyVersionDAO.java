f5b9b49056f3b43d905ace0858a678fee9445a81
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-11-02 21:02:15 +0000

    renamed ncbo_ontology table into ncbo_ontology_version

---
+++ src/java/org/ncbo/stanford/domain/custom/CustomNcboOntologyVersionDAO.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.ncbo.stanford.domain.custom;
+
+import org.ncbo.stanford.domain.generated.NcboOntologyVersionDAO;
+
+/**
+ * @author mdorf
+ *
+ */
+public class CustomNcboOntologyVersionDAO extends NcboOntologyVersionDAO {
+
+	/**
+	 * 
+	 */
+	public CustomNcboOntologyVersionDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
