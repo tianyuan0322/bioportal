349583ed03d3585ae83edede42107c6f2799ced8
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-11-02 01:11:25 +0000

    new table ncbo_ontology_file

---
+++ src/java/org/ncbo/stanford/domain/custom/CustomNcboOntologyFileDAO.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.ncbo.stanford.domain.custom;
+
+import org.ncbo.stanford.domain.generated.NcboOntologyFileDAO;
+
+/**
+ * @author mdorf
+ *
+ */
+public class CustomNcboOntologyFileDAO extends NcboOntologyFileDAO {
+
+	/**
+	 * 
+	 */
+	public CustomNcboOntologyFileDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
