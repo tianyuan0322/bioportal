e57f2ed292e39e768661565dc29d741a2fb3d9db
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-11-16 01:03:02 +0000

    file to store request constants

---
+++ src/java/org/ncbo/stanford/view/util/constants/RequestParamConstants.java
@@ -0,0 +1,27 @@
+package org.ncbo.stanford.view.util.constants;
+
+/**
+ * Request parameter constants
+ * 
+ * @author Michael Dorf
+ */
+public interface RequestParamConstants {
+
+	public static final String PARAM_ERROR = "error";
+
+	public static final String PARAM_USERNAME = "username";
+	public static final String PARAM_PASSWORD = "password";
+	public static final String PARAM_APPLICATIONID = "applicationid";
+	public static final String PARAM_SESSIONID = "sessionid";
+
+	/**
+	 * A mandatory parmeter required to be included in all encrypted
+	 * URL query strings
+	 */
+	public static final String PARAM_ENCRYPTED_MANDATORY = "owmvurhsllhheeddtgkxx";
+	
+	/**
+	 * The constant for value "true" appended to query string
+	 */
+	public static final String PARAM_VALUE_TRUE = "true";
+}
