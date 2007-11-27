edaaca74d22575f251d717d144397d7ebc046556
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-11-27 01:44:53 +0000

    implemented custom Acegi classes required for additional, application based, authentication

--- src/java/org/ncbo/stanford/util/security/ui/ApplicationAuthenticationDetails.java
+++ src/java/org/ncbo/stanford/util/security/ui/ApplicationAuthenticationDetails.java
@@ -4,37 +4,45 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.acegisecurity.ui.WebAuthenticationDetails;
 
+/**
+ * Extends Acegi's WebAuthenticationDetails class to allow storing of
+ * application id
+ * 
+ * @author Michael Dorf
+ * 
+ */
 public class ApplicationAuthenticationDetails extends WebAuthenticationDetails {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private String applicationId;
 
 	/**
 	 * @param request
 	 * @param applicationId
 	 */
-	public ApplicationAuthenticationDetails(HttpServletRequest request, String applicationId) {
+	public ApplicationAuthenticationDetails(HttpServletRequest request,
+			String applicationId) {
 		super(request);
 		this.applicationId = applicationId;
 	}
-	
-    protected ApplicationAuthenticationDetails() {
-        throw new IllegalArgumentException("Cannot use default constructor");
-    }
 
-    /**
+	protected ApplicationAuthenticationDetails() {
+		throw new IllegalArgumentException("Cannot use default constructor");
+	}
+
+	/**
 	 * @return the applicationId
 	 */
 	public String getApplicationId() {
 		return applicationId;
 	}
-	
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-        sb.append(super.toString() + "; ");
-        sb.append("ApplicationId: " + this.getApplicationId());
 
-        return sb.toString();
-    }	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(super.toString() + "; ");
+		sb.append("ApplicationId: " + this.getApplicationId());
+
+		return sb.toString();
+	}
 }
