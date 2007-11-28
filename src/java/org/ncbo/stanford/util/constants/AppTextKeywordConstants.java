859859f6890fe56487f78ab61efa0c351d48c3e6
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-11-28 01:08:38 +0000

    added TextManager related constants

---
+++ src/java/org/ncbo/stanford/util/constants/AppTextKeywordConstants.java
@@ -0,0 +1,11 @@
+package org.ncbo.stanford.util.constants;
+
+/**
+ * Constant file to store TextManager keywords
+ * 
+ * @author Michael Dorf
+ *
+ */
+public interface AppTextKeywordConstants {
+
+}
--- src/java/org/ncbo/stanford/util/constants/ApplicationConstants.java
+++ src/java/org/ncbo/stanford/util/constants/ApplicationConstants.java
@@ -18,4 +18,22 @@ public interface ApplicationConstants {
 	 */
 	public static final Byte TRUE = 1;
 	public static final Byte FALSE = 0;
+	
+	/**
+	 * TextManager constants
+	 */ 
+	// TAG Constants
+	public static final String KEYWORD = "KEYWORD";
+	public static final String IMPORT = "IMPORT";
+	public static final String SETKEYWORD = "SETKEYWORD";
+
+	public static final String TEXT_TAG_SUFFIX = "Tag";
+	public static final String TEXT_TAG_PREFIX = "Text";
+	public static final String TM_TAG_JAVA_PACKAGE = "org.ncbo.stanford.util.textmanager.tagprocessor.tag";
+	public static final String TEXT_NOT_FOUND = "###_TEXT_NOT_FOUND_###";
+	public static final String[] ALL_TAGS = {KEYWORD, IMPORT, SETKEYWORD};
+	
+	// Content retrieval constants
+	public static final int FROM_DB = 1200;
+	public static final int FROM_CACHE = 1201;
 }
