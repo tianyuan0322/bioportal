2625ff0c489314314d25d7b16b7826ffc93ea57e
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-11-30 02:11:43 +0000

    xml serialization service

---
+++ src/java/org/ncbo/stanford/service/xml/impl/XMLSerializationServiceImpl.java
@@ -0,0 +1,15 @@
+package org.ncbo.stanford.service.xml.impl;
+
+import org.ncbo.stanford.service.xml.XMLSerializationService;
+
+import com.thoughtworks.xstream.XStream;
+
+public class XMLSerializationServiceImpl implements XMLSerializationService {
+	
+//	private XStream xstream;
+//	
+//	
+//	public XMLSerializationServiceImpl {
+//		xstream = new XStream
+//	}
+}
