a68b05e894e3e71c47321725b11da5b969cf3df3
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-12-01 02:24:59 +0000

    added error types

--- src/java/org/ncbo/stanford/enumeration/ErrorType.java
+++ src/java/org/ncbo/stanford/enumeration/ErrorType.java
@@ -1,7 +1,7 @@
 package org.ncbo.stanford.enumeration;
 
 /**
- * Enum to store site-wide errors with codes and default messages
+ * An Enum to store site-wide errors with codes and default messages
  * 
  * @author Michael Dorf
  *
@@ -11,9 +11,15 @@ public enum ErrorType {
 	AUTHENTICATION_REQUIRED(
 			"AUTHENTICATION_REQUIRED", 
 			"Access to this resource requires authentication"),
+	INVALID_CREDENTIALS(
+			"INVALID_CREDENTIALS", 
+			"The credentials you supplied are not valid"),
 	ACCESS_DENIED(
 			"ACCESS_DENIED", 
-			"You do not possess sufficient privileges to access this resource");
+			"You do not possess sufficient privileges to access this resource"),
+	RUNTIME_ERROR(
+			"RUNTIME_ERROR", 
+			"A runtime error has occurred");
 	
     private final String errorCode;
     private final String defErrorMessage;
