fa3c5efc23c496dcf3db764d65480e091d2b05a3
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-12-06 20:07:42 +0000

    added generateNewOntologyId method

--- src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboSeqOntologyIdDAO.java
+++ src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboSeqOntologyIdDAO.java
@@ -17,4 +17,19 @@ public class CustomNcboSeqOntologyIdDAO extends NcboSeqOntologyIdDAO {
 	public CustomNcboSeqOntologyIdDAO() {
 		super();
 	}
+	
+	/**
+	 * Generates a new ontology id (the same for all versions of a unique ontology)
+	 * 
+	 * @return new ontology id
+	 */
+	public Integer generateNewOntologyId() {		
+		int updatedEntities = getSession().createSQLQuery("update ncbo_seq_ontology_id set id = last_insert_id(id + 1)").executeUpdate();
+
+		if (updatedEntities > 0) {
+			return (Integer) getSession().createSQLQuery("select last_insert_id()").uniqueResult();
+		}
+		
+		return null;
+	}
 }
