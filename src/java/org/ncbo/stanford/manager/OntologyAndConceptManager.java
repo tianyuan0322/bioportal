1a72b1035d1e06b6bff9b069c1a8b6de53a1569d
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-12-07 01:11:33 +0000

    OntologyAndConceptManager, an abstraction layer for Ontology and Concept backends

---
+++ src/java/org/ncbo/stanford/manager/OntologyAndConceptManager.java
@@ -0,0 +1,27 @@
+package org.ncbo.stanford.manager;
+
+import java.util.List;
+
+import org.ncbo.stanford.bean.OntologyBean;
+
+/**
+ * An interface designed to provide an abstraction layer to ontology and concept
+ * operations. The service layer will consume this interface instead of directly
+ * calling a specific implementation (i.e. LexGrid, Protege etc.). This
+ * interface should be used by internal services and should not be exposed to
+ * upper layers.
+ * 
+ * @author Michael Dorf
+ * 
+ */
+public interface OntologyAndConceptManager {
+
+	/**
+	 * Returns a single record for each ontology in the system. If more than one
+	 * version of ontology exists, return the latest version.
+	 * 
+	 * @return
+	 */
+	public List<OntologyBean> findLatestOntologyVersions();
+
+}
---
+++ src/java/org/ncbo/stanford/manager/impl/OntologyAndConceptManagerImpl.java
@@ -0,0 +1,60 @@
+package org.ncbo.stanford.manager.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ncbo.stanford.bean.OntologyBean;
+import org.ncbo.stanford.domain.custom.dao.CustomNcboOntologyVersionDAO;
+import org.ncbo.stanford.domain.custom.entity.NcboOntology;
+import org.ncbo.stanford.manager.OntologyAndConceptManager;
+
+/**
+ * A default implementation to OntologyAndConceptManager interface designed to
+ * provide an abstraction layer to ontology and concept operations. The service
+ * layer will consume this interface instead of directly calling a specific
+ * implementation (i.e. LexGrid, Protege etc.). Do not use this class directly
+ * in upper layers.
+ * 
+ * @author Michael Dorf
+ * 
+ */
+public class OntologyAndConceptManagerImpl implements OntologyAndConceptManager {
+
+	private CustomNcboOntologyVersionDAO ncboOntologyVersionDAO;
+
+	/**
+	 * Returns a single record for each ontology in the system. If more than one
+	 * version of ontology exists, return the latest version.
+	 * 
+	 * @return
+	 */
+	public List<OntologyBean> findLatestOntologyVersions() {
+		ArrayList<OntologyBean> ontBeanList = new ArrayList<OntologyBean>(1);
+		List<NcboOntology> ontEntityList = ncboOntologyVersionDAO
+				.findLatestOntologyVersions();
+
+		for (NcboOntology ncboOntology : ontEntityList) {
+			OntologyBean ontologyBean = new OntologyBean();
+			ontologyBean.populateFromEntity(ncboOntology);
+			ontBeanList.add(ontologyBean);
+		}
+
+		return ontBeanList;
+	}
+
+	/**
+	 * @return the ncboOntologyVersionDAO
+	 */
+	public CustomNcboOntologyVersionDAO getNcboOntologyVersionDAO() {
+		return ncboOntologyVersionDAO;
+	}
+
+	/**
+	 * @param ncboOntologyVersionDAO
+	 *            the ncboOntologyVersionDAO to set
+	 */
+	public void setNcboOntologyVersionDAO(
+			CustomNcboOntologyVersionDAO ncboOntologyVersionDAO) {
+		this.ncboOntologyVersionDAO = ncboOntologyVersionDAO;
+	}
+}
