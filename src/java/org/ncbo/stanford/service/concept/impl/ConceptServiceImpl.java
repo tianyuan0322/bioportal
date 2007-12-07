b52ff48ae81f4e80d091d3c685eb0200b2ad2907
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-12-07 23:23:37 +0000

    formatting

--- src/java/org/ncbo/stanford/service/concept/impl/ConceptServiceImpl.java
+++ src/java/org/ncbo/stanford/service/concept/impl/ConceptServiceImpl.java
@@ -9,58 +9,59 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ncbo.stanford.bean.ConceptBean;
 import org.ncbo.stanford.service.concept.ConceptService;
-import org.ncbo.stanford.service.ontology.impl.OntologyServiceImpl;
 
 /**
  * @author Nick Griffith
- *
+ * 
  */
-public class ConceptServiceImpl implements ConceptService{
-	
-	private static final Log log = LogFactory.getLog(OntologyServiceImpl.class);
-	
-	
-    public ConceptBean findConcept(String id, int ontologyId){
-    	return new ConceptBean();
-    }
-    
-	public ArrayList<ConceptBean> findPathToRoot(String id, int ontologyId){
-		return new ArrayList();
-	}
-	
-	public ConceptBean findParent(String id, int ontologyId){
+public class ConceptServiceImpl implements ConceptService {
+
+	private static final Log log = LogFactory.getLog(ConceptServiceImpl.class);
+
+	public ConceptBean findConcept(String id, int ontologyId) {
 		return new ConceptBean();
 	}
-	
-	public ArrayList<ConceptBean> findChildren(String id, int ontologyId){
+
+	public ArrayList<ConceptBean> findPathToRoot(String id, int ontologyId) {
 		return new ArrayList();
 	}
-	
-	public ArrayList<ConceptBean> findConceptNameExact(String query, ArrayList<Integer> ontologyIds){
+
+	public ConceptBean findParent(String id, int ontologyId) {
+		return new ConceptBean();
+	}
+
+	public ArrayList<ConceptBean> findChildren(String id, int ontologyId) {
 		return new ArrayList();
 	}
-	
-	public ArrayList<ConceptBean> findConceptNameStartsWith(String query, ArrayList<Integer> ontologyIds){
+
+	public ArrayList<ConceptBean> findConceptNameExact(String query,
+			ArrayList<Integer> ontologyIds) {
 		return new ArrayList();
 	}
-	
-	public ArrayList<ConceptBean> findConceptNameContains(String query, ArrayList<Integer> ontologyIds){
+
+	public ArrayList<ConceptBean> findConceptNameStartsWith(String query,
+			ArrayList<Integer> ontologyIds) {
 		return new ArrayList();
 	}
-	
-	public ArrayList<ConceptBean> findConceptPropertyExact(String property, String query, ArrayList<Integer> ontologyIds){
+
+	public ArrayList<ConceptBean> findConceptNameContains(String query,
+			ArrayList<Integer> ontologyIds) {
 		return new ArrayList();
 	}
-	
-	public ArrayList<ConceptBean> findConceptPropertyStartsWith(String property, String query, ArrayList<Integer> ontologyIds){
+
+	public ArrayList<ConceptBean> findConceptPropertyExact(String property,
+			String query, ArrayList<Integer> ontologyIds) {
 		return new ArrayList();
 	}
-	
-	public ArrayList<ConceptBean> findConceptPropertyContains(String property, String query, ArrayList<Integer> ontologyIds){
+
+	public ArrayList<ConceptBean> findConceptPropertyStartsWith(
+			String property, String query, ArrayList<Integer> ontologyIds) {
 		return new ArrayList();
 	}
-	
+
+	public ArrayList<ConceptBean> findConceptPropertyContains(String property,
+			String query, ArrayList<Integer> ontologyIds) {
+		return new ArrayList();
+	}
+
 }
-	
-
-
