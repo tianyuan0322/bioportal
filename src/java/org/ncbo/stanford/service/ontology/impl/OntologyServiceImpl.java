26ff5fa94471b9f4f2cc07762d46041fb5b29653
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-12-07 01:12:30 +0000

    moved DAO injections to OntologyAndConceptManager

--- src/java/org/ncbo/stanford/service/ontology/impl/OntologyServiceImpl.java
+++ src/java/org/ncbo/stanford/service/ontology/impl/OntologyServiceImpl.java
@@ -6,59 +6,53 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ncbo.stanford.bean.OntologyBean;
-import org.ncbo.stanford.domain.custom.dao.CustomNcboOntologyVersionDAO;
-import org.ncbo.stanford.domain.custom.entity.NcboOntology;
+import org.ncbo.stanford.manager.OntologyAndConceptManager;
 import org.ncbo.stanford.service.ontology.OntologyService;
 
 public class OntologyServiceImpl implements OntologyService {
 
 	private static final Log log = LogFactory.getLog(OntologyServiceImpl.class);
 
-	private CustomNcboOntologyVersionDAO ncboOntologyVersionDAO;
-	
-	public OntologyBean findOntology(int id,String version){
+	private OntologyAndConceptManager ontologyAndConceptManager;
+
+	/**
+	 * Returns a single record for each ontology in the system. If more than one
+	 * version of ontology exists, return the latest version.
+	 * 
+	 * @return list of ontology beans
+	 */
+	public List<OntologyBean> findLatestOntologyVersions() {
+		return ontologyAndConceptManager.findLatestOntologyVersions();
+	}
+
+	public OntologyBean findOntology(int id, String version) {
 		return new OntologyBean();
 	}
-	
-	public List<OntologyBean> findOntologyVersions(int id){
+
+	public List<OntologyBean> findOntologyVersions(int id) {
 		return new ArrayList();
 	}
-	
-	public List<String> findProperties(int id){
+
+	public List<String> findProperties(int id) {
 		return new ArrayList();
 	}
-	
-	public void createOntology(OntologyBean ontology){
-		
-	}
-	
-	public List<OntologyBean> findLatestOntologyVersions() {
-		ArrayList<OntologyBean> ontBeanList = new ArrayList<OntologyBean>(1);
-		List<NcboOntology> ontEntityList = ncboOntologyVersionDAO
-				.findLatestOntologyVersions();
 
-		for (NcboOntology ncboOntology : ontEntityList) {
-			OntologyBean ontologyBean = new OntologyBean();
-			ontologyBean.populateFromEntity(ncboOntology);
-			ontBeanList.add(ontologyBean);
-		}
-
-		return ontBeanList;
+	public void createOntology(OntologyBean ontology) {
 	}
 
 	/**
-	 * @return the ncboOntologyVersionDAO
+	 * @return the ontologyAndConceptManager
 	 */
-	public CustomNcboOntologyVersionDAO getNcboOntologyVersionDAO() {
-		return ncboOntologyVersionDAO;
+	public OntologyAndConceptManager getOntologyAndConceptManager() {
+		return ontologyAndConceptManager;
 	}
 
 	/**
-	 * @param ncboOntologyVersionDAO
-	 *            the ncboOntologyVersionDAO to set
+	 * @param ontologyAndConceptManager
+	 *            the ontologyAndConceptManager to set
 	 */
-	public void setNcboOntologyVersionDAO(
-			CustomNcboOntologyVersionDAO ncboOntologyVersionDAO) {
-		this.ncboOntologyVersionDAO = ncboOntologyVersionDAO;
+	public void setOntologyAndConceptManager(
+			OntologyAndConceptManager ontologyAndConceptManager) {
+		this.ontologyAndConceptManager = ontologyAndConceptManager;
 	}
 }
