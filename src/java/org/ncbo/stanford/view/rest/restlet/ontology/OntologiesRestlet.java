0ebf076c347816a58860a4e2d8f4270661b7b92d
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-12-07 23:33:58 +0000

    added setters for injected properties

--- src/java/org/ncbo/stanford/view/rest/restlet/ontology/OntologiesRestlet.java
+++ src/java/org/ncbo/stanford/view/rest/restlet/ontology/OntologiesRestlet.java
@@ -6,18 +6,14 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ncbo.stanford.bean.OntologyBean;
-import org.ncbo.stanford.bean.UserBean;
 import org.ncbo.stanford.enumeration.ErrorTypeEnum;
 import org.ncbo.stanford.service.ontology.OntologyService;
 import org.ncbo.stanford.service.upload.UploadService;
 import org.ncbo.stanford.service.xml.XMLSerializationService;
 import org.ncbo.stanford.util.RequestUtils;
-import org.ncbo.stanford.util.constants.ApplicationConstants;
-import org.ncbo.stanford.view.util.constants.RequestParamConstants;
 import org.restlet.Restlet;
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
-import org.restlet.data.Reference;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
@@ -29,66 +25,63 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 public class OntologiesRestlet extends Restlet {
 
 	private static final Log log = LogFactory.getLog(OntologiesRestlet.class);
-	
+
 	private UploadService uploadService;
 	private OntologyService ontologyService;
 	private XMLSerializationService xmlSerializationService;
 
 	@Override
 	public void handle(Request request, Response response) {
-		
-		if(request.getMethod().equals(Method.GET)){
+
+		if (request.getMethod().equals(Method.GET)) {
 			getRequest(request, response);
-		}else if(request.getMethod().equals(Method.POST)){
+		} else if (request.getMethod().equals(Method.POST)) {
 			postRequest(request, response);
 		}
-		
+
 	}
-	
-	
+
 	/**
 	 * Handle GET calls here
 	 */
-	private void getRequest(Request request,Response response){
-		listOntologies(response);	
+	private void getRequest(Request request, Response response) {
+		listOntologies(response);
 	}
-	
-	
+
 	/**
 	 * Handle POST calls here
+	 * 
 	 * @param request
 	 * @param response
 	 */
-	private void postRequest(Request request,Response response){		
+	private void postRequest(Request request, Response response) {
 		RestletFileUpload rfu = new RestletFileUpload();
-		try{
-		List files = rfu.parseRequest(request);
+		try {
+			List files = rfu.parseRequest(request);
 		} catch (FileUploadException fue) {
 			RequestUtils.setHttpServletResponse(response,
 					Status.CLIENT_ERROR_BAD_REQUEST, MediaType.TEXT_XML,
 					xmlSerializationService.getErrorAsXML(
-							ErrorTypeEnum.INVALID_FILE,null));
+							ErrorTypeEnum.INVALID_FILE, null));
 			return;
 		}
-		RequestUtils
-		.setHttpServletResponse(response, Status.SUCCESS_OK,
-				MediaType.TEXT_XML, xmlSerializationService
-						.getSuccessAsXML(RequestUtils.getSessionId(request),
-								null));
+		RequestUtils.setHttpServletResponse(response, Status.SUCCESS_OK,
+				MediaType.TEXT_XML, xmlSerializationService.getSuccessAsXML(
+						RequestUtils.getSessionId(request), null));
 	}
-	
+
 	/**
 	 * Return to the response a listing of ontologies
+	 * 
 	 * @param response
 	 */
 	private void listOntologies(Response response) {
-		List<OntologyBean> ontList = ontologyService.findLatestOntologyVersions();
+		List<OntologyBean> ontList = ontologyService
+				.findLatestOntologyVersions();
 		XStream xstream = new XStream(new DomDriver());
 		xstream.alias("OntologyBean", OntologyBean.class);
 		response.setEntity(xstream.toXML(ontList), MediaType.APPLICATION_XML);
 	}
-	
-	
 
 	/**
 	 * @return the ontologyService
@@ -103,5 +96,36 @@ public class OntologiesRestlet extends Restlet {
 	 */
 	public void setOntologyService(OntologyService ontologyService) {
 		this.ontologyService = ontologyService;
+	}
+
+	/**
+	 * @return the uploadService
+	 */
+	public UploadService getUploadService() {
+		return uploadService;
+	}
+
+	/**
+	 * @param uploadService
+	 *            the uploadService to set
+	 */
+	public void setUploadService(UploadService uploadService) {
+		this.uploadService = uploadService;
+	}
+
+	/**
+	 * @return the xmlSerializationService
+	 */
+	public XMLSerializationService getXmlSerializationService() {
+		return xmlSerializationService;
+	}
+
+	/**
+	 * @param xmlSerializationService
+	 *            the xmlSerializationService to set
+	 */
+	public void setXmlSerializationService(
+			XMLSerializationService xmlSerializationService) {
+		this.xmlSerializationService = xmlSerializationService;
 	}
 }
