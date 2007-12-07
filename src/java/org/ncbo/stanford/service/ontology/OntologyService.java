9119ab9cecdf3f540c7768d0a1e861492d8710e4
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2007-12-07 01:12:19 +0000

    moved DAO injections to OntologyAndConceptManager

--- src/java/org/ncbo/stanford/service/ontology/OntologyService.java
+++ src/java/org/ncbo/stanford/service/ontology/OntologyService.java
@@ -4,18 +4,27 @@ import java.util.List;
 
 import org.ncbo.stanford.bean.OntologyBean;
 
+/**
+ * Exposes services that provide read and write operations on ontologies
+ * 
+ * @author Michael Dorf
+ * @author Nick Griffith
+ */
 public interface OntologyService {
-	
-	List<OntologyBean> findLatestOntologyVersions();
-	
-	OntologyBean findOntology(int id,String version);
-	
-	List<OntologyBean> findOntologyVersions(int id);
-	
-	List<String> findProperties(int id);
-	
-	void createOntology(OntologyBean ontology);
+
+	/**
+	 * Returns a single record for each ontology in the system. If more than one
+	 * version of ontology exists, return the latest version.
+	 * 
+	 * @return list of Ontology beans
+	 */
+	public List<OntologyBean> findLatestOntologyVersions();
+
+	public OntologyBean findOntology(int id, String version);
+
+	public List<OntologyBean> findOntologyVersions(int id);
+
+	public List<String> findProperties(int id);
+
+	public void createOntology(OntologyBean ontology);
 }
-
-
-
