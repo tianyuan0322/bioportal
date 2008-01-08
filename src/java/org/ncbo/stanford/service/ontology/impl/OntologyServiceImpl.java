509696f89471ee8487401293b9d75a0867df237e
bdai <bdai@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-01-08 23:31:24 +0000

    Added setter and getter methods for OntologyLoader.

--- src/java/org/ncbo/stanford/service/ontology/impl/OntologyServiceImpl.java
+++ src/java/org/ncbo/stanford/service/ontology/impl/OntologyServiceImpl.java
@@ -8,12 +8,14 @@ import org.apache.commons.logging.LogFactory;
 import org.ncbo.stanford.bean.OntologyBean;
 import org.ncbo.stanford.manager.OntologyAndConceptManager;
 import org.ncbo.stanford.service.ontology.OntologyService;
+import org.ncbo.stanford.manager.OntologyLoader;
 
 public class OntologyServiceImpl implements OntologyService {
 
-	private static final Log log = LogFactory.getLog(OntologyServiceImpl.class);	
+	private static final Log log = LogFactory.getLog(OntologyServiceImpl.class);
 
-	private OntologyAndConceptManager ontologyAndConceptManager;
+	private OntologyAndConceptManager ontologyAndConceptManager = null;
+	private OntologyLoader ontologyLoader = null;
 
 	/**
 	 * Returns a single record for each ontology in the system. If more than one
@@ -24,13 +26,11 @@ public class OntologyServiceImpl implements OntologyService {
 	public List<OntologyBean> findLatestOntologyVersions() {
 		return ontologyAndConceptManager.findLatestOntologyVersions();
 	}
-	
-	
+
 	public OntologyBean findOntology(int id, String version) {
 		return new OntologyBean();
 	}
 
-	
 	public List<OntologyBean> findOntologyVersions(int id) {
 		return new ArrayList();
 	}
@@ -39,7 +39,23 @@ public class OntologyServiceImpl implements OntologyService {
 		return new ArrayList();
 	}
 
+	/**
+	 * Assumption here is that creat ontology loads the specified ontology into
+	 * the BioPortal repository.
+	 */
 	public void createOntology(OntologyBean ontology) {
+		try {
+			ontologyLoader.loadOntology(ontology);
+		} 
+		catch (Exception exc) {
+			// This is where one would set the code to indicate a failed ontology load...
+			
+			// TODO: There needs to be a better mechanism to handle exceptions
+			// during ontology loads (e.g. this method could throw an exception
+			log.error("Unable load specified ontology: "
+					+ ontology.getDisplayLabel());
+			exc.printStackTrace();
+		}
 	}
 
 	/**
@@ -56,5 +72,20 @@ public class OntologyServiceImpl implements OntologyService {
 	public void setOntologyAndConceptManager(
 			OntologyAndConceptManager ontologyAndConceptManager) {
 		this.ontologyAndConceptManager = ontologyAndConceptManager;
+	}
+	
+	/**
+	 * @return the ontologyLoader
+	 */
+	public OntologyLoader getOntologyLoader() {
+		return ontologyLoader;
+	}
+	
+	/**
+	 * @param ontologyLoader
+	 *            the ontologyLoader to set
+	 */
+	public void setOntologyLoader(OntologyLoader ontologyLoader) {
+		this.ontologyLoader = ontologyLoader;
 	}
 }
