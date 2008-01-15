3d8818c249410468473fd241443c70aeaf9f468f
bdai <bdai@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-01-15 01:09:20 +0000

    Helper method for ConceptBean.  Not in use yet.  May remove if no need.

---
+++ src/java/org/ncbo/stanford/util/helper/ConceptBeanHelper.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.ncbo.stanford.util.helper;
+
+/**
+ * Convenience helper methods for ConceptBean. This theoretically should be
+ * included as a behavior for ConceptBean. However beans may be serialized and
+ * shared across layers and remotely. Thus, helpers methods have been
+ * tentatively placed here.  
+ * 
+ * @author Benjamin Dai
+ * 
+ */
+public class ConceptBeanHelper {
+	
+	/**
+	 * Outputs all the ConceptBean and all sub classes and superclasses.
+	 */
+	public final static void log() {
+		
+	}
+}
