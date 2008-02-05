07b1ba286bc4b9cde80661f7f929e44b77e46478
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-02-05 01:57:49 +0000

    regenerated Hibernate classes

---
+++ src/java/org/ncbo/stanford/domain/generated/NcboUserRole.java
@@ -0,0 +1,22 @@
+package org.ncbo.stanford.domain.generated;
+
+/**
+ * NcboUserRole entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+public class NcboUserRole extends AbstractNcboUserRole implements
+		java.io.Serializable {
+
+	// Constructors
+
+	/** default constructor */
+	public NcboUserRole() {
+	}
+
+	/** full constructor */
+	public NcboUserRole(NcboLRole ncboLRole, NcboUser ncboUser) {
+		super(ncboLRole, ncboUser);
+	}
+
+}
