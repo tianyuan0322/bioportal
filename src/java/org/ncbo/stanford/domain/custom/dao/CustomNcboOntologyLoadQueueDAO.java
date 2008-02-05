e837ae846fe236fe28d538f7e86956a4d2d4a2be
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-02-05 01:58:52 +0000

    new db tables

---
+++ src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboOntologyLoadQueueDAO.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.ncbo.stanford.domain.custom.dao;
+
+import org.ncbo.stanford.domain.generated.NcboOntologyLoadQueueDAO;
+
+/**
+ * @author Michael Dorf
+ *
+ */
+public class CustomNcboOntologyLoadQueueDAO extends NcboOntologyLoadQueueDAO {
+
+	/**
+	 * 
+	 */
+	public CustomNcboOntologyLoadQueueDAO() {
+		super();
+	}
+
+}
