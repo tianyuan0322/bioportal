f17ea2b57d497b322a7cb43b88fd7eece8b19998
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-02-22 03:00:21 +0000

    removed references to absolete ConceptBean

--- src/test/org/ncbo/stanford/manager/impl/OntologyRetrievalManagerProtegeTest.java
+++ src/test/org/ncbo/stanford/manager/impl/OntologyRetrievalManagerProtegeTest.java
@@ -1,11 +1,8 @@
 package org.ncbo.stanford.manager.impl;
 
-import java.util.Iterator;
-import java.util.List;
-
 import junit.framework.TestCase;
 
-import org.ncbo.stanford.bean.ConceptBean;
+import org.ncbo.stanford.bean.concept.ClassBean;
 import org.ncbo.stanford.manager.wrapper.impl.OntologyRetrievalManagerWrapperProtegeImpl;
 
 /**
@@ -20,84 +17,27 @@ public class OntologyRetrievalManagerProtegeTest extends TestCase {
 
 	public void testGetRootNode() {
     	OntologyRetrievalManagerWrapperProtegeImpl ocMgr = new OntologyRetrievalManagerWrapperProtegeImpl();
-    	ConceptBean conceptBean = ocMgr.getRootConcept(TEST_ONT_ID);
+    	ClassBean conceptBean = ocMgr.getRootConcept(TEST_ONT_ID);
   
       	System.out.println("ROOT");
         
     	outputConcept(conceptBean);
-
-    	int i = 0;
-    	
-    	List children = conceptBean.getChildren();
-
-    	this.assertEquals(children.size(), 2);
-    	
-    	System.out.println("CHILDREN");
-    	for (Iterator it = children.iterator(); it.hasNext(); ) {
-    		ConceptBean child = (ConceptBean)it.next();
-     		System.out.println("Child "+ (++i));
-     		outputConcept(child);
-    	}
-    	
     }
     
 	public void testPizzaConcept() {
       	System.out.println("Starting testGetConcept");
     	OntologyRetrievalManagerWrapperProtegeImpl ocMgr = new OntologyRetrievalManagerWrapperProtegeImpl();
-    	ConceptBean conceptBean = ocMgr.findConcept(TEST_CONCEPT_NAME, TEST_ONT_ID);
-  
-        
+    	ClassBean conceptBean = ocMgr.findConcept(TEST_CONCEPT_NAME, TEST_ONT_ID);
+      
     	outputConcept(conceptBean);
-
-    	List children = conceptBean.getChildren();
-    	this.assertEquals(children.size(), 11);
-
-    	int i = 0;
-    	for (Iterator it = children.iterator(); it.hasNext(); ) {
-    		ConceptBean child = (ConceptBean)it.next();
-    		System.out.println("Child "+ (++i));
-    		outputConcept(child);
-    	}
-    	
-    	System.out.println("Parents of " + TEST_CONCEPT_NAME);
-
-    	i = 0;
-    	for (Iterator it = conceptBean.getParents().iterator(); it.hasNext(); ) {
-    		ConceptBean parent = (ConceptBean)it.next();
-    		System.out.println("Parent "+ (++i));
-    		outputConcept(parent);
-    	}
     }
 
 	public void testCheeseyVegetablePizzaConcept() {
       	System.out.println("Starting cheesyvegetablepizza concept");
     	OntologyRetrievalManagerWrapperProtegeImpl ocMgr = new OntologyRetrievalManagerWrapperProtegeImpl();
-    	ConceptBean conceptBean = ocMgr.findConcept("CheeseyVegetableTopping", TEST_ONT_ID);
-  
+    	ClassBean classBean = ocMgr.findConcept("CheeseyVegetableTopping", TEST_ONT_ID);
         
-    	outputConcept(conceptBean);
-
-    	List children = conceptBean.getChildren();
-    	this.assertEquals(children.size(), 0);
-
-    	int i = 0;
-    	for (Iterator it = children.iterator(); it.hasNext(); ) {
-    		ConceptBean child = (ConceptBean)it.next();
-    		System.out.println("Child "+ (++i));
-    		outputConcept(child);
-    	}
-
-    	
-    	System.out.println("Parents of " + TEST_CONCEPT_NAME);
-
-    	i = 0;
-    	List parents = conceptBean.getParents();
-    	this.assertEquals(parents.size(), 2);
-    	for (Iterator it = parents.iterator(); it.hasNext(); ) {
-    		ConceptBean parent = (ConceptBean)it.next();
-    		System.out.println("Parent "+ (++i));
-    		outputConcept(parent);
-    	}
+    	outputConcept(classBean);
     }
 
     
@@ -108,12 +48,8 @@ public class OntologyRetrievalManagerProtegeTest extends TestCase {
      * Output concept content for debugging.  Potential helper method to be added to ConceptBean itself or in the 
      * helper package.
      */
-    private void outputConcept(ConceptBean conceptBean) {
-    	System.out.println("conceptBean: " + conceptBean);
-    	System.out.println("name: " + conceptBean.getDisplayLabel());
-    	System.out.println("concept id: " + conceptBean.getId());
-    	System.out.println("ontology id: " + conceptBean.getOntologyId());
-    	System.out.println("display label: " + conceptBean.getDisplayLabel());
+    private void outputConcept(ClassBean classBean) {
+    	System.out.println(classBean);
     }
     
 }
