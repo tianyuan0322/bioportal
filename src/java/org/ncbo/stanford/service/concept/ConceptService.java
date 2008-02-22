db1cb17b985aa7afb80e865cae4e6c6f3c05f8ca
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-02-22 02:57:32 +0000

    removed references to absolete ConceptBean

--- src/java/org/ncbo/stanford/service/concept/ConceptService.java
+++ src/java/org/ncbo/stanford/service/concept/ConceptService.java
@@ -5,7 +5,8 @@ package org.ncbo.stanford.service.concept;
 
 import java.util.ArrayList;
 
-import org.ncbo.stanford.bean.ConceptBean;
+import org.ncbo.stanford.bean.concept.AbstractConceptBean;
+import org.ncbo.stanford.bean.concept.ClassBean;
 
 /**
  * @author nickgriffith
@@ -13,23 +14,33 @@ import org.ncbo.stanford.bean.ConceptBean;
  */
 public interface ConceptService {
 	
-	public ConceptBean findConcept(String id, int ontologyId);
 	
-	public ArrayList<ConceptBean> findPathToRoot(String id, int ontologyId);
 	
-	public ConceptBean findParent(String id, int ontologyId);
 	
-	public ArrayList<ConceptBean> findChildren(String id, int ontologyId);
+	public AbstractConceptBean findConcept();
 	
-	public ArrayList<ConceptBean> findConceptNameExact(String query, ArrayList<Integer> ontologyIds);
 	
-	public ArrayList<ConceptBean> findConceptNameStartsWith(String query, ArrayList<Integer> ontologyIds);
 	
-	public ArrayList<ConceptBean> findConceptNameContains(String query, ArrayList<Integer> ontologyIds);
 	
-	public ArrayList<ConceptBean> findConceptPropertyExact(String property, String query, ArrayList<Integer> ontologyIds);
 	
-	public ArrayList<ConceptBean> findConceptPropertyStartsWith(String property, String query, ArrayList<Integer> ontologyIds);
 	
-	public ArrayList<ConceptBean> findConceptPropertyContains(String property, String query, ArrayList<Integer> ontologyIds);
+	public ClassBean findConcept(String id, int ontologyId);
+	
+	public ArrayList<ClassBean> findPathToRoot(String id, int ontologyId);
+	
+	public ClassBean findParent(String id, int ontologyId);
+	
+	public ArrayList<ClassBean> findChildren(String id, int ontologyId);
+	
+	public ArrayList<ClassBean> findConceptNameExact(String query, ArrayList<Integer> ontologyIds);
+	
+	public ArrayList<ClassBean> findConceptNameStartsWith(String query, ArrayList<Integer> ontologyIds);
+	
+	public ArrayList<ClassBean> findConceptNameContains(String query, ArrayList<Integer> ontologyIds);
+	
+	public ArrayList<ClassBean> findConceptPropertyExact(String property, String query, ArrayList<Integer> ontologyIds);
+	
+	public ArrayList<ClassBean> findConceptPropertyStartsWith(String property, String query, ArrayList<Integer> ontologyIds);
+	
+	public ArrayList<ClassBean> findConceptPropertyContains(String property, String query, ArrayList<Integer> ontologyIds);
 }
