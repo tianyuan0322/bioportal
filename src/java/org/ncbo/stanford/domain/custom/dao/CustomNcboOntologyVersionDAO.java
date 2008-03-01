14dfe71043ce0dd269b7b2bca3be4eb02a932d09
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-01 02:59:13 +0000

    added findOntologyVersion

--- src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboOntologyVersionDAO.java
+++ src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboOntologyVersionDAO.java
@@ -5,6 +5,7 @@ package org.ncbo.stanford.domain.custom.dao;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -12,6 +13,7 @@ import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.ncbo.stanford.domain.custom.entity.NcboOntology;
+import org.ncbo.stanford.domain.generated.NcboOntologyFile;
 import org.ncbo.stanford.domain.generated.NcboOntologyVersion;
 import org.ncbo.stanford.domain.generated.NcboOntologyVersionDAO;
 import org.ncbo.stanford.util.constants.ApplicationConstants;
@@ -32,6 +34,36 @@ public class CustomNcboOntologyVersionDAO extends NcboOntologyVersionDAO {
 	 */
 	public CustomNcboOntologyVersionDAO() {
 		super();
+	}
+
+	/**
+	 * Finds a given version of ontology
+	 * 
+	 * @return ontology object
+	 */
+	public NcboOntology findOntologyVersion(final Integer ontologyVersionId) {
+		NcboOntology ontology = (NcboOntology) getHibernateTemplate().execute(
+				new HibernateCallback() {
+					public Object doInHibernate(Session session)
+							throws HibernateException, SQLException {
+						Query query = session
+								.getNamedQuery("NcboOntologyVersionDAO.GET_ONTOLOGY_VERSION_QUERY");
+						query
+								.setInteger("ontologyVersionId",
+										ontologyVersionId);
+
+						return query.uniqueResult();
+					}
+				});
+		
+		NcboOntologyVersion ontologyVer = findById(ontologyVersionId);
+		Set<NcboOntologyFile> files = ontologyVer.getNcboOntologyFiles();
+		
+		for (NcboOntologyFile file : files) {
+			ontology.addFilename(file.getFilename());
+		}
+
+		return ontology;
 	}
 
 	/**
