4fe721d8cb6fc05ff1ba4871a6fc20cecee2deb5
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-04 00:45:07 +0000

    added comments

--- src/java/org/ncbo/stanford/manager/OntologyLoadManager.java
+++ src/java/org/ncbo/stanford/manager/OntologyLoadManager.java
@@ -2,10 +2,16 @@ package org.ncbo.stanford.manager;
 
 import org.ncbo.stanford.bean.OntologyBean;
 
+/**
+ * Interface to load ontology into the repository
+ * 
+ * @author Michael Dorf
+ * 
+ */
 public interface OntologyLoadManager {
+
 	/**
 	 * Loads the specified ontology into the BioPortal repository.
 	 */
 	public void loadOntology(OntologyBean ontologyBean) throws Exception;
-
 }
