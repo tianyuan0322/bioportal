e72eecf12d253a54ca8c216568c52742c7b19ffc
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-04 00:45:53 +0000

    added comments

--- src/java/org/ncbo/stanford/manager/wrapper/OntologyLoadManagerWrapper.java
+++ src/java/org/ncbo/stanford/manager/wrapper/OntologyLoadManagerWrapper.java
@@ -4,10 +4,22 @@ import java.net.URI;
 
 import org.ncbo.stanford.bean.OntologyBean;
 
+/**
+ * An interface for all API specific load wrappers to conform to. This allows
+ * for a seamless Spring injection.
+ * 
+ * @author Michael Dorf
+ * 
+ */
 public interface OntologyLoadManagerWrapper {
-	
-	
-	public void loadOntology(URI ontologyUri, OntologyBean ontologyBean) throws Exception;
 
-
+	/**
+	 * Load an ontology using the vendor API
+	 * 
+	 * @param ontologyUri
+	 * @param ontologyBean
+	 * @throws Exception
+	 */
+	public void loadOntology(URI ontologyUri, OntologyBean ontologyBean)
+			throws Exception;
 }
