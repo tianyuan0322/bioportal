bf9ea4211b4db2a88c76f45ab3cd3215dcfc4c99
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-08 02:33:47 +0000

    Test for verifying ontology load service

---
+++ src/test/org/ncbo/stanford/service/loader/processor/OntologyLoadProcessorServiceTest.java
@@ -0,0 +1,76 @@
+package org.ncbo.stanford.service.loader.processor;
+
+import java.io.File;
+import java.util.Date;
+
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.ncbo.stanford.bean.OntologyBean;
+import org.ncbo.stanford.util.filehandler.FileHandler;
+import org.ncbo.stanford.util.filehandler.impl.PhysicalDirectoryFileHandler;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+
+/**
+ * @author Michael Dorf
+ * 
+ */
+public class OntologyLoadProcessorServiceTest extends
+		AbstractDependencyInjectionSpringContextTests {
+
+	protected String[] getConfigLocations() {
+		return new String[] { "classpath:applicationContext-datasources.xml",
+				"classpath:applicationContext-services.xml",
+				"classpath:applicationContext-rest.xml",
+				"classpath:applicationContext-security.xml" };
+	}
+
+	public void testUploadOntology() throws Exception {
+
+		OntologyLoadProcessorService service = (OntologyLoadProcessorService) applicationContext
+				.getBean("ontologyLoadProcessorService",
+						OntologyLoadProcessorService.class);
+		
+		File inputFile = new File("/apps/bmir.apps/bioportal/files/test.obo");
+		FileHandler ontologyFile = new PhysicalDirectoryFileHandler(inputFile);
+		
+		
+//		DiskFileItemFactory factory = new DiskFileItemFactory(
+//		        DiskFileItemFactory.DEFAULT_SIZE_THRESHOLD, inputFile);
+//		ServletFileUpload upload = new ServletFileUpload(factory);		
+//		FileItem ontologyFile = factory.createItem("file1", null, false, "test.obo");
+		
+		
+//		DiskFileItem fileItem = new DiskFileItem("file1", null, true, "test.obo", 22, new File("/hello"));
+		
+//		inputFile = new File("/Users/nickgriffith/Documents/GWT.txt");
+		
+//		FileInputStream inputStream = new FileInputStream(inputFile);
+
+		
+		
+		service.processOntologyLoad(ontologyFile, createTestBean());
+//		
+//		
+//		uploadOntology(inputStream, createTestBean());
+
+	}
+
+	private OntologyBean createTestBean() {
+		OntologyBean bean = new OntologyBean();
+		
+		bean.setUserId(12564);
+		bean.setVersionNumber("5");
+		bean.setIsCurrent(new Byte("1"));
+		bean.setIsRemote(new Byte("0"));
+		bean.setIsReviewed(new Byte("1"));
+		bean.setDateCreated(new Date());
+		bean.setDateReleased(new Date());
+		bean.setContactEmail("email@email.com");
+		bean.setContactName("Name");
+		bean.setDisplayLabel("Testing Loading Ontology");
+		bean.setFormat("OBO");
+		bean.setIsFoundry(new Byte("0"));
+
+		return bean;
+	}
+
+}
