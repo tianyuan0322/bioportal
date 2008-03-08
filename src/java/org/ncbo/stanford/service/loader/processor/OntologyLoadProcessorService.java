438adb6c7f86929f748204c441973dd8e6879b3f
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-08 02:30:28 +0000

    added a wrapper interface for handling files

--- src/java/org/ncbo/stanford/service/loader/processor/OntologyLoadProcessorService.java
+++ src/java/org/ncbo/stanford/service/loader/processor/OntologyLoadProcessorService.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 
 import org.apache.commons.fileupload.FileItem;
 import org.ncbo.stanford.bean.OntologyBean;
+import org.ncbo.stanford.util.filehandler.FileHandler;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Interface to load an ontology into BioPortal.
  * 
  * @author Michael Dorf
- *
+ * 
  */
 @Transactional
 public interface OntologyLoadProcessorService {
@@ -22,6 +23,6 @@ public interface OntologyLoadProcessorService {
 	 * @param ontologyBean
 	 * @throws IOException
 	 */
-	public void processOntologyLoad(FileItem ontologyFile,
-			OntologyBean ontologyBean) throws IOException;
+	public void processOntologyLoad(FileHandler ontologyFile,
+			OntologyBean ontologyBean) throws Exception;
 }
