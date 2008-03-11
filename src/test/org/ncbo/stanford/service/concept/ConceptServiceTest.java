997dd0c06bb4fbe103a2a9f8a1b96a532880c02d
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-11 21:33:11 +0000

    changed all tests to use AbstractBioPortalTest

--- src/test/org/ncbo/stanford/service/concept/ConceptServiceTest.java
+++ src/test/org/ncbo/stanford/service/concept/ConceptServiceTest.java
@@ -3,28 +3,16 @@ package org.ncbo.stanford.service.concept;
 import java.util.ArrayList;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.ncbo.stanford.AbstractBioPortalTest;
 import org.ncbo.stanford.bean.concept.ClassBean;
 import org.ncbo.stanford.util.constants.ApplicationConstants;
 import org.springframework.orm.hibernate3.SessionFactoryUtils;
 import org.springframework.orm.hibernate3.SessionHolder;
-import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-public class ConceptServiceTest extends
-		AbstractDependencyInjectionSpringContextTests {
-
-	private SessionFactory sessionFactory;
-
-	protected String[] getConfigLocations() {
-		return new String[] { "classpath:applicationContext-datasources.xml",
-				"classpath:applicationContext-services.xml",
-				"classpath:applicationContext-rest.xml",
-				"classpath:applicationContext-security.xml" };
-	}
+public class ConceptServiceTest extends AbstractBioPortalTest {
 
 	public void testfindRoot() {
-
 		ConceptService service = (ConceptService) applicationContext.getBean(
 				"conceptService", ConceptService.class);
 
@@ -33,15 +21,8 @@ public class ConceptServiceTest extends
 		TransactionSynchronizationManager.bindResource(this.sessionFactory,
 				new SessionHolder(session));
 
-		
-		
-		
-		
 		ClassBean root = service.findRoot(2854);
 
-		
-		
-		
 		System.out.println("Subclasses");
 		ArrayList<ClassBean> subclasses = (ArrayList<ClassBean>) root
 				.getRelations().get(ApplicationConstants.SUB_CLASS);
@@ -50,27 +31,7 @@ public class ConceptServiceTest extends
 			System.out.println(subclass.getLabel() + " " + subclass.getId());
 		}
 
-		
-		
-		
 		TransactionSynchronizationManager.unbindResource(sessionFactory);
 		SessionFactoryUtils.releaseSession(session, sessionFactory);
-
 	}
-
-	/**
-	 * @return the sessionFactory
-	 */
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-
-	/**
-	 * @param sessionFactory
-	 *            the sessionFactory to set
-	 */
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
-
 }
