b9ae847d9bcb5ae63bb8edd12771e79c3e8f5425
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-11 21:55:21 +0000

    moved hibernate session instantiation to the global Abstract class

--- src/test/org/ncbo/stanford/service/concept/ConceptServiceTest.java
+++ src/test/org/ncbo/stanford/service/concept/ConceptServiceTest.java
@@ -2,36 +2,24 @@ package org.ncbo.stanford.service.concept;
 
 import java.util.ArrayList;
 
-import org.hibernate.Session;
 import org.ncbo.stanford.AbstractBioPortalTest;
 import org.ncbo.stanford.bean.concept.ClassBean;
 import org.ncbo.stanford.util.constants.ApplicationConstants;
-import org.springframework.orm.hibernate3.SessionFactoryUtils;
-import org.springframework.orm.hibernate3.SessionHolder;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 public class ConceptServiceTest extends AbstractBioPortalTest {
 
 	public void testfindRoot() {
 		ConceptService service = (ConceptService) applicationContext.getBean(
 				"conceptService", ConceptService.class);
-
-		Session session = SessionFactoryUtils.getSession(this.sessionFactory,
-				true);
-		TransactionSynchronizationManager.bindResource(this.sessionFactory,
-				new SessionHolder(session));
-
 		ClassBean root = service.findRoot(2854);
 
 		System.out.println("Subclasses");
 		ArrayList<ClassBean> subclasses = (ArrayList<ClassBean>) root
 				.getRelations().get(ApplicationConstants.SUB_CLASS);
 		System.out.println("Size:" + subclasses.size());
+
 		for (ClassBean subclass : subclasses) {
 			System.out.println(subclass.getLabel() + " " + subclass.getId());
 		}
-
-		TransactionSynchronizationManager.unbindResource(sessionFactory);
-		SessionFactoryUtils.releaseSession(session, sessionFactory);
 	}
 }
