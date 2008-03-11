a9ec383cdf186704139542926035dba100a023e0
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-11 21:54:51 +0000

    added hibernate session instantiation and destroy

--- src/test/org/ncbo/stanford/AbstractBioPortalTest.java
+++ src/test/org/ncbo/stanford/AbstractBioPortalTest.java
@@ -1,12 +1,34 @@
 package org.ncbo.stanford;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
+import org.springframework.orm.hibernate3.SessionHolder;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+/**
+ * Contains setup and configuration functionality common to all BioPortal tests
+ * 
+ * @author Michael Dorf
+ * 
+ */
 public class AbstractBioPortalTest extends
 		AbstractDependencyInjectionSpringContextTests {
 
 	protected SessionFactory sessionFactory;
+	private Session session;
+
+	protected void onSetUp() {
+		session = SessionFactoryUtils.getSession(this.sessionFactory, true);
+		TransactionSynchronizationManager.bindResource(this.sessionFactory,
+				new SessionHolder(session));
+	}
+
+	protected void onTearDown() {
+		TransactionSynchronizationManager.unbindResource(sessionFactory);
+		SessionFactoryUtils.releaseSession(session, sessionFactory);
+	}
 
 	protected String[] getConfigLocations() {
 		return new String[] { "classpath:applicationContext-datasources.xml",
