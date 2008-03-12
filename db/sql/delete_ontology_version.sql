ad53d6a6dc292d010c1fc365376180a733f6d5a9
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-12 22:54:27 +0000

    no change

--- db/sql/delete_ontology_version.sql
+++ db/sql/delete_ontology_version.sql
@@ -1,4 +1,4 @@
-set @var_ontology_version_id := 15828; 
+set @var_ontology_version_id := 15830; 
 
 delete from ncbo_ontology_file where ontology_version_id = @var_ontology_version_id;
 delete from ncbo_ontology_category where ontology_version_id = @var_ontology_version_id;
