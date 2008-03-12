bcec0590f96933b2f13a67dc2bf631c978bb3532
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-12 23:07:23 +0000

    implemented test

--- src/test/org/ncbo/stanford/service/loader/scheduler/OntologyLoadSchedulerServiceTest.java
+++ src/test/org/ncbo/stanford/service/loader/scheduler/OntologyLoadSchedulerServiceTest.java
@@ -5,6 +5,9 @@ import org.ncbo.stanford.AbstractBioPortalTest;
 public class OntologyLoadSchedulerServiceTest extends AbstractBioPortalTest {
 
 	public void testProcessOntologyLoad() throws Exception {
-
+		OntologyLoadSchedulerService service = (OntologyLoadSchedulerService) applicationContext
+				.getBean("ontologyLoadSchedulerService",
+						OntologyLoadSchedulerService.class);
+		service.processOntologyLoad();
 	}
 }
