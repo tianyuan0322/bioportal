e2384163906d4083cb6a9f2b0b922b06cfd66e0d
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-03-12 23:01:27 +0000

    added comments

--- src/java/org/ncbo/stanford/service/xml/XMLSerializationService.java
+++ src/java/org/ncbo/stanford/service/xml/XMLSerializationService.java
@@ -2,6 +2,12 @@ package org.ncbo.stanford.service.xml;
 
 import org.ncbo.stanford.enumeration.ErrorTypeEnum;
 
+/**
+ * An interface that allows predefined structure of XML responses
+ * 
+ * @author Michael Dorf
+ * 
+ */
 public interface XMLSerializationService {
 
 	/**
@@ -17,12 +23,22 @@ public interface XMLSerializationService {
 	 * should only be used when no other XML response is expected (i.e.
 	 * authentication).
 	 * 
-	 * @param errorType
+	 * @param sessionId
+	 * @param accessedResource
 	 * @return
 	 */
 	public String getSuccessAsXML(String sessionId, String accessedResource);
-	
 
-	
-	public String getSuccessAsXML(String sessionId, String accessedResource, Object data);
+	/**
+	 * Generate an XML representation of a successfully processed request. This
+	 * should only be used when no other XML response is expected (i.e.
+	 * authentication).
+	 * 
+	 * @param sessionId
+	 * @param accessedResource
+	 * @param data
+	 * @return
+	 */
+	public String getSuccessAsXML(String sessionId, String accessedResource,
+			Object data);
 }
