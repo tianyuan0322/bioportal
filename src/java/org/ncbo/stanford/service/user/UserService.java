65934800a37f44adbcdfc67ef13f96b52b062ea7
ngriff <ngriff@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-04-17 21:37:23 +0000

    added service for findAll() users.

--- src/java/org/ncbo/stanford/service/user/UserService.java
+++ src/java/org/ncbo/stanford/service/user/UserService.java
@@ -1,8 +1,11 @@
 package org.ncbo.stanford.service.user;
 
+import java.util.List;
+
 import org.ncbo.stanford.bean.UserBean;
 
 public interface UserService {
 	public UserBean getUser(Integer userId);
 	public UserBean getUser(String username);
+	public List<UserBean> findUsers();
 }
