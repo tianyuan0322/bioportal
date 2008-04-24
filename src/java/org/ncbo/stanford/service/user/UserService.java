e1e0716b4b0b8420f0d47768b3614e3a30eefb0d
ngriff <ngriff@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-04-24 21:41:04 +0000

    added saveUser() related API - cyoun

--- src/java/org/ncbo/stanford/service/user/UserService.java
+++ src/java/org/ncbo/stanford/service/user/UserService.java
@@ -3,9 +3,15 @@ package org.ncbo.stanford.service.user;
 import java.util.List;
 
 import org.ncbo.stanford.bean.UserBean;
+import org.ncbo.stanford.domain.generated.NcboUser;
 
 public interface UserService {
 	public UserBean getUser(Integer userId);
 	public UserBean getUser(String username);
 	public List<UserBean> findUsers();
+	public void saveUser(NcboUser ncboUser);
+	public void deleteUser(NcboUser ncboUser);
+	public NcboUser populateUser(UserBean userBean);
+	public UserBean populateUser(NcboUser ncboUser);
+	
 }
