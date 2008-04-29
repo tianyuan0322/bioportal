dd0eb8dc4538fb4a23051d39e897165a4755fb98
ngriff <ngriff@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-04-29 17:20:42 +0000

    draft

--- src/java/org/ncbo/stanford/view/rest/restlet/user/UsersRestlet.java
+++ src/java/org/ncbo/stanford/view/rest/restlet/user/UsersRestlet.java
@@ -1,5 +1,6 @@
 package org.ncbo.stanford.view.rest.restlet.user;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -15,12 +16,17 @@ import org.ncbo.stanford.util.RequestUtils;
 import org.ncbo.stanford.util.helper.DateHelper;
 import org.restlet.Restlet;
 import org.restlet.data.Form;
+import org.restlet.data.Parameter;
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.restlet.ext.fileupload.RestletFileUpload;
+
+// cyoun
+import org.ncbo.stanford.domain.generated.NcboUser;
+
 
 /**
  * Question : the naming convention - when to use "find" or "get"?
@@ -60,22 +66,34 @@ public class UsersRestlet extends Restlet {
 	 * @param response
 	 */
 	private void postRequest(Request request, Response response) {
-		Form form = request.getEntityAsForm();
 
+		createUser(request, response);
+		
+		/*   
+		 * ??? cyoun
+		 *  
+		Form form = request.getEntityAsForm();
+		
 		// 1/ Create a factory for disk-based file items
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		factory.setSizeThreshold(10002400);
 
+		
 		RestletFileUpload rfu = new RestletFileUpload(factory);
 		rfu.setSizeMax(10000000);
+		
+		
+		
 		try {
+		
+	
 			List<FileItem> files = rfu.parseRepresentation(request.getEntity());
 
-			
-			
+				
 			
 //			ontologyLoadProcessorService.processOntologyLoad(files.get(0),
 //					buildBeanFromForm(form));
+		
 			
 			
 		} catch (Exception e) {
@@ -87,9 +105,12 @@ public class UsersRestlet extends Restlet {
 			return;
 		}
 
+		 */
+		
 		RequestUtils.setHttpServletResponse(response, Status.SUCCESS_OK,
 				MediaType.TEXT_XML, xmlSerializationService.getSuccessAsXML(
 						RequestUtils.getSessionId(request), null));
+	
 	}
 
 	/**
@@ -104,6 +125,79 @@ public class UsersRestlet extends Restlet {
 				MediaType.TEXT_XML, xmlSerializationService.getSuccessAsXML(
 						RequestUtils.getSessionId(request), request
 								.getResourceRef().getPath(), userList));
+	}
+	
+	
+	/**
+	 * Return to the response creating user
+	 * 
+	 * @param request response
+	 */
+	private void createUser(Request request, Response response) {
+		
+		
+		
+
+		Form form = request.getResourceRef().getQueryAsForm();
+
+		System.out.println(" Form as entity from = " + form);
+
+		System.out.println(" Form as query String = " + form.getQueryString());
+		
+
+		System.out.println("Trying to getParameters...");
+		//StringBuffer sb = new StringBuffer("foo");
+		for (Parameter p : form) {
+			// System.out.println(p);
+			System.out.println("field name = " + p.getName() + "  value = "
+					+ p.getValue());
+
+		}
+
+		         
+		
+		/*
+		 * Form form = request.getEntityAsForm();
+		 *  // error handling?
+		 * 
+		 * if (form != null) {
+		 * 
+		 * 
+		 * String username = form.getFirstValue("username"); String password =
+		 * form.getFirstValue("password"); String firstname =
+		 * form.getFirstValue("firstname"); String lastname =
+		 * form.getFirstValue("lastname"); String email =
+		 * form.getFirstValue("email"); Date dateCreated =
+		 * DateHelper.getDateFrom(form .getFirstValue("dateCreated"));
+		 * 
+		 * 
+		 * 
+		 * 
+		 * NcboUser ncboUser = new NcboUser(username, password, email,
+		 * firstname, lastname, dateCreated);
+		 * 
+		 * System.out.println("+++++++++++++++++++++++++"); System.out.println("
+		 * createUser() : NCBO USER created "); System.out.println(username);
+		 * System.out.println(password); System.out.println(email);
+		 * System.out.println("----");
+		 * System.out.println(ncboUser.getUsername());
+		 * System.out.println(ncboUser.getPassword());
+		 * System.out.println(ncboUser.getEmail());
+		 * System.out.println("+++++++++++++++++++++++++");
+		 * 
+		 *  // getUserService().saveUser(ncboUser);
+		 *  }
+		 * 
+		 */
+		
+		/*
+		List<UserBean> userList = getUserService().findUsers();
+
+		RequestUtils.setHttpServletResponse(response, Status.SUCCESS_OK,
+				MediaType.TEXT_XML, xmlSerializationService.getSuccessAsXML(
+						RequestUtils.getSessionId(request), request
+								.getResourceRef().getPath(), userList));
+		*/
 	}
 
 	/**
