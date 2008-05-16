1a31daa121e6df19ea5467d88cb8ecd2e2c28f5b
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-05-16 00:39:08 +0000

    removed population of remote ontologies into the queue

--- classic_to_core_migration/db/sql/populate_load_queue.sql
+++ classic_to_core_migration/db/sql/populate_load_queue.sql
@@ -1,6 +1,6 @@
 use bioportal;
 
 insert into ncbo_ontology_load_queue (ontology_version_id, status_id)
-select id, 1 from ncbo_ontology_version;
+select id, 1 from ncbo_ontology_version where is_remote = 0;
 
 update ncbo_ontology_version set status_id = 1;
