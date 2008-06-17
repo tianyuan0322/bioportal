ed700f50fc2d8e4d363ba2f0f80a30980d7329f4
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-06-17 00:26:41 +0000

    implementing OBO CVS pull

---
+++ src/java/org/ncbo/stanford/service/loader/remote/OBOCVSPullService.java
@@ -0,0 +1,6 @@
+package org.ncbo.stanford.service.loader.remote;
+
+public interface OBOCVSPullService {
+	
+	public void doCVSPull();
+}
---
+++ src/java/org/ncbo/stanford/service/loader/remote/impl/OBOSVSPullServiceImpl.java
@@ -0,0 +1,150 @@
+package org.ncbo.stanford.service.loader.remote.impl;
+
+import org.ncbo.stanford.service.loader.remote.OBOCVSPullService;
+import org.ncbo.stanford.util.cvs.CVSUtils;
+
+public class OBOSVSPullServiceImpl implements OBOCVSPullService {
+
+	private String oboSourceforgeCVSUsername;
+	private String oboSourceforgeCVSPassword;
+	private String oboSourceforgeCVSHostname;
+	private String oboSourceforgeCVSModule;
+	private String oboSourceforgeCVSRootDirectory;
+	private String oboSourceforgeCVSArgumentString;
+	private String oboSourceforgeCVSCheckoutDir;
+	private String tempDir;
+
+	public void doCVSPull() {
+		CVSUtils cvsUtils = new CVSUtils(oboSourceforgeCVSUsername,
+				oboSourceforgeCVSPassword, oboSourceforgeCVSHostname,
+				oboSourceforgeCVSModule, oboSourceforgeCVSRootDirectory,
+				oboSourceforgeCVSArgumentString, oboSourceforgeCVSCheckoutDir,
+				tempDir);
+
+		cvsUtils.cvsCheckout();
+	}
+
+	/**
+	 * @return the oboSourceforgeCVSUsername
+	 */
+	public String getOboSourceforgeCVSUsername() {
+		return oboSourceforgeCVSUsername;
+	}
+
+	/**
+	 * @param oboSourceforgeCVSUsername
+	 *            the oboSourceforgeCVSUsername to set
+	 */
+	public void setOboSourceforgeCVSUsername(String oboSourceforgeCVSUsername) {
+		this.oboSourceforgeCVSUsername = oboSourceforgeCVSUsername;
+	}
+
+	/**
+	 * @return the oboSourceforgeCVSPassword
+	 */
+	public String getOboSourceforgeCVSPassword() {
+		return oboSourceforgeCVSPassword;
+	}
+
+	/**
+	 * @param oboSourceforgeCVSPassword
+	 *            the oboSourceforgeCVSPassword to set
+	 */
+	public void setOboSourceforgeCVSPassword(String oboSourceforgeCVSPassword) {
+		this.oboSourceforgeCVSPassword = oboSourceforgeCVSPassword;
+	}
+
+	/**
+	 * @return the oboSourceforgeCVSHostname
+	 */
+	public String getOboSourceforgeCVSHostname() {
+		return oboSourceforgeCVSHostname;
+	}
+
+	/**
+	 * @param oboSourceforgeCVSHostname
+	 *            the oboSourceforgeCVSHostname to set
+	 */
+	public void setOboSourceforgeCVSHostname(String oboSourceforgeCVSHostname) {
+		this.oboSourceforgeCVSHostname = oboSourceforgeCVSHostname;
+	}
+
+	/**
+	 * @return the oboSourceforgeCVSModule
+	 */
+	public String getOboSourceforgeCVSModule() {
+		return oboSourceforgeCVSModule;
+	}
+
+	/**
+	 * @param oboSourceforgeCVSModule
+	 *            the oboSourceforgeCVSModule to set
+	 */
+	public void setOboSourceforgeCVSModule(String oboSourceforgeCVSModule) {
+		this.oboSourceforgeCVSModule = oboSourceforgeCVSModule;
+	}
+
+	/**
+	 * @return the oboSourceforgeCVSRootDirectory
+	 */
+	public String getOboSourceforgeCVSRootDirectory() {
+		return oboSourceforgeCVSRootDirectory;
+	}
+
+	/**
+	 * @param oboSourceforgeCVSRootDirectory
+	 *            the oboSourceforgeCVSRootDirectory to set
+	 */
+	public void setOboSourceforgeCVSRootDirectory(
+			String oboSourceforgeCVSRootDirectory) {
+		this.oboSourceforgeCVSRootDirectory = oboSourceforgeCVSRootDirectory;
+	}
+
+	/**
+	 * @return the oboSourceforgeCVSArgumentString
+	 */
+	public String getOboSourceforgeCVSArgumentString() {
+		return oboSourceforgeCVSArgumentString;
+	}
+
+	/**
+	 * @param oboSourceforgeCVSArgumentString
+	 *            the oboSourceforgeCVSArgumentString to set
+	 */
+	public void setOboSourceforgeCVSArgumentString(
+			String oboSourceforgeCVSArgumentString) {
+		this.oboSourceforgeCVSArgumentString = oboSourceforgeCVSArgumentString;
+	}
+
+	/**
+	 * @return the oboSourceforgeCVSCheckoutDir
+	 */
+	public String getOboSourceforgeCVSCheckoutDir() {
+		return oboSourceforgeCVSCheckoutDir;
+	}
+
+	/**
+	 * @param oboSourceforgeCVSCheckoutDir
+	 *            the oboSourceforgeCVSCheckoutDir to set
+	 */
+	public void setOboSourceforgeCVSCheckoutDir(
+			String oboSourceforgeCVSCheckoutDir) {
+		this.oboSourceforgeCVSCheckoutDir = oboSourceforgeCVSCheckoutDir;
+	}
+
+	/**
+	 * @return the tempDir
+	 */
+	public String getTempDir() {
+		return tempDir;
+	}
+
+	/**
+	 * @param tempDir
+	 *            the tempDir to set
+	 */
+	public void setTempDir(String tempDir) {
+		this.tempDir = tempDir;
+	}
+
+}
