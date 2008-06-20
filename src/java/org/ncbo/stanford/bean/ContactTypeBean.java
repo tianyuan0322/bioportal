76468e6ed0a73a0d9a70f37027ca05bb540924b2
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-06-20 00:54:36 +0000

    contact type bean required by cvs obo pull

---
+++ src/java/org/ncbo/stanford/bean/ContactTypeBean.java
@@ -0,0 +1,79 @@
+package org.ncbo.stanford.bean;
+
+import java.io.Serializable;
+
+public class ContactTypeBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3012806504438328145L;
+	private String email;
+	private String emailType;
+	private String name;
+	private String organization;
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the emailType
+	 */
+	public String getEmailType() {
+		return emailType;
+	}
+
+	/**
+	 * @param emailType
+	 *            the emailType to set
+	 */
+	public void setEmailType(String emailType) {
+		this.emailType = emailType;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the organization
+	 */
+	public String getOrganization() {
+		return organization;
+	}
+
+	/**
+	 * @param organization
+	 *            the organization to set
+	 */
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+
+	public String toString() {
+		return "name: " + name + "\nemail: " + email;
+	}
+}
