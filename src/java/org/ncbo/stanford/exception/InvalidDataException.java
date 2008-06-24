b8686ae937ee32711c3d5511bc7da7dd8453dff7
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-06-24 00:47:54 +0000

    custom exception for improperly formatted data

---
+++ src/java/org/ncbo/stanford/exception/InvalidDataException.java
@@ -0,0 +1,27 @@
+package org.ncbo.stanford.exception;
+
+public class InvalidDataException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5777897520002877516L;
+
+	public static final String DEFAULT_MESSAGE = "Invalid data";
+	
+	public InvalidDataException(String msg) {
+		super(msg);
+	}
+
+	public InvalidDataException(String msg, Exception e) {
+		super(msg, e);
+	}
+
+	public InvalidDataException(Throwable t) {
+		super(t);
+	}
+
+	public InvalidDataException(String msg, Throwable t) {
+		super(msg, t);
+	}
+}
