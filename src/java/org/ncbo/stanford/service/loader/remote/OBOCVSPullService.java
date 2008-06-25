9f61bd24f5887d97d32d66362e72d486fd9a29db
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-06-25 19:05:57 +0000

    added comments

--- src/java/org/ncbo/stanford/service/loader/remote/OBOCVSPullService.java
+++ src/java/org/ncbo/stanford/service/loader/remote/OBOCVSPullService.java
@@ -1,6 +1,15 @@
 package org.ncbo.stanford.service.loader.remote;
 
+/**
+ * A utility that runs as a scheduled process, connecting to OBO Sourceforge CVS
+ * system and pulling all new and updated ontologies into BioPortal
+ * 
+ * @author Michael Dorf
+ */
 public interface OBOCVSPullService {
-	
+
+	/**
+	 * Performs the pull of ontologies from OBO Sourceforge CVS
+	 */
 	public void doCVSPull();
 }
