460c839879b2e886bd2c79f8bc03a471ec72c3a5
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-06-26 00:12:46 +0000

    added method findCategoriesByOBOFoundryNames

--- src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboLCategoryDAO.java
+++ src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboLCategoryDAO.java
@@ -3,11 +3,15 @@
  */
 package org.ncbo.stanford.domain.custom.dao;
 
+import java.util.List;
+
+import org.hibernate.criterion.Expression;
+import org.ncbo.stanford.domain.generated.NcboLCategory;
 import org.ncbo.stanford.domain.generated.NcboLCategoryDAO;
 
 /**
- * @author mdorf
- *
+ * @author Michael Dorf
+ * 
  */
 public class CustomNcboLCategoryDAO extends NcboLCategoryDAO {
 
@@ -16,5 +20,19 @@ public class CustomNcboLCategoryDAO extends NcboLCategoryDAO {
 	 */
 	public CustomNcboLCategoryDAO() {
 		super();
+	}
+
+	/**
+	 * Returns a list of categories that correspond to the given obo foundry
+	 * names
+	 * 
+	 * @param oboFoundryNames
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<NcboLCategory> findCategoriesByOBOFoundryNames(
+			String[] oboFoundryNames) {
+		return getSession().createCriteria(NcboLCategory.class).add(
+				Expression.in("oboFoundryName", oboFoundryNames)).list();
 	}
 }
