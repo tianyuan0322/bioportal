d99a05c97d3b5cbcf0d5f73cd632c9a25cebcd85
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-06-28 20:59:23 +0000

    added saveOntologyCategory method

--- src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboOntologyCategoryDAO.java
+++ src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboOntologyCategoryDAO.java
@@ -3,13 +3,19 @@
  */
 package org.ncbo.stanford.domain.custom.dao;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ncbo.stanford.domain.generated.NcboOntologyCategory;
 import org.ncbo.stanford.domain.generated.NcboOntologyCategoryDAO;
 
 /**
  * @author mdorf
- *
+ * 
  */
 public class CustomNcboOntologyCategoryDAO extends NcboOntologyCategoryDAO {
+
+	private static final Log log = LogFactory
+			.getLog(CustomNcboOntologyCategoryDAO.class);
 
 	/**
 	 * 
@@ -18,4 +24,21 @@ public class CustomNcboOntologyCategoryDAO extends NcboOntologyCategoryDAO {
 		super();
 	}
 
+	/**
+	 * @param transientInstance
+	 * @return
+	 */
+	public NcboOntologyCategory saveOntologyCategory(
+			NcboOntologyCategory transientInstance) {
+		try {
+			Integer newId = (Integer) getHibernateTemplate().save(
+					transientInstance);
+			getHibernateTemplate().flush();
+
+			return this.findById(newId);
+		} catch (RuntimeException re) {
+			log.error("save failed", re);
+			throw re;
+		}
+	}
 }
