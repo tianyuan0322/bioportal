f0785f08d226efa3fd94d977e095cef9dd8cfc57
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-07-01 23:12:12 +0000

    changed the logic to create handlers using reflection - no hardcoding

--- src/java/org/ncbo/stanford/util/ontologyfile/compressedfilehandler/impl/CompressedFileHandlerFactory.java
+++ src/java/org/ncbo/stanford/util/ontologyfile/compressedfilehandler/impl/CompressedFileHandlerFactory.java
@@ -3,33 +3,61 @@
  */
 package org.ncbo.stanford.util.ontologyfile.compressedfilehandler.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.ncbo.stanford.util.constants.ApplicationConstants;
 import org.ncbo.stanford.util.ontologyfile.compressedfilehandler.CompressedFileHandler;
 
 /**
+ * Factory for creating compressed file handlers based on the ontology format
+ * 
  * @author Michael Dorf
  * 
  */
 public class CompressedFileHandlerFactory {
+
+	private static final Log log = LogFactory
+			.getLog(CompressedFileHandlerFactory.class);
+	private static final String FORMAT_OWL = "OWL";
+	private static final String FORMAT_LEXGRID_XML = "LEXGRID_XML";
+
 	public static CompressedFileHandler createFileHandler(String format) {
 		CompressedFileHandler compressedFileHandler = null;
 
-		if (format.equalsIgnoreCase(ApplicationConstants.FORMAT_OBO)) {
-			compressedFileHandler = new OBOCompressedFileHandlerImpl();
-		} else if (format.equalsIgnoreCase(ApplicationConstants.FORMAT_OWL_DL)
-				|| format
-						.equalsIgnoreCase(ApplicationConstants.FORMAT_OWL_FULL)) {
-			compressedFileHandler = new OWLCompressedFileHandlerImpl();
-		} else if (format.equalsIgnoreCase(ApplicationConstants.FORMAT_PROTEGE)) {
-			compressedFileHandler = new PROTEGECompressedFileHandlerImpl();
-		} else if (format
-				.equalsIgnoreCase(ApplicationConstants.FORMAT_UMLS_RRF)) {
-			compressedFileHandler = new UMLSCompressedFileHandlerImpl();
-		} else if (format
-				.equalsIgnoreCase(ApplicationConstants.FORMAT_LEXGRID_XML)) {
-			compressedFileHandler = new LEXGRID_XMLCompressedFileHandlerImpl();
+		try {
+			compressedFileHandler = (CompressedFileHandler) (Class
+					.forName(CompressedFileHandlerFactory.class.getPackage()
+							.getName()
+							+ "."
+							+ getHandlerPrefix(format)
+							+ CompressedFileHandler.class.getSimpleName()
+							+ "Impl").newInstance());
+		} catch (InstantiationException e) {
+			log.error(e);
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			log.error(e);
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			log.error(e);
+			e.printStackTrace();
 		}
 
 		return compressedFileHandler;
+	}
+
+	private static String getHandlerPrefix(String format) {
+		String prefix = format;
+
+		if (format.equalsIgnoreCase(ApplicationConstants.FORMAT_OWL_DL)
+				|| format
+						.equalsIgnoreCase(ApplicationConstants.FORMAT_OWL_FULL)) {
+			prefix = FORMAT_OWL;
+		} else if (format
+				.equalsIgnoreCase(ApplicationConstants.FORMAT_LEXGRID_XML)) {
+			prefix = FORMAT_LEXGRID_XML;
+		}
+
+		return prefix;
 	}
 }
