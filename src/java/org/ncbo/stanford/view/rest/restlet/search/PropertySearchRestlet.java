38e4d480fd2ae8eb38606139816dacfc1d7cae4e
ngriff <ngriff@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-07-22 23:13:00 +0000

    changed error type CLIENT_BAD_REQUEST to SERVER_INTERNAL_ERROR where applicable. - cyoun

--- src/java/org/ncbo/stanford/view/rest/restlet/search/PropertySearchRestlet.java
+++ src/java/org/ncbo/stanford/view/rest/restlet/search/PropertySearchRestlet.java
@@ -7,97 +7,93 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.ncbo.stanford.bean.OntologyBean;
-import org.ncbo.stanford.bean.UserBean;
-import org.ncbo.stanford.bean.concept.ClassBean;
 import org.ncbo.stanford.bean.search.SearchResultBean;
 import org.ncbo.stanford.service.concept.ConceptService;
-import org.ncbo.stanford.service.ontology.OntologyService;
 import org.ncbo.stanford.service.xml.XMLSerializationService;
 import org.ncbo.stanford.util.RequestUtils;
 import org.ncbo.stanford.view.util.constants.RequestParamConstants;
 import org.restlet.Restlet;
-import org.restlet.data.MediaType;
 import org.restlet.data.Method;
-import org.restlet.data.Reference;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-
 public class PropertySearchRestlet extends Restlet {
 
-	private static final Log log = LogFactory.getLog(PropertySearchRestlet.class);
-	
+	private static final Log log = LogFactory
+			.getLog(PropertySearchRestlet.class);
+
 	private ConceptService conceptService;
 	private XMLSerializationService xmlSerializationService;
 
 	@Override
 	public void handle(Request request, Response response) {
-		
-		if(request.getMethod().equals(Method.GET)){
+
+		if (request.getMethod().equals(Method.GET)) {
 			getRequest(request, response);
 		}
-		
+
 	}
-	
-	
+
 	/**
 	 * Handle GET calls here
 	 */
-	private void getRequest(Request request,Response response){		
-		searchProperty(request,response);			
+	private void getRequest(Request request, Response response) {
+		searchProperty(request, response);
 	}
-	
-	
+
 	/**
 	 * Handle POST calls here
+	 * 
 	 * @param request
 	 * @param response
 	 */
-	private void postRequest(Request request,Response response){
-		
+	private void postRequest(Request request, Response response) {
+
 	}
-	
+
 	/**
 	 * Return to the response an individual ontology
+	 * 
 	 * @param ref
 	 * @param resp
 	 */
 	private void searchProperty(Request request, Response resp) {
 		List<SearchResultBean> concepts = null;
 		String query = (String) request.getAttributes().get("query");
-		HttpServletRequest httpRequest = RequestUtils.getHttpServletRequest(request);
-		String ontologies = (String) httpRequest.getParameter("ontologies");		
-		
+		HttpServletRequest httpRequest = RequestUtils
+				.getHttpServletRequest(request);
+		String ontologies = (String) httpRequest.getParameter("ontologies");
+
 		try {
-			
+
 			List<Integer> ontologyIds = new ArrayList<Integer>();
-			if(!ontologies.equalsIgnoreCase(RequestParamConstants.PARAM_ALL_ONTOLOGIES)){
+			if (!ontologies
+					.equalsIgnoreCase(RequestParamConstants.PARAM_ALL_ONTOLOGIES)) {
 				String[] ontologyStrings = ontologies.split(",");
-				for(int x=0;x<ontologyStrings.length;x++){
+				for (int x = 0; x < ontologyStrings.length; x++) {
 					ontologyIds.add(Integer.parseInt(ontologyStrings[x]));
 				}
-				
-				
+
 			}
-						
-			concepts = conceptService.findConceptPropertyContains(ontologyIds, query);
-			
+
+			concepts = conceptService.findConceptPropertyContains(ontologyIds,
+					query);
+
 			if (concepts.isEmpty()) {
-				resp.setStatus(Status.CLIENT_ERROR_NOT_FOUND, "Concept not found");
+				resp.setStatus(Status.CLIENT_ERROR_NOT_FOUND,
+						"Concept not found");
 			}
 		} catch (NumberFormatException nfe) {
 			nfe.printStackTrace();
 			resp.setStatus(Status.CLIENT_ERROR_BAD_REQUEST, nfe.getMessage());
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
-			resp.setStatus(Status.CLIENT_ERROR_BAD_REQUEST, e.getMessage());
+			resp.setStatus(Status.SERVER_ERROR_INTERNAL, e.getMessage());
 		}
 
-		getXmlSerializationService().generateXMLResponse (request, resp, concepts);
+		getXmlSerializationService().generateXMLResponse(request, resp,
+				concepts);
 	}
 
 	/**
@@ -115,15 +111,13 @@ public class PropertySearchRestlet extends Restlet {
 		this.conceptService = conceptService;
 	}
 
-
 	public XMLSerializationService getXmlSerializationService() {
 		return xmlSerializationService;
 	}
-
 
 	public void setXmlSerializationService(
 			XMLSerializationService xmlSerializationService) {
 		this.xmlSerializationService = xmlSerializationService;
 	}
-	
+
 }
