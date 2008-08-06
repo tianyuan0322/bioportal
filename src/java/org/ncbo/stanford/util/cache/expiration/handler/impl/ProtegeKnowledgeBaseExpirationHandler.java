4b4cd696621146b1606c7446ea94839dbd068946
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-08-06 00:39:55 +0000

    new class to handler protege knowledgebase expirations

---
+++ src/java/org/ncbo/stanford/util/cache/expiration/handler/impl/ProtegeKnowledgeBaseExpirationHandler.java
@@ -0,0 +1,22 @@
+package org.ncbo.stanford.util.cache.expiration.handler.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ncbo.stanford.util.cache.expiration.handler.AbstractSimpleExpirationHandler;
+
+import edu.stanford.smi.protege.model.KnowledgeBase;
+
+public class ProtegeKnowledgeBaseExpirationHandler extends
+		AbstractSimpleExpirationHandler<Integer, KnowledgeBase> {
+
+	private static final Log log = LogFactory.getLog(ProtegeKnowledgeBaseExpirationHandler.class);
+
+	@Override
+	protected void timeExpired(KnowledgeBase object) {
+		log.debug("Disposing of the knowledgebase: " + object.getName());
+		
+		if (object != null) {
+			object.getProject().dispose();
+		}
+	}
+}
