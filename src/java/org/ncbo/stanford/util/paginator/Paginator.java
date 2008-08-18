cbbaec03f487f66c0a32f8a87cc3df3a70bc16e6
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-08-18 20:15:20 +0000

    added comments

--- src/java/org/ncbo/stanford/util/paginator/Paginator.java
+++ src/java/org/ncbo/stanford/util/paginator/Paginator.java
@@ -2,13 +2,42 @@ package org.ncbo.stanford.util.paginator;
 
 import org.ncbo.stanford.util.paginator.impl.Page;
 
+/**
+ * Interface that allows paginating over any result sets
+ * 
+ * @author Michael Dorf
+ * 
+ * @param <E>
+ */
 public interface Paginator<E> {
 
+	/**
+	 * Returns the first page of results
+	 * 
+	 * @return
+	 */
 	public Page<E> getFirstPage();
 
+	/**
+	 * Returns the last page of results
+	 * 
+	 * @return
+	 */
 	public Page<E> getLastPage();
 
+	/**
+	 * Returns the next page of results
+	 * 
+	 * @param currentPage
+	 * @return
+	 */
 	public Page<E> getNextPage(Page<E> currentPage);
 
+	/**
+	 * Returns the previous page of results
+	 * 
+	 * @param currentPage
+	 * @return
+	 */
 	public Page<E> getPrevPage(Page<E> currentPage);
 }
