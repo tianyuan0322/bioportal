b8d68c67f444985cb9bc57d9147de1183333fa98
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-08-20 19:38:57 +0000

    a shell for pagination restlet

---
+++ src/java/org/ncbo/stanford/view/rest/restlet/paginator/PaginatorRestlet.java
@@ -0,0 +1,28 @@
+package org.ncbo.stanford.view.rest.restlet.paginator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.restlet.Restlet;
+import org.restlet.data.Method;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+
+public class PaginatorRestlet extends Restlet {
+
+	private static final Log log = LogFactory.getLog(PaginatorRestlet.class);
+
+	@Override
+	public void handle(Request request, Response response) {
+		if (request.getMethod().equals(Method.GET)) {
+			getRequest(request, response);
+		}
+	}
+	
+	/**
+	 * Handle GET calls here
+	 */
+	private void getRequest(Request request, Response response) {
+
+	
+	}	
+}
