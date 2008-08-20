124f86a4e096f2c3228b0c92a8aff214883dab31
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-08-20 20:04:58 +0000

    cleanup

--- src/java/org/ncbo/stanford/bean/concept/ClassBean.java
+++ src/java/org/ncbo/stanford/bean/concept/ClassBean.java
@@ -5,7 +5,8 @@ public class ClassBean extends AbstractConceptBean {
 	public String toString() {
 		return "Type: ClassBean " + super.toString();
 	}
+
 	public String toString(String indent) {
-		return "Type: ClassBean "  + super.toString(indent);
+		return "Type: ClassBean " + super.toString(indent);
 	}
 }
