a05ca8235c2cca4d5642f694d3a5d3303b3427aa
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-08-29 01:53:50 +0000

    formatting

--- src/java/org/ncbo/stanford/enumeration/ErrorTypeEnum.java
+++ src/java/org/ncbo/stanford/enumeration/ErrorTypeEnum.java
@@ -4,29 +4,21 @@ package org.ncbo.stanford.enumeration;
  * An Enum to store site-wide errors with codes and default messages
  * 
  * @author Michael Dorf
- *
+ * 
  */
 public enum ErrorTypeEnum {
-	
-	AUTHENTICATION_REQUIRED(
-			"AUTHENTICATION_REQUIRED", 
-			"Access to this resource requires authentication"),
-	INVALID_CREDENTIALS(
-			"INVALID_CREDENTIALS", 
-			"The credentials you supplied are not valid"),
-	ACCESS_DENIED(
-			"ACCESS_DENIED", 
-			"You do not possess sufficient privileges to access this resource"),
-	RUNTIME_ERROR(
-			"RUNTIME_ERROR", 
-			"A runtime error has occurred"),
-	INVALID_FILE(
-			"INVALID_FILE",
-			"The uploaded file is invalid");
-	
-    private final String errorCode;
-    private final String defErrorMessage;
-	
+
+	AUTHENTICATION_REQUIRED("AUTHENTICATION_REQUIRED",
+			"Access to this resource requires authentication"), INVALID_CREDENTIALS(
+			"INVALID_CREDENTIALS", "The credentials you supplied are not valid"), ACCESS_DENIED(
+			"ACCESS_DENIED",
+			"You do not possess sufficient privileges to access this resource"), RUNTIME_ERROR(
+			"RUNTIME_ERROR", "A runtime error has occurred"), INVALID_FILE(
+			"INVALID_FILE", "The uploaded file is invalid");
+
+	private final String errorCode;
+	private final String defErrorMessage;
+
 	private ErrorTypeEnum(String errCode, String defErrMessage) {
 		errorCode = errCode;
 		defErrorMessage = defErrMessage;
