4a8ea44eb1282367ec240d6f5cb72c2ff30ac4da
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-10-10 22:59:53 +0000

    added synonym and preferred name slot values for existing ontologies

--- db/sql/bioportal_db_changes_1006.sql
+++ db/sql/bioportal_db_changes_1006.sql
@@ -8,6 +8,62 @@ ALTER TABLE ncbo_ontology_version_metadata ADD
 
 ALTER TABLE ncbo_ontology_version_metadata AUTO_INCREMENT = 1000;
 
+UPDATE ncbo_ontology_version_metadata 
+SET
+	synonym_slot = 'http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#Synonym', 
+	preferred_name_slot = 'http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#Preferred_Name'
+WHERE
+	ontology_version_id = 13578;
+
+UPDATE ncbo_ontology_version_metadata 
+SET
+	synonym_slot = 'http://ccdb.ucsd.edu/SAO/1.2#synonym' 
+WHERE
+	ontology_version_id = 14391;
+
+UPDATE ncbo_ontology_version_metadata 
+SET
+	synonym_slot = 'Synonym_Name', 
+	preferred_name_slot = 'Preferred_Name'
+WHERE
+	ontology_version_id = 21275;
+
+UPDATE ncbo_ontology_version_metadata 
+SET
+	synonym_slot = 'http://ccdb.ucsd.edu/SAO/1.2#synonym' 
+WHERE
+	ontology_version_id = 28096;
+
+UPDATE ncbo_ontology_version_metadata 
+SET
+	synonym_slot = 'http://revelation.wustl.edu/NPO.owl#synonym' 
+WHERE
+	ontology_version_id = 29531;
+
+UPDATE ncbo_ontology_version_metadata 
+SET
+	synonym_slot = 'http://bioontology.org/projects/ontologies/birnlex#synonyms' 
+WHERE
+	ontology_version_id = 29684;
+
+UPDATE ncbo_ontology_version_metadata 
+SET
+	synonym_slot = 'http://www.bootstrep.eu/ontology/GRO#synonym' 
+WHERE
+	ontology_version_id = 38291;
+
+UPDATE ncbo_ontology_version_metadata 
+SET
+	synonym_slot = ':SYNONYMS' 
+WHERE
+	ontology_version_id = 38563;
+
+UPDATE ncbo_ontology_version_metadata 
+SET
+	preferred_name_slot = 'http://epoch.stanford.edu/ProtocolOntology.owl#label'
+WHERE
+	ontology_version_id = 8056;
+
 CREATE OR REPLACE VIEW `v_ncbo_ontology` AS
 SELECT 
 	ov.id,
