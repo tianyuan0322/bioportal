f9db037a86c33b3ef21af3aa90c8abc900649193
bdai <bdai@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-10-21 19:20:27 +0000

    Minor tweak to update user drop and creation.

--- db/sql/test_db_fixture.sql
+++ db/sql/test_db_fixture.sql
@@ -11,11 +11,11 @@ CREATE DATABASE `bioportal_lexgrid`;
 CREATE DATABASE `bioportal_protege`;
 
 /* Create Users */
-create user 'bioportal_user';
-GRANT SELECT, CREATE, INSERT, UPDATE, DELETE ON bioportal.* TO 'bioportal_user'@'%' IDENTIFIED BY 'bioportal_user';
+create user 'bioportal_user'@'localhost' identified by 'password';
+GRANT SELECT, CREATE, INSERT, UPDATE, DELETE ON bioportal.* TO 'bioportal_user'@'localhost';
 
-create user 'lexgrid_user';
-GRANT SELECT, CREATE, INSERT, UPDATE, DELETE ON bioportal_lexgrid.* TO 'lexgrid_user'@'%' IDENTIFIED BY 'lexgrid_user';
+create user 'lexgrid_user'@'localhost' identified by 'password';
+GRANT SELECT, CREATE, INSERT, UPDATE, DELETE ON bioportal_lexgrid.* TO 'lexgrid_user'@'localhost';
 
-create user 'protege_user';
-GRANT SELECT, CREATE, INSERT, UPDATE, DELETE ON bioportal_protege.* TO 'protege_user'@'%' IDENTIFIED BY 'protege_user';
+create user 'protege_user'@'localhost' identified by 'password';
+GRANT SELECT, CREATE, INSERT, UPDATE, DELETE ON bioportal_protege.* TO 'protege_user'@'localhost';
