a374d7a16ef808cbd77edf3ffd165bfd5555edd8
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-03 06:38:12 +0000

    started LexGrid Lucene implementation

--- classic_to_core_migration/src/java/lucene/manager/impl/LuceneSearchManagerLexGridImpl.java
+++ classic_to_core_migration/src/java/lucene/manager/impl/LuceneSearchManagerLexGridImpl.java
@@ -1,16 +1,93 @@
 package lucene.manager.impl;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.StringTokenizer;
 
 import lucene.manager.LuceneSearchManager;
 import lucene.wrapper.IndexWriterWrapper;
 
+import org.LexGrid.LexBIG.DataModel.Core.CodingSchemeVersionOrTag;
+import org.LexGrid.LexBIG.Impl.LexBIGServiceImpl;
+import org.LexGrid.LexBIG.LexBIGService.CodedNodeSet;
+import org.LexGrid.LexBIG.LexBIGService.LexBIGService;
+import org.LexGrid.LexBIG.Utility.Constructors;
+import org.apache.commons.lang.StringUtils;
+
 public class LuceneSearchManagerLexGridImpl implements LuceneSearchManager {
 
+	private LexBIGService lbs;
+
+	public LuceneSearchManagerLexGridImpl() {
+		lbs = LexBIGServiceImpl.defaultInstance();
+	}
+
 	public void indexOntology(IndexWriterWrapper writer, ResultSet rs)
-			throws SQLException, IOException {
-		// TODO Auto-generated method stub
+			throws Exception {
+		String schemeName = getLexGridCodingSchemeName(rs);
+		CodingSchemeVersionOrTag csvt = getLexGridCodingSchemeVersion(rs);
+		CodedNodeSet codeSet = lbs.getCodingSchemeConcepts(schemeName, csvt);
+		CodedNodeSet designationSet = lbs.getCodingSchemeConcepts(schemeName,
+				csvt);
+		
+	}
+
+	/**
+	 * 
+	 * @param ncboOntology
+	 * @return The LexGrid codingScheme URN string (registered Name)
+	 * @throws SQLException
+	 */
+	private String getLexGridCodingSchemeName(ResultSet rs) throws SQLException {
+		String urnAndVersion = rs.getString("coding_scheme");
+		String urnVersionArray[] = splitUrnAndVersion(urnAndVersion);
+
+		if (urnVersionArray != null && urnVersionArray.length > 0) {
+			return urnVersionArray[0];
+		}
+
+		return (urnVersionArray != null && urnVersionArray.length > 0) ? urnVersionArray[0]
+				: null;
+	}
+
+	/**
+	 * 
+	 * @param ncboOntology
+	 * @return The LexGrid codingScheme URN string (registered Name)
+	 * @throws SQLException
+	 */
+	private CodingSchemeVersionOrTag getLexGridCodingSchemeVersion(ResultSet rs)
+			throws SQLException {
+		String urnAndVersion = rs.getString("coding_scheme");
+		String urnVersionArray[] = splitUrnAndVersion(urnAndVersion);
+
+		return (urnVersionArray != null && urnVersionArray.length > 1) ? Constructors
+				.createCodingSchemeVersionOrTagFromVersion(urnVersionArray[1])
+				: null;
+	}
+
+	/**
+	 * 
+	 * @param urnAndVersion
+	 * @return A string array of length 2, with the first element holding the
+	 *         urn information and the second element holding the version
+	 */
+	private String[] splitUrnAndVersion(String urnAndVersion) {
+		if (StringUtils.isEmpty(urnAndVersion)) {
+			return null;
+		}
+
+		String array[] = { "", "" };
+		StringTokenizer st = new StringTokenizer(urnAndVersion, "|");
+
+		if (st.hasMoreTokens()) {
+			array[0] = st.nextToken();
+		}
+
+		if (st.hasMoreTokens()) {
+			array[1] = st.nextToken();
+		}
+
+		return array;
 	}
 }
