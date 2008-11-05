b9608c87811e2f5f80647ec13498caba30702d3e
kpradip <kpradip@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-05 20:11:59 +0000

    Store the directory of the rrf files, so it could be used by the lexgrid loader

--- src/java/org/ncbo/stanford/util/ontologyfile/compressedfilehandler/impl/RRFCompressedFileHandlerImpl.java
+++ src/java/org/ncbo/stanford/util/ontologyfile/compressedfilehandler/impl/RRFCompressedFileHandlerImpl.java
@@ -6,9 +6,11 @@ package org.ncbo.stanford.util.ontologyfile.compressedfilehandler.impl;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ncbo.stanford.bean.OntologyBean;
+import org.ncbo.stanford.util.constants.ApplicationConstants;
 import org.ncbo.stanford.util.ontologyfile.compressedfilehandler.AbstractCompressedFileHandler;
 
 /**
@@ -23,7 +25,19 @@ public class RRFCompressedFileHandlerImpl extends AbstractCompressedFileHandler 
 	public List<String> handle(File outputFile, OntologyBean ontologyBean)
 			throws FileNotFoundException, IOException {
 		List<String> relevantFiles = super.handle(outputFile, ontologyBean);
-
+		String uploadedFile = relevantFiles.get(0);
+        relevantFiles = new ArrayList<String>(0);
+        relevantFiles.add(uploadedFile);
+        int indexOfLastFileSeparator= uploadedFile.toString().lastIndexOf(System
+                        .getProperty("file.separator"));
+        if (indexOfLastFileSeparator== -1) {
+            indexOfLastFileSeparator=0;
+        }
+        String directoryName= uploadedFile.substring(0, indexOfLastFileSeparator);
+        relevantFiles.add(directoryName);
+                
 		return relevantFiles;
 	}
+	
+
 }
