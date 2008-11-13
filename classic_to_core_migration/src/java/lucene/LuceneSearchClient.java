4a582159f58acebf5cad91c22b5e8a5bd36d4cc9
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-13 01:44:14 +0000

    added Galen ontology

--- classic_to_core_migration/src/java/lucene/LuceneSearchClient.java
+++ classic_to_core_migration/src/java/lucene/LuceneSearchClient.java
@@ -17,6 +17,7 @@ public class LuceneSearchClient {
 //			search.indexOntology(1070);
 //			search.indexOntology(1032);
 //			search.indexOntology(1058);
+			search.indexOntology(1055);
 //			search.indexOntology(1056);
 //			search.indexOntology(1089);
 //			search.indexOntology(1057);
@@ -25,21 +26,22 @@ public class LuceneSearchClient {
 //			search.indexOntology(1054);
 //			search.indexAllOntologies();
 
-//			System.out.println("Searching...");
-			search.executeQuery("Blood and Blood Products", true);
+			System.out.println("Searching...");
+			search.executeQuery("blood", true);
 
 			Collection<Integer> ontologyIds = new ArrayList<Integer>();
-//			ontologyIds.add(1070); // Biological Process (GO) - OBO
+			ontologyIds.add(1070); // Biological Process (GO) - OBO
 //			ontologyIds.add(1032); // NCIT - OWL
 			ontologyIds.add(1058); // SNP-Ontology - OWL
+			ontologyIds.add(1055); // Galen - OWL
 			ontologyIds.add(1056); // Basic Vertebrate Anatomy - OWL
-//			ontologyIds.add(1089); // BIRNLex - OWL
-//			ontologyIds.add(1057); // RadLex - FRAMES
+			ontologyIds.add(1089); // BIRNLex - OWL
+			ontologyIds.add(1057); // RadLex - FRAMES
 			ontologyIds.add(1090); // Amphibian gross anatomy - OBO
-//			ontologyIds.add(1053); // FMA - OWL
+			ontologyIds.add(1053); // FMA - OWL
 			ontologyIds.add(1054); // Amino Acid - OWL
-			System.out.println("Searching...");
-//			search.executeQuery("muscle", ontologyIds, false);
+//			System.out.println("Searching...");
+//			search.executeQuery("blood", ontologyIds, false);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
