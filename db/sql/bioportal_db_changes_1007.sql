147affdb245b635b73d1a9f8061d7475c6165f5d
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-14 22:05:35 +0000

    db changes for 1007 build

---
+++ db/sql/bioportal_db_changes_1007.sql
@@ -0,0 +1,8 @@
+update ncbo_ontology_version_metadata set description = ''
+where display_label = '';
+
+update ncbo_ontology_version_metadata set description = ''
+where display_label = '';
+
+update ncbo_ontology_version_metadata set description = ''
+where display_label = '';
