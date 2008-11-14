2511e73da823e4122ab5ca4c738be9c5e696fc45
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-14 23:45:23 +0000

    added utility methods

--- src/java/org/ncbo/stanford/util/ontologyfile/pathhandler/AbstractFilePathHandler.java
+++ src/java/org/ncbo/stanford/util/ontologyfile/pathhandler/AbstractFilePathHandler.java
@@ -1,6 +1,11 @@
 package org.ncbo.stanford.util.ontologyfile.pathhandler;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.ncbo.stanford.bean.OntologyBean;
 import org.ncbo.stanford.util.MessageUtils;
@@ -34,7 +39,7 @@ public abstract class AbstractFilePathHandler implements FilePathHandler {
 	public static boolean deleteDirectory(File path) {
 		if (path.exists()) {
 			File[] files = path.listFiles();
-			
+
 			for (int i = 0; i < files.length; i++) {
 				if (files[i].isDirectory()) {
 					deleteDirectory(files[i]);
@@ -43,7 +48,103 @@ public abstract class AbstractFilePathHandler implements FilePathHandler {
 				}
 			}
 		}
-		
+
 		return path.delete();
+	}
+
+	// Deletes all files and subdirectories under dir.
+	// Returns true if all deletions were successful.
+	// If a deletion fails, the method stops attempting to delete and returns
+	// false.
+	public static boolean deleteDirRecursive(File dir) {
+		if (dir.isDirectory()) {
+			String[] children = dir.list();
+			for (int i = 0; i < children.length; i++) {
+				boolean success = deleteDirRecursive(new File(dir, children[i]));
+				
+				if (!success) {
+					return false;
+				}
+			}
+		}
+
+		// The directory is now empty so delete it
+		return dir.delete();
+	}
+
+	/**
+	 * Copies all files under srcDir to dstDir. If dstDir does not exist, it
+	 * will be created.
+	 * 
+	 * @param srcDir
+	 * @param dstDir
+	 * @throws IOException
+	 */
+	public static void copyAllFiles(File srcDir, File dstDir)
+			throws IOException {
+		if (srcDir.isDirectory()) {
+			String[] children = srcDir.list();
+
+			if (!dstDir.exists()) {
+				dstDir.mkdirs();
+			}
+
+			for (int i = 0; i < children.length; i++) {
+				File f = new File(srcDir, children[i]);
+
+				if (f.isFile()) {
+					copyFile(f, new File(dstDir + "\\" + f.getName()));
+				}
+			}
+		}
+	}
+
+	/**
+	 * Copies all files under srcDir to dstDir. If dstDir does not exist, it
+	 * will be created.
+	 * 
+	 * @param srcDir
+	 * @param dstDir
+	 * @throws IOException
+	 */
+	public static void copyDirectory(File srcDir, File dstDir)
+			throws IOException {
+		if (srcDir.isDirectory()) {
+			if (!dstDir.exists()) {
+				dstDir.mkdirs();
+			}
+
+			String[] children = srcDir.list();
+
+			for (int i = 0; i < children.length; i++) {
+				copyDirectory(new File(srcDir, children[i]), new File(dstDir,
+						children[i]));
+			}
+		} else {
+			copyFile(srcDir, dstDir);
+		}
+	}
+
+	/**
+	 * Copies src file to dst file. If the dst file does not exist, it is
+	 * created
+	 * 
+	 * @param src
+	 * @param dst
+	 */
+	public static void copyFile(File src, File dst) throws IOException {
+		InputStream in = new FileInputStream(src);
+		OutputStream out = new FileOutputStream(dst);
+
+		// Transfer bytes from in to out
+		byte[] buf = new byte[1024];
+		int len = 0;
+
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+
+		in.close();
+		out.close();
 	}
 }
