244f434ce496753e58c5bfc6b2c7d80382826d9b
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-19 19:37:08 +0000

    changed to new search results bean

--- src/java/org/ncbo/stanford/bean/search/SearchResultListBean.java
+++ src/java/org/ncbo/stanford/bean/search/SearchResultListBean.java
@@ -1,8 +1,10 @@
 package org.ncbo.stanford.bean.search;
 
+import lucene.bean.LuceneSearchBean;
+
 import org.ncbo.stanford.util.paginator.impl.PaginatableList;
 
-public class SearchResultListBean extends PaginatableList<SearchResultBean> {
+public class SearchResultListBean extends PaginatableList<LuceneSearchBean> {
 
 	/**
 	 * 
