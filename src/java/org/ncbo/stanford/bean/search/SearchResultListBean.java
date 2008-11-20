ce288f3776e1629ba3db3549d7ddee83c0b36ba4
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-20 01:22:08 +0000

    added constructors from parent

--- src/java/org/ncbo/stanford/bean/search/SearchResultListBean.java
+++ src/java/org/ncbo/stanford/bean/search/SearchResultListBean.java
@@ -1,5 +1,7 @@
 package org.ncbo.stanford.bean.search;
 
+import java.util.Collection;
+
 import lucene.bean.LuceneSearchBean;
 
 import org.ncbo.stanford.util.paginator.impl.PaginatableList;
@@ -11,4 +13,24 @@ public class SearchResultListBean extends PaginatableList<LuceneSearchBean> {
 	 */
 	private static final long serialVersionUID = 1067248571020887697L;
 
+	/**
+	 * 
+	 */
+	public SearchResultListBean() {
+		super();
+	}
+
+	/**
+	 * @param c
+	 */
+	public SearchResultListBean(Collection<LuceneSearchBean> c) {
+		super(c);
+	}
+
+	/**
+	 * @param initialCapacity
+	 */
+	public SearchResultListBean(int initialCapacity) {
+		super(initialCapacity);
+	}
 }
