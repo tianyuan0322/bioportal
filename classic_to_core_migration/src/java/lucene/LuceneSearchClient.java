54414fba6abf9c6a469d919eb5c2155163fc27f6
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-24 19:51:10 +0000

    removed old packaging

--- classic_to_core_migration/src/java/lucene/LuceneSearchClient.java
+++ classic_to_core_migration/src/java/lucene/LuceneSearchClient.java
@@ -3,18 +3,24 @@ package lucene;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import lucene.bean.LuceneSearchBean;
-
+import org.apache.lucene.search.Query;
+import org.ncbo.stanford.bean.search.SearchBean;
 import org.ncbo.stanford.bean.search.SearchResultListBean;
+import org.ncbo.stanford.service.search.SearchService;
+import org.ncbo.stanford.service.search.impl.SearchServiceImpl;
+import org.ncbo.stanford.util.cache.expiration.system.ExpirationSystem;
+import org.ncbo.stanford.util.cache.expiration.system.impl.UpdatingHashbeltExpirationSystem;
 import org.ncbo.stanford.util.constants.ApplicationConstants;
 import org.ncbo.stanford.util.paginator.Paginator;
 import org.ncbo.stanford.util.paginator.impl.Page;
 import org.ncbo.stanford.util.paginator.impl.PaginatorImpl;
 
-import com.sshtools.j2ssh.io.UnsignedInteger64;
 import com.thoughtworks.xstream.XStream;
 
 public class LuceneSearchClient {
+
+//	private static ExpirationSystem<Integer, SearchResultListBean> resultsCache = new UpdatingHashbeltExpirationSystem<Integer, SearchResultListBean>();
+
 	public static void main(String[] args) {
 		try {
 			// contents:"blood" && (ontologyId:1116 || ontologyId:1053)
@@ -23,13 +29,12 @@ public class LuceneSearchClient {
 			// File("/apps/bmir.apps/bioportal_resources/lucene", "lock");
 			// System.out.println(file.getAbsolutePath());
 
-			LuceneSearch search = LuceneSearch.getInstance();
+			SearchService search = SearchServiceImpl.getInstance();
 			// search.indexOntology(1121);
 
 			// search.backupIndex();
 			// search.indexOntology(1070);
 			// search.indexOntology(1032);
-			// search.indexOntology(1058);
 			// search.indexOntology(1055);
 			// search.indexOntology(1056);
 			// search.indexOntology(1089);
@@ -39,50 +44,69 @@ public class LuceneSearchClient {
 			// search.indexOntology(1054);
 			// search.indexOntology(1104);
 			// search.indexOntology(1066);
+			// search.indexOntology(1058);
 			// search.indexOntology(1047);
 
 			// search.indexAllOntologies();
-			Collection<Integer> ontologyIds = new ArrayList<Integer>();
+			Collection<Integer> ontologyIds = new ArrayList<Integer>(0);
 
-			ontologyIds.add(1070); // Biological Process (GO) - OBO
-			ontologyIds.add(1102); // Cellular Component (GO) - OBO
-			ontologyIds.add(1032); // NCIT - OWL
-			ontologyIds.add(1058); // SNP-Ontology - OWL
-			ontologyIds.add(1055); // Galen - OWL
-			ontologyIds.add(1056); // Basic Vertebrate Anatomy - OWL
-			ontologyIds.add(1089); // BIRNLex - OWL
-			ontologyIds.add(1057); // RadLex - FRAMES
-			ontologyIds.add(1090); // Amphibian gross anatomy - OBO
-			ontologyIds.add(1053); // FMA - OWL
-			ontologyIds.add(1054); // Amino Acid - OWL
-			ontologyIds.add(1104); // Biomedical Resource Ontology (BRO) - OWL
-			ontologyIds.add(1066); // Unit - OBO
-			ontologyIds.add(1047); // Cereal plant development - OBO
+			// ontologyIds.add(1070); // Biological Process (GO) - OBO
+			// ontologyIds.add(1102); // Cellular Component (GO) - OBO
+			// ontologyIds.add(1032); // NCIT - OWL
+			// ontologyIds.add(1055); // Galen - OWL
+			// ontologyIds.add(1056); // Basic Vertebrate Anatomy - OWL
+			// ontologyIds.add(1089); // BIRNLex - OWL
+			// ontologyIds.add(1057); // RadLex - FRAMES
+			// ontologyIds.add(1090); // Amphibian gross anatomy - OBO
+			// ontologyIds.add(1053); // FMA - OWL
+			// ontologyIds.add(1054); // Amino Acid - OWL
+			// ontologyIds.add(1104); // Biomedical Resource Ontology (BRO) -
+			// OWL
+			// ontologyIds.add(1066); // Unit - OBO
+			// ontologyIds.add(1058); // SNP-Ontology - OWL
+			// ontologyIds.add(1047); // Cereal plant development - OBO
 
 			System.out.println("Searching...");
-			SearchResultListBean results = search.executeQuery("Cell",
-					ontologyIds, false, true);
-
-			Paginator<LuceneSearchBean> p = new PaginatorImpl<LuceneSearchBean>(
+			Query q = search.generateLuceneSearchQuery(ontologyIds, "cell", true, false);
+			
+			
+			long start = System.currentTimeMillis();
+			SearchResultListBean results = search.executeQuery(q);
+			long stop = System.currentTimeMillis();
+			System.out.println("Query Time: " + (double) (stop - start) / 1000
+					+ " seconds.");
+			
+//			resultsCache.put(q.hashCode(), results);
+			
+			
+			start = System.currentTimeMillis();
+//			results = resultsCache.get(q.hashCode());
+			stop = System.currentTimeMillis();
+			System.out.println("Cache Time: " + (double) (stop - start) / 1000
+					+ " seconds." + " " + q.hashCode());
+			
+			
+			Paginator<SearchBean> p = new PaginatorImpl<SearchBean>(
 					results);
-			Page<LuceneSearchBean> page = p.getAll();
+			Page<SearchBean> page = p.getAll();
 
-			String xmlHeader = ApplicationConstants.XML_DECLARATION + "\n";
-			XStream xstream = new XStream();
-			xstream.setMode(XStream.NO_REFERENCES);
-			// xstream.alias("paginatableList", PaginatableList.class);
-			xstream.alias("page", Page.class);
-			xstream.alias("luceneSearchBean", LuceneSearchBean.class);
-			// xstream.addDefaultImplementation(ArrayList.class, SearchResultListBean.class);
-			// xstream.addImplicitCollection(Page.class, "contents", List.class);
-
-			// ArrayList l = new ArrayList();
-			// l.add("hello");
-			// l.add("world");
-
-//			System.out.println(xmlHeader + xstream.toXML(page));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	public String getXML(Page<SearchBean> page) {
+		String xmlHeader = ApplicationConstants.XML_DECLARATION + "\n";
+		XStream xstream = new XStream();
+		xstream.setMode(XStream.NO_REFERENCES);
+		// xstream.alias("paginatableList", PaginatableList.class);
+		xstream.alias("page", Page.class);
+		xstream.alias("luceneSearchBean", SearchBean.class);
+		// xstream.addDefaultImplementation(ArrayList.class,
+		// SearchResultListBean.class);
+		// xstream.addImplicitCollection(Page.class, "contents", List.class);
+
+		return xmlHeader + xstream.toXML(page);
+	}
+
 }
