1dd65ed71254beade61c54bcfe9635cb1518c16e
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-24 15:47:30 +0000

    moved managers into subpackages

---
+++ src/java/org/ncbo/stanford/manager/load/impl/OntologyLoadManagerProtegeImpl.java
@@ -0,0 +1,220 @@
+package org.ncbo.stanford.manager.load.impl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ncbo.stanford.bean.OntologyBean;
+import org.ncbo.stanford.manager.AbstractOntologyManagerProtege;
+import org.ncbo.stanford.manager.load.OntologyLoadManager;
+import org.ncbo.stanford.util.constants.ApplicationConstants;
+
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.query.api.QueryApi;
+import edu.stanford.smi.protege.query.api.QueryConfiguration;
+import edu.stanford.smi.protege.query.indexer.IndexMechanism;
+import edu.stanford.smi.protege.storage.database.DatabaseKnowledgeBaseFactory;
+import edu.stanford.smi.protege.util.ApplicationProperties;
+import edu.stanford.smi.protege.util.PropertyList;
+import edu.stanford.smi.protegex.owl.ProtegeOWL;
+import edu.stanford.smi.protegex.owl.database.CreateOWLDatabaseFromFileProjectPlugin;
+import edu.stanford.smi.protegex.owl.database.OWLDatabaseKnowledgeBaseFactory;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.factory.AlreadyImportedException;
+import edu.stanford.smi.protegex.owl.model.factory.FactoryUtils;
+
+/**
+ * Provides the functionality to load an ontology into the Protege back-end
+ * 
+ * @author Michael Dorf
+ * 
+ */
+public class OntologyLoadManagerProtegeImpl extends
+		AbstractOntologyManagerProtege implements OntologyLoadManager {
+
+	private static final Log log = LogFactory
+			.getLog(OntologyLoadManagerProtegeImpl.class);
+
+	/**
+	 * Loads the specified ontology into the BioPortal repository. If the
+	 * specified ontology identifier already exists, overwrite the ontology with
+	 * the new ontology file.
+	 * 
+	 * @param ontologyId
+	 *            the ontology id for the specified ontology file.
+	 * @param ontologyFile
+	 *            the file containing the ontlogy to be loaded.
+	 * 
+	 * @exception FileNotFoundException
+	 *                the ontology file to be loaded was not found.
+	 * @exception Exception
+	 *                catch all for all other ontlogy file load errors.
+	 */
+	@SuppressWarnings("unchecked")
+	public void loadOntology(URI ontologyUri, OntologyBean ob) throws Exception {
+		File ontologyFile = new File(ontologyUri.getPath());
+		String filePath = ontologyUri.getPath();
+
+		if (ontologyFile == null) {
+			log.error("Missing ontology file to load: " + filePath);
+			throw new FileNotFoundException("Missing ontology file to load: "
+					+ filePath);
+		}
+
+		log.debug("Loading ontology file: " + ontologyFile.getName());
+
+		// If the ontology file is small, use the fast non-streaming Protege
+		// load code.
+		List errors = new ArrayList();
+		Integer ontologyId = ob.getId();
+		String tableName = getTableName(ontologyId);
+
+		// Clear knowledgebase cache for this item
+		protegeKnowledgeBases.remove(ontologyId);
+
+		Project dbProject = null;
+
+		if (ob.getFormat().contains(ApplicationConstants.FORMAT_OWL)) {
+			if (ontologyFile.length() < protegeBigFileThreshold) {
+				log.debug("Using non-streaming mode. Ontology: "
+						+ ob.getDisplayLabel() + " (" + ob.getId() + ")");
+
+				ApplicationProperties.setBoolean(
+						"protege.owl.parser.convert.file.merge.mode", true);
+
+				OWLModel owlModel = ProtegeOWL
+						.createJenaOWLModelFromURI(ontologyUri.toString());
+				Project fileProject = owlModel.getProject();
+
+				OWLDatabaseKnowledgeBaseFactory factory = new OWLDatabaseKnowledgeBaseFactory();
+				PropertyList sources = PropertyList.create(fileProject
+						.getInternalProjectKnowledgeBase());
+
+				OWLDatabaseKnowledgeBaseFactory.setSources(sources,
+						protegeJdbcDriver, protegeJdbcUrl, tableName,
+						protegeJdbcUsername, protegeJdbcPassword);
+
+				factory.saveKnowledgeBase(fileProject.getKnowledgeBase(),
+						sources, errors);
+				fileProject.dispose();
+
+				dbProject = Project.createBuildProject(factory, errors);
+				OWLDatabaseKnowledgeBaseFactory.setSources(dbProject
+						.getSources(), protegeJdbcDriver, protegeJdbcUrl,
+						tableName, protegeJdbcUsername, protegeJdbcPassword);
+
+				dbProject.createDomainKnowledgeBase(factory, errors, true);
+
+				try {
+					FactoryUtils.writeOntologyAndPrefixInfo(
+							(OWLModel) dbProject.getKnowledgeBase(), errors);
+				} catch (AlreadyImportedException e) {
+					e.printStackTrace();
+					log
+							.error(
+									"Error at loadOntology: Already Imported Exception",
+									e);
+				}
+			} else {
+				// If the ontology file is big, use the streaming Protege load
+				// approach.
+				log.debug("Using streaming mode. Ontology: "
+						+ ob.getDisplayLabel() + " (" + ob.getId() + ")");
+				CreateOWLDatabaseFromFileProjectPlugin creator = new CreateOWLDatabaseFromFileProjectPlugin();
+				creator
+						.setKnowledgeBaseFactory(new OWLDatabaseKnowledgeBaseFactory());
+				creator.setDriver(protegeJdbcDriver);
+				creator.setURL(protegeJdbcUrl);
+				creator.setTable(tableName);
+				creator.setUsername(protegeJdbcUsername);
+				creator.setPassword(protegeJdbcPassword);
+				creator.setOntologyInputSource(ontologyUri);
+				creator.setUseExistingSources(true);
+				creator.setMergeImportMode(true);
+				dbProject = creator.createProject();
+				dbProject.save(errors);
+			}
+		} else {
+			// PROTEGE .pprj format
+			Project fileProject = Project.loadProjectFromFile(filePath, errors);
+			DatabaseKnowledgeBaseFactory factory = new DatabaseKnowledgeBaseFactory();
+			PropertyList sources = PropertyList.create(fileProject
+					.getInternalProjectKnowledgeBase());
+			DatabaseKnowledgeBaseFactory.setSources(sources, protegeJdbcDriver,
+					protegeJdbcUrl, tableName, protegeJdbcUsername,
+					protegeJdbcPassword);
+			factory.saveKnowledgeBase(fileProject.getKnowledgeBase(), sources,
+					errors);
+			fileProject.dispose();
+
+			dbProject = Project.createNewProject(factory, errors);
+			DatabaseKnowledgeBaseFactory.setSources(dbProject.getSources(),
+					protegeJdbcDriver, protegeJdbcUrl, tableName,
+					protegeJdbcUsername, protegeJdbcPassword);
+			dbProject.createDomainKnowledgeBase(factory, errors, true);
+			dbProject.save(errors);
+		}
+
+		// If errors are found during the load, log the errors and throw an
+		// exception.
+		if (errors.size() > 0) {
+			log.error(errors);
+			throw new Exception("Error during loading "
+					+ ontologyUri.toString());
+		}
+
+		if (dbProject != null) {
+			createIndex(dbProject.getKnowledgeBase(), ob);
+			dbProject.dispose();
+		}
+	}
+
+	/**
+	 * Creates a Lucene index for a given ontology version. The ontology record
+	 * must exist.
+	 * 
+	 * @param ob -
+	 *            populated ontology bean.
+	 */
+	@SuppressWarnings("unchecked")
+	public void indexOntology(OntologyBean ob) {
+		createIndex(getKnowledgeBase(ob), ob);
+	}
+
+	public void cleanup(OntologyBean ontologyBean) throws Exception {
+		// TODO: fill in
+	}
+
+	private void createIndex(KnowledgeBase kb, OntologyBean ob) {
+		QueryConfiguration config = new QueryConfiguration(kb);
+		config.setBaseIndexPath(getIndexPath(ob));
+		Set<Slot> searchableSlots = config.getSearchableSlots();
+
+		try { // this should unneccessary but it doesn't hurt.
+			Slot synonymSlot = getSynonymSlot(kb, ob.getSynonymSlot());
+
+			if (synonymSlot != null) {
+				searchableSlots.add(synonymSlot);
+			}
+
+			searchableSlots.add(getPreferredNameSlot(kb, ob
+					.getPreferredNameSlot()));
+			config.setSearchableSlots(searchableSlots);
+		} catch (Throwable t) {
+		}
+
+		config.setIndexers(EnumSet.of(IndexMechanism.STANDARD_INDICIES));
+
+		QueryApi api = new QueryApi(kb);
+		installLuceneIndexMechanism(api, ob);
+		api.index(config);
+	}
+}
---
+++ src/java/org/ncbo/stanford/manager/retrieval/impl/OntologyRetrievalManagerProtegeImpl.java
@@ -0,0 +1,444 @@
+package org.ncbo.stanford.manager.retrieval.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ncbo.stanford.bean.concept.ClassBean;
+import org.ncbo.stanford.domain.custom.entity.VNcboOntology;
+import org.ncbo.stanford.manager.AbstractOntologyManagerProtege;
+import org.ncbo.stanford.manager.retrieval.OntologyRetrievalManager;
+import org.ncbo.stanford.util.constants.ApplicationConstants;
+
+import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.Instance;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.ModelUtilities;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protegex.owl.model.NamespaceUtil;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
+import edu.stanford.smi.protegex.owl.model.RDFResource;
+import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
+
+/**
+ * A default implementation to OntologyRetrievalManager interface designed to
+ * provide an abstraction layer to ontology and concept retrieval operations.
+ * The service layer will consume this interface instead of directly calling a
+ * specific implementation (i.e. LexGrid, Protege etc.). Do not use this class
+ * directly in upper layers.
+ * 
+ * 
+ * @author Michael Dorf
+ */
+@SuppressWarnings("unchecked")
+public class OntologyRetrievalManagerProtegeImpl extends
+		AbstractOntologyManagerProtege implements OntologyRetrievalManager {
+
+	@SuppressWarnings("unused")
+	private static final Log log = LogFactory
+			.getLog(OntologyRetrievalManagerProtegeImpl.class);
+
+	/**
+	 * Default Constructor
+	 */
+	public OntologyRetrievalManagerProtegeImpl() {
+	}
+
+	//
+	// Concept methods
+	//
+
+	/**
+	 * Get the root concept for the specified ontology.
+	 */
+	public ClassBean findRootConcept(VNcboOntology ontologyVersion) {
+		KnowledgeBase kb = getKnowledgeBase(ontologyVersion);
+
+		// Get all root nodes associated with this ontology. Then iterate
+		// through the collection, returning the first one.
+		Cls oThing = kb.getRootCls();
+
+		if (oThing != null) {
+			return createClassBean(oThing, true);
+		}
+
+		return null;
+	}
+
+	public ClassBean findConcept(VNcboOntology ontologyVersion, String conceptId) {
+		KnowledgeBase kb = getKnowledgeBase(ontologyVersion);
+
+		Cls owlClass = getCls(conceptId, kb);
+
+		if (owlClass != null) {
+			return createClassBean(owlClass, true);
+		}
+
+		return null;
+	}
+
+	public ClassBean findPathFromRoot(VNcboOntology ontologyVersion,
+			String conceptId, boolean light) {
+		KnowledgeBase kb = getKnowledgeBase(ontologyVersion);
+
+		Cls cls = getCls(conceptId, kb);
+		Collection nodes = ModelUtilities.getPathToRoot(cls);
+
+		return buildPath(nodes, light);
+	}
+
+	public ClassBean findParent(String id, Integer ontologyVersionId) {
+		throw new UnsupportedOperationException();
+	}
+
+	public ArrayList<ClassBean> findChildren(String id,
+			Integer ontologyVersionId) {
+		throw new UnsupportedOperationException();
+	}
+
+/*	public List<SearchResultBean> findConceptNameExact(
+			List<VNcboOntology> ontologyVersions, String query,
+			boolean includeObsolete, int maxToReturn) {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<SearchResultBean> findConceptNameStartsWith(
+			List<VNcboOntology> ontologyVersions, String query,
+			boolean includeObsolete, int maxToReturn) {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<SearchResultBean> findConceptNameContains(
+			List<VNcboOntology> ontologyVersions, String query,
+			boolean includeObsolete, int maxToReturn) {
+		ArrayList<SearchResultBean> results = new ArrayList<SearchResultBean>();
+
+		for (VNcboOntology ontologyVersion : ontologyVersions) {
+			SearchResultBean srb = new SearchResultBean();
+			srb.setOntologyVersionId(ontologyVersion.getId());
+			KnowledgeBase kb = getKnowledgeBase(ontologyVersion);
+
+			Set<Slot> slots = new HashSet<Slot>();
+
+			slots.add(getPreferredNameSlot(kb, ontologyVersion
+					.getPreferredNameSlot()));
+			slots.add(kb.getNameSlot());
+
+			Slot synonymSlot = getSynonymSlot(kb, ontologyVersion
+					.getSynonymSlot());
+
+			if (synonymSlot != null) {
+				slots.add(synonymSlot);
+			}
+
+			Collection<Frame> frames = kb
+					.executeQuery(new LuceneOwnSlotValueQuery(slots, query));
+
+			int i = 0;
+
+			for (Frame frame : frames) {
+				if (i++ >= maxToReturn) {
+					break;
+				}
+
+				if (frame instanceof Cls && frame.isVisible()
+						&& !frame.isSystem()) {
+					Cls cls = (Cls) frame;
+					srb.addName(createLightClassBean(cls));
+				}
+			}
+
+			results.add(srb);
+		}
+
+		return results;
+	}
+
+	public ArrayList<SearchResultBean> findConceptPropertyContains(
+			List<VNcboOntology> ontologyVersions, String query,
+			boolean includeObsolete, int maxToReturn) {
+		ArrayList<SearchResultBean> results = new ArrayList<SearchResultBean>();
+
+		for (VNcboOntology ontologyVersion : ontologyVersions) {
+			SearchResultBean srb = new SearchResultBean();
+			srb.setOntologyVersionId(ontologyVersion.getId());
+
+			KnowledgeBase kb = getKnowledgeBase(ontologyVersion);
+			Collection<Frame> frames = kb
+					.executeQuery(new LuceneOwnSlotValueQuery(
+							new HashSet<Slot>(), "*" + query + "*"));
+			int i = 0;
+
+			for (Frame frame : frames) {
+				if (i++ >= maxToReturn) {
+					break;
+				}
+
+				if (frame instanceof Cls && frame.isVisible()
+						&& !frame.isSystem()) {
+					Cls cls = (Cls) frame;
+					srb.addProperty(createLightClassBean(cls));
+				}
+			}
+
+			results.add(srb);
+		}
+
+		return results;
+	}
+*/
+	//
+	// Non interface methods
+	//
+
+	//
+	// Private methods
+	//
+
+	// This is to remove the URI reference that is used by protege for IDs
+	private String getId(Cls node) {
+		if (node instanceof RDFResource) {
+			RDFResource rdfNode = (RDFResource) node;
+			return NamespaceUtil.getPrefixedName(rdfNode.getOWLModel(), rdfNode
+					.getName());
+		} else {
+			return node.getName();
+		}
+	}
+
+	private Cls getCls(String conceptId, KnowledgeBase kb) {
+		Cls cls = null;
+
+		if (kb instanceof OWLModel) {
+			cls = ((OWLModel) kb).getRDFSNamedClass(conceptId);
+		} else {
+			cls = kb.getCls(conceptId);
+		}
+
+		return cls;
+	}
+
+	private ClassBean buildPath(Collection nodes, boolean light) {
+		ClassBean rootBean = null;
+		ClassBean currentBean = null;
+		Cls previousNode = null;
+
+		for (Object nodeObj : nodes) {
+			ClassBean clsBean = new ClassBean();
+			Cls node = (Cls) nodeObj;
+			clsBean.setId(getId(node));
+
+			if (currentBean != null) {
+				if (light) {
+					Set beanSet = new HashSet();
+					beanSet.add(clsBean);
+					currentBean.addRelation(ApplicationConstants.SUB_CLASS,
+							new ArrayList(beanSet));
+				} else {
+					List<ClassBean> siblings = convertLightBeans(getUniqueClasses(previousNode
+							.getDirectSubclasses()));
+
+					for (ClassBean sibling : siblings) {
+						if (sibling.getId().equals(clsBean.getId())) {
+							clsBean = sibling;
+						}
+					}
+
+					currentBean.addRelation(ApplicationConstants.SUB_CLASS,
+							siblings);
+				}
+			} else {
+				rootBean = clsBean;
+			}
+
+			previousNode = node;
+			currentBean = clsBean;
+		}
+
+		return rootBean;
+	}
+
+	private List<ClassBean> convertLightBeans(Collection<Cls> protegeClses) {
+		List<ClassBean> beans = new ArrayList<ClassBean>();
+
+		for (Cls cls : protegeClses) {
+			if (cls.isVisible())
+				beans.add(createLightClassBean(cls));
+		}
+
+		return beans;
+	}
+
+	private List<ClassBean> convertClasses(Collection<Cls> protegeClses,
+			boolean recursive) {
+		List<ClassBean> beans = new ArrayList<ClassBean>();
+
+		for (Cls cls : protegeClses) {
+			if (cls.isVisible())
+				beans.add(createClassBean(cls, recursive));
+		}
+
+		return beans;
+	}
+
+	/*
+	 * protected String getLabel(Frame node) { String label = null;
+	 * 
+	 * if (node instanceof RDFResource) { RDFResource rs = (RDFResource) node;
+	 * Collection labels = rs.getLabels();
+	 * 
+	 * if (labels == null || labels.isEmpty()) { label = node.getName(); } else {
+	 * label = CollectionUtilities.getFirstItem(labels).toString(); } } else {
+	 * label = node.getName(); }
+	 * 
+	 * return label; }
+	 */
+
+	private ClassBean createLightClassBean(Cls cls) {
+		ClassBean classBean = new ClassBean();
+		classBean.setId(getId(cls));
+
+		classBean.setLabel(cls.getBrowserText());
+		classBean.addRelation(ApplicationConstants.CHILD_COUNT,
+				getUniqueClasses(cls.getDirectSubclasses()).size());
+
+		return classBean;
+	}
+
+	private ClassBean createClassBean(Cls cls, boolean recursive) {
+		boolean isOwl = cls.getKnowledgeBase() instanceof OWLModel;
+
+		ClassBean classBean = new ClassBean();
+		classBean.setId(getId(cls));
+
+		classBean.setLabel(cls.getBrowserText());
+
+		// add properties
+		Collection<Slot> slots;
+
+		if (isOwl && cls instanceof RDFSNamedClass) {
+			slots = ((RDFSNamedClass) cls).getPossibleRDFProperties();
+		} else {
+			slots = cls.getOwnSlots();
+		}
+
+		classBean.addRelations(convertProperties(cls, slots, isOwl));
+
+		// add subclasses
+		// if OWLNamedClass, then use getNamedSubclasses/Superclasses,
+		// else use getDirectSubclasses/Superclasses (cast to
+		// Collection<Cls>)
+		List<Cls> subclasses = null;
+		List<Cls> superclasses = null;
+
+		if (cls instanceof OWLNamedClass) {
+			subclasses = getUniqueClasses(((OWLNamedClass) cls)
+					.getNamedSubclasses(false));
+
+			OWLModel owlModel = (OWLModel) cls.getKnowledgeBase();
+
+			if (cls.equals(owlModel.getOWLThingClass())) {
+				Iterator<Cls> it = subclasses.iterator();
+
+				while (it.hasNext()) {
+					Cls subclass = it.next();
+
+					if (subclass.isSystem()) {
+						it.remove();
+					}
+				}
+			}
+		} else {
+			subclasses = getUniqueClasses(cls.getDirectSubclasses());
+		}
+
+		classBean.addRelation(ApplicationConstants.CHILD_COUNT, subclasses
+				.size());
+
+		if (recursive) {
+			classBean.addRelation(ApplicationConstants.SUB_CLASS,
+					convertClasses(subclasses, false));
+
+			// add superclasses
+			if (cls instanceof OWLNamedClass) {
+				superclasses = getUniqueClasses(((OWLNamedClass) cls)
+						.getNamedSuperclasses(false));
+			} else {
+				superclasses = getUniqueClasses(cls.getDirectSuperclasses());
+			}
+
+			classBean.addRelation(ApplicationConstants.SUPER_CLASS,
+					convertClasses(superclasses, false));
+		}
+
+		// add RDF type
+		if (cls instanceof OWLNamedClass) {
+			classBean.addRelation(ApplicationConstants.RDF_TYPE,
+					convertClasses(getUniqueClasses(((OWLNamedClass) cls)
+							.getRDFTypes()), false));
+		}
+
+		return classBean;
+	}
+
+	private List getUniqueClasses(Collection classes) {
+		if (classes != null) {
+			Set c = new HashSet(classes);
+			return new ArrayList(c);
+		}
+
+		return Collections.emptyList();
+	}
+
+	/**
+	 * Converts collection of slots into a string representation of values
+	 * 
+	 * @param slots
+	 * @return
+	 */
+	private HashMap<String, List<String>> convertProperties(Cls concept,
+			Collection<Slot> slots, boolean isOwl) {
+		HashMap<String, List<String>> bpProps = new HashMap<String, List<String>>();
+		ArrayList<String> bpPropVals = new ArrayList<String>();
+
+		// add properties
+		for (Slot slot : slots) {
+			Collection classes = (isOwl && slot instanceof RDFProperty) ? ((OWLNamedClass) concept)
+					.getPropertyValues((RDFProperty) slot)
+					: concept.getOwnSlotValues(slot);
+			List vals = getUniqueClasses(classes);
+
+			if (vals.isEmpty()) {
+				continue;
+			}
+
+			for (Object val : vals) {
+				if (val instanceof Instance) {
+					String value = ((Instance) val).getBrowserText();
+
+					if (value != null) {
+						bpPropVals.add(value);
+					}
+				} else {
+					// Tried to assume its a slot and failed, defaulting to
+					// toString
+					bpPropVals.add(val.toString());
+				}
+			}
+
+			bpProps.put(slot.getBrowserText(), bpPropVals);
+			bpPropVals = new ArrayList<String>();
+		}
+
+		return bpProps;
+	}
+}
