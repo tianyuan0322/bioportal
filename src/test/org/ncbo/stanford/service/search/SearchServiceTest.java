7bc4f3e90d4ed1ea52f4a10c42f428a3e382e13e
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-11-27 01:53:13 +0000

    separated indexing from searching functionality into separate services

--- src/test/org/ncbo/stanford/service/search/SearchServiceTest.java
+++ src/test/org/ncbo/stanford/service/search/SearchServiceTest.java
@@ -2,36 +2,43 @@ package org.ncbo.stanford.service.search;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.IOException;
-
 import org.apache.lucene.search.Query;
 import org.junit.Test;
 import org.ncbo.stanford.AbstractBioPortalTest;
-import org.ncbo.stanford.bean.search.SearchBean;
 import org.ncbo.stanford.bean.search.SearchResultListBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class SearchServiceTest extends AbstractBioPortalTest {
 
 	@Autowired
-	SearchService searchService;
+	IndexService indexService;
+
+	@Autowired
+	QueryService queryService;
 
 	@Test
-	public void testSearchAllOntologies() throws IOException {
+	public void testIndexOntology() throws Exception {
+		System.out
+				.println("SearchServiceTest: indexOntology().......................BEGIN");
+
+		indexService.indexOntology(1056, false);
+
+		System.out
+				.println("SearchServiceTest: indexOntology().........................DONE");
+	}
+
+	@Test
+	public void testSearchAllOntologies() throws Exception {
 		System.out
 				.println("SearchServiceTest: searchAllOntologies().......................BEGIN");
 
-		Query query = searchService.generateLuceneSearchQuery(null, "cell",
+		Query query = queryService.generateLuceneSearchQuery(null, "cell",
 				true, false);
-		SearchResultListBean results = searchService.executeQuery(query);
+		SearchResultListBean results = queryService.executeQuery(query);
 
 		assertNotNull(results);
-		
-		for (SearchBean rec : results) {
-			System.out.println(rec);
-		}
 
 		System.out
-				.println("SearcjServiceTest: searchAllOntologies().........................DONE");
+				.println("SearchServiceTest: searchAllOntologies().........................DONE");
 	}
 }
