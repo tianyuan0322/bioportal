7d401e9ba5017fbec960843994cdc7e5030e33c7
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2008-12-05 20:23:49 +0000

    added code comments

--- src/java/org/ncbo/stanford/service/search/IndexSearchService.java
+++ src/java/org/ncbo/stanford/service/search/IndexSearchService.java
@@ -1,20 +1,71 @@
 package org.ncbo.stanford.service.search;
 
+/**
+ * Service responsible for creating and maintaining BioPortal search (Lucene)
+ * index
+ * 
+ * @author Michael Dorf
+ * 
+ */
 public interface IndexSearchService {
 
+	/**
+	 * Recreate the index of all ontologies, overwriting the existing one
+	 * 
+	 * @throws Exception
+	 */
 	public void indexAllOntologies() throws Exception;
 
+	/**
+	 * Index a given ontology
+	 * 
+	 * @param ontologyId
+	 * @throws Exception
+	 */
 	public void indexOntology(Integer ontologyId) throws Exception;
 
+	/**
+	 * Index a given ontology with options to backup and optimize index
+	 * 
+	 * @param ontologyId
+	 * @param doBackup
+	 * @param doOptimize
+	 * @throws Exception
+	 */
 	public void indexOntology(Integer ontologyId, boolean doBackup,
 			boolean doOptimize) throws Exception;
 
+	/**
+	 * Remove an ontology from index
+	 * 
+	 * @param ontologyId
+	 * @throws Exception
+	 */
 	public void removeOntology(Integer ontologyId) throws Exception;
 
+	/**
+	 * Remove a given ontology from index with options to backup and optimize
+	 * index
+	 * 
+	 * @param ontologyId
+	 * @param doBackup
+	 * @param doOptimize
+	 * @throws Exception
+	 */
 	public void removeOntology(Integer ontologyId, boolean doBackup,
 			boolean doOptimize) throws Exception;
 
+	/**
+	 * Create a backup of the existing search index
+	 * 
+	 * @throws Exception
+	 */
 	public void backupIndex() throws Exception;
 
+	/**
+	 * Run an optimization command on the existing index
+	 * 
+	 * @throws Exception
+	 */
 	public void optimizeIndex() throws Exception;
 }
