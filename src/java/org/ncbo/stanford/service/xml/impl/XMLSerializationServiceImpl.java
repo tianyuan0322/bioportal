a1200342b761dfab45a845d5740a1ec8286fb7eb
ngriff <ngriff@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-02-04 20:14:51 +0000

    Fixed the UTF-8 issue with the response XML

--- src/java/org/ncbo/stanford/service/xml/impl/XMLSerializationServiceImpl.java
+++ src/java/org/ncbo/stanford/service/xml/impl/XMLSerializationServiceImpl.java
@@ -2,6 +2,7 @@ package org.ncbo.stanford.service.xml.impl;
 
 import java.io.File;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -317,8 +318,26 @@ public class XMLSerializationServiceImpl implements XMLSerializationService {
 		StringBuffer sb = new StringBuffer(ApplicationConstants.XML_DECLARATION);
 		sb.append('\n');
 		sb.append(xmlSerializer.toXML(responseBean));
+		
+		
+		// Added code to encode ALL responses in UTF-8 
+		String utf8String=null;
+		try{
+		  byte[] stringBytes = sb.toString().getBytes();
+		    utf8String= new String(stringBytes, "UTF-8");
+		}catch(UnsupportedEncodingException e)
+		   {
+		    //  TODO: This should never happen. The UnsupportedEncodingException
+		    // should be propagated instead of swallowed. This error would indicate
+		    // a severe misconfiguration of the JVM.
 
-		return sb.toString();
+		    // As we can't translate just send back the best guess.
+		    System.out.println("UnsupportedEncodingException is: " +
+		e.getMessage());
+		    utf8String = sb.toString();
+		   }
+
+		return utf8String;
 	}
 
 	/**
