cfe4501f7ec766bf524ce72dd3bd2bf237b0fc34
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-03-19 23:51:10 +0000

    formatting

--- src/java/org/ncbo/stanford/service/diff/impl/DiffServiceImpl.java
+++ src/java/org/ncbo/stanford/service/diff/impl/DiffServiceImpl.java
@@ -3,23 +3,27 @@
  */
 package org.ncbo.stanford.service.diff.impl;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.apache.commons.logging.*;
-import org.ncbo.stanford.bean.*;
-import org.ncbo.stanford.exception.*;
-import org.ncbo.stanford.manager.diff.*;
-import org.ncbo.stanford.service.diff.*;
-import org.ncbo.stanford.service.ontology.*;
-import org.ncbo.stanford.service.ontology.impl.*;
-import org.springframework.transaction.annotation.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ncbo.stanford.bean.OntologyBean;
+import org.ncbo.stanford.exception.InvalidOntologyFormatException;
+import org.ncbo.stanford.manager.diff.OntologyDiffManager;
+import org.ncbo.stanford.service.diff.DiffService;
+import org.ncbo.stanford.service.ontology.OntologyService;
+import org.ncbo.stanford.service.ontology.impl.OntologyServiceImpl;
+import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class DiffServiceImpl implements DiffService {
 
-	private static final Log log = LogFactory
-			.getLog(OntologyServiceImpl.class);
+	private static final Log log = LogFactory.getLog(OntologyServiceImpl.class);
 	private OntologyService ontologyService;
 	private Map<String, String> ontologyFormatHandlerMap = new HashMap<String, String>(
 			0);
@@ -31,63 +35,70 @@ public class DiffServiceImpl implements DiffService {
 	 * 
 	 * @param ontologyId
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	public List<ArrayList<String>> getAllDiffsForOntology(Integer ontologyId) throws Exception {
-		OntologyBean ontologyBean = findOntologyBeanByOntologyId (ontologyId);
-		
-		OntologyDiffManager ontologyDiffManager = getDiffManager (ontologyBean);
-		return ontologyDiffManager.getAllDiffsForOntology (ontologyId);
+	public List<ArrayList<String>> getAllDiffsForOntology(Integer ontologyId)
+			throws Exception {
+		OntologyBean ontologyBean = findOntologyBeanByOntologyId(ontologyId);
+
+		OntologyDiffManager ontologyDiffManager = getDiffManager(ontologyBean);
+		return ontologyDiffManager.getAllDiffsForOntology(ontologyId);
 	}
 
 	/**
 	 * Get file object for rdf-formated diff between specified ontology verisons
 	 * 
-	 * @param ontologyVersionId1, ontologyVerisonId2
+	 * @param ontologyVersionId1,
+	 *            ontologyVerisonId2
 	 * @param format
 	 * @return
-	 * @throws FileNotFoundException 
-	 * @throws Exception 
+	 * @throws FileNotFoundException
+	 * @throws Exception
 	 */
 	public File getDiffFileForOntologyVersions(Integer ontologyVerisonId1,
-			Integer ontologyVersionId2, String format) throws FileNotFoundException, Exception {
-		OntologyBean ontologyBean = findOntologyBeanByOntologyVersionId (ontologyVerisonId1);
-		OntologyDiffManager ontologyDiffManager = getDiffManager (ontologyBean);
-		
-		return ontologyDiffManager.getDiffFileForOntologyVersions (ontologyVerisonId1, ontologyVersionId2, format);
-		
+			Integer ontologyVersionId2, String format)
+			throws FileNotFoundException, Exception {
+		OntologyBean ontologyBean = findOntologyBeanByOntologyVersionId(ontologyVerisonId1);
+		OntologyDiffManager ontologyDiffManager = getDiffManager(ontologyBean);
+
+		return ontologyDiffManager.getDiffFileForOntologyVersions(
+				ontologyVerisonId1, ontologyVersionId2, format);
+
 	}
-	
-	
+
 	// Non-interface methods
-	
-	private OntologyBean findOntologyBeanByOntologyId (Integer ontologyId) throws Exception {
-		OntologyBean ontologyBean = ontologyService.findLatestOntologyVersion(ontologyId);
+
+	private OntologyBean findOntologyBeanByOntologyId(Integer ontologyId)
+			throws Exception {
+		OntologyBean ontologyBean = ontologyService
+				.findLatestOntologyVersion(ontologyId);
 		if (ontologyBean == null) {
-			throw new Exception ();
+			throw new Exception();
 		}
 		return ontologyBean;
 	}
 
-	private OntologyBean findOntologyBeanByOntologyVersionId (Integer ontologyVersionId) throws Exception {
-		OntologyBean ontologyBean = ontologyService.findOntology(ontologyVersionId);
+	private OntologyBean findOntologyBeanByOntologyVersionId(
+			Integer ontologyVersionId) throws Exception {
+		OntologyBean ontologyBean = ontologyService
+				.findOntology(ontologyVersionId);
 		if (ontologyBean == null) {
-			throw new Exception ();
+			throw new Exception();
 		}
 		return ontologyBean;
 	}
 
-
-	
 	private OntologyDiffManager getDiffManager(OntologyBean ontologyBean)
-	throws Exception {
-		String formatHandler = ontologyFormatHandlerMap.get(ontologyBean.getFormat());
+			throws Exception {
+		String formatHandler = ontologyFormatHandlerMap.get(ontologyBean
+				.getFormat());
 		OntologyDiffManager diffManager = ontologyDiffHandlerMap
-		.get(formatHandler);
+				.get(formatHandler);
 
 		if (diffManager == null) {
-			log.error("Cannot find diffHandler for "
-					+ ontologyBean.getFormat());
+			log
+					.error("Cannot find diffHandler for "
+							+ ontologyBean.getFormat());
 			throw new InvalidOntologyFormatException(
 					"Cannot find formatHandler for " + ontologyBean.getFormat());
 		}
@@ -116,7 +127,4 @@ public class DiffServiceImpl implements DiffService {
 	public void setOntologyService(OntologyService ontologyService) {
 		this.ontologyService = ontologyService;
 	}
-
-
-
 }
