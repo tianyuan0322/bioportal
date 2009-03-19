ad52e04dacff6e2c0bf1532f0b9e56ed4c988b0e
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-03-19 22:38:18 +0000

    formatting

--- src/java/org/ncbo/stanford/manager/diff/OntologyDiffManager.java
+++ src/java/org/ncbo/stanford/manager/diff/OntologyDiffManager.java
@@ -11,56 +11,54 @@ package org.ncbo.stanford.manager.diff;
  * 
  */
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.ncbo.stanford.domain.custom.entity.*;
+import org.ncbo.stanford.domain.custom.entity.VNcboOntology;
 
 public interface OntologyDiffManager {
-	/** 
+	/**
 	 * Creates a diff between two ontology versions
 	 * 
-	 * @param ontologyVersionOld, ontologyVersionNew
+	 * @param ontologyVersionOld,
+	 *            ontologyVersionNew
 	 * 
 	 * @throws Exception
 	 */
-
-	public void createDiff (VNcboOntology ontologyVersionOld, VNcboOntology ontologyVersionNew)
-	throws Exception;
+	public void createDiff(VNcboOntology ontologyVersionOld,
+			VNcboOntology ontologyVersionNew) throws Exception;
 
 	/**
 	 * Creates a diff between the ontology that has just been loaded and its
-	 * previous version(if one exists in BioPortal).
-	 * Save the diff in a file
+	 * previous version(if one exists in BioPortal). Save the diff in a file
 	 * 
 	 * @param ontologyBean
-	 *	 
+	 * 
 	 * @throws Exception
 	 */
-
-	public void createDiffForTwoLatestVersions (Integer ontologyId)
-	throws Exception;
+	public void createDiffForTwoLatestVersions(Integer ontologyId)
+			throws Exception;
 
 	/**
-	 * Returns the file for the diff between two ontology versions in th specified format
-	 * (in any order)
+	 * Returns the file for the diff between two ontology versions in th
+	 * specified format (in any order)
 	 * 
-	 * @param ontologyVersionId1, ontologyVersionId2
+	 * @param ontologyVersionId1,
+	 *            ontologyVersionId2
 	 * @throws FileNotFoundException
 	 */
-
-	public File getDiffFileForOntologyVersions (Integer ontologyVerisonId1,
-			Integer ontologyVersionId2, String format) 
-	throws FileNotFoundException, Exception;
+	public File getDiffFileForOntologyVersions(Integer ontologyVerisonId1,
+			Integer ontologyVersionId2, String format)
+			throws FileNotFoundException, Exception;
 
 	/**
-	 * Returns a list of all diffs that are available for a given ontology 
-	 * in the following format:
-	 * <versionId1, versionId2>, <versionId2, versionId3>...
+	 * Returns a list of all diffs that are available for a given ontology in
+	 * the following format: <versionId1, versionId2>, <versionId2,
+	 * versionId3>...
 	 * 
 	 * @param onotlogyId
 	 */
-	
 	public List<ArrayList<String>> getAllDiffsForOntology(Integer ontologyId);
-
 }
