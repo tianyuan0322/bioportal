023fd103712eac841fe4f3503d29ddc129dd61b1
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-03-19 23:50:42 +0000

    formatting

--- src/java/org/ncbo/stanford/service/diff/DiffService.java
+++ src/java/org/ncbo/stanford/service/diff/DiffService.java
@@ -2,33 +2,37 @@
  * Exposes services that provide access to diff on ontologies
  * 
  * @author Natasha Noy
-  */
+ */
 package org.ncbo.stanford.service.diff;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface DiffService {
-	
+
 	/**
 	 * Return the list of all diff pairs for a given ontology id
 	 * 
 	 * @param ontologyId
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	public List<ArrayList<String>> getAllDiffsForOntology(Integer ontologyId) throws Exception;
+	public List<ArrayList<String>> getAllDiffsForOntology(Integer ontologyId)
+			throws Exception;
 
 	/**
 	 * Get file object for rdf-formated diff between specified ontology verisons
 	 * 
-	 * @param ontologyVersionId1, ontologyVerisonId2
+	 * @param ontologyVersionId1,
+	 *            ontologyVerisonId2
 	 * @param format
 	 * @return
-	 * @throws FileNotFoundException 
-	 * @throws Exception 
+	 * @throws FileNotFoundException
+	 * @throws Exception
 	 */
-	public File getDiffFileForOntologyVersions (Integer ontologyVerisonId1, Integer ontologyVersionId2, String format) throws FileNotFoundException, Exception;
-
-
+	public File getDiffFileForOntologyVersions(Integer ontologyVerisonId1,
+			Integer ontologyVersionId2, String format)
+			throws FileNotFoundException, Exception;
 }
