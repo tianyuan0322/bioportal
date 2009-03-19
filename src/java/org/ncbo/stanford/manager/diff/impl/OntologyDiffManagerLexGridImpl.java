2b45c0b42172fade5a682dc0000f7295051cd820
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-03-19 23:49:07 +0000

    added UnsupportedOpertationException(s) instead of null returns to unimplemented methods

--- src/java/org/ncbo/stanford/manager/diff/impl/OntologyDiffManagerLexGridImpl.java
+++ src/java/org/ncbo/stanford/manager/diff/impl/OntologyDiffManagerLexGridImpl.java
@@ -3,38 +3,35 @@
  */
 package org.ncbo.stanford.manager.diff.impl;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.ncbo.stanford.domain.custom.entity.*;
-import org.ncbo.stanford.manager.*;
-import org.ncbo.stanford.manager.diff.*;
+import org.ncbo.stanford.domain.custom.entity.VNcboOntology;
+import org.ncbo.stanford.manager.AbstractOntologyManagerLexGrid;
+import org.ncbo.stanford.manager.diff.OntologyDiffManager;
 
-public class OntologyDiffManagerLexGridImpl extends		
-	AbstractOntologyManagerLexGrid implements OntologyDiffManager {
+public class OntologyDiffManagerLexGridImpl extends
+		AbstractOntologyManagerLexGrid implements OntologyDiffManager {
 
 	public void createDiff(VNcboOntology ontologyVersionOld,
 			VNcboOntology ontologyVersionNew) throws Exception {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
 	}
 
 	public void createDiffForTwoLatestVersions(Integer ontologyId)
 			throws Exception {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
 	}
 
 	public List<ArrayList<String>> getAllDiffsForOntology(Integer ontologyId) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public File getDiffFileForOntologyVersions(Integer ontologyVerisonId1,
 			Integer ontologyVersionId2, String format)
 			throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
-
 }
