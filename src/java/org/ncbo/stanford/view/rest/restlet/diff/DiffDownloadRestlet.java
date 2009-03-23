4e7b64af7228eefd76c10c7a89a415a5f5a306f1
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-03-23 19:04:17 +0000

    fixed logger references

--- src/java/org/ncbo/stanford/view/rest/restlet/diff/DiffDownloadRestlet.java
+++ src/java/org/ncbo/stanford/view/rest/restlet/diff/DiffDownloadRestlet.java
@@ -1,25 +1,29 @@
 package org.ncbo.stanford.view.rest.restlet.diff;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.logging.*;
-import org.ncbo.stanford.service.diff.*;
-import org.ncbo.stanford.util.*;
-import org.ncbo.stanford.view.rest.restlet.*;
-import org.ncbo.stanford.view.rest.restlet.ontology.*;
-import org.ncbo.stanford.view.util.constants.*;
-import org.restlet.data.*;
-import org.restlet.resource.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ncbo.stanford.service.diff.DiffService;
+import org.ncbo.stanford.util.MessageUtils;
+import org.ncbo.stanford.util.RequestUtils;
+import org.ncbo.stanford.view.rest.restlet.AbstractBaseRestlet;
+import org.ncbo.stanford.view.util.constants.RequestParamConstants;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+import org.restlet.resource.FileRepresentation;
 
 /**
  * @author Natasha Noy
  */
 public class DiffDownloadRestlet extends AbstractBaseRestlet {
 	private DiffService diffService;
-	private static final Log log = LogFactory
-			.getLog(OntologyDownloadRestlet.class);
+	private static final Log log = LogFactory.getLog(DiffDownloadRestlet.class);
 
 	/**
 	 * Handle GET calls here
@@ -31,7 +35,7 @@ public class DiffDownloadRestlet extends AbstractBaseRestlet {
 	protected void getRequest(Request request, Response response) {
 		downloadDiff(request, response);
 	}
-	
+
 	/**
 	 * Return the file with the diff between ontology verisons, if one exists.
 	 * Tries both possible orders for the version
@@ -39,43 +43,42 @@ public class DiffDownloadRestlet extends AbstractBaseRestlet {
 	 * @param request
 	 * @param response
 	 */
-	
+
 	private void downloadDiff(Request request, Response response) {
 		String ontologyVersionId1 = (String) request.getAttributes().get(
 				MessageUtils.getMessage("entity.ontologyversionid1"));
 		String ontologyVersionId2 = (String) request.getAttributes().get(
 				MessageUtils.getMessage("entity.ontologyversionid2"));
-		
+
 		try {
 			Integer intId1 = Integer.parseInt(ontologyVersionId1);
 			Integer intId2 = Integer.parseInt(ontologyVersionId2);
-			
-			HttpServletRequest httpRequest = RequestUtils.getHttpServletRequest(request);
-			String format = (String) httpRequest.getParameter(RequestParamConstants.PARAM_FORMAT);
-			
-			File file = diffService.getDiffFileForOntologyVersions(intId1, intId2, format);
-			
+
+			HttpServletRequest httpRequest = RequestUtils
+					.getHttpServletRequest(request);
+			String format = (String) httpRequest
+					.getParameter(RequestParamConstants.PARAM_FORMAT);
+
+			File file = diffService.getDiffFileForOntologyVersions(intId1,
+					intId2, format);
 
 			FileRepresentation fileRepresentation = new FileRepresentation(
 					file, MediaType.APPLICATION_ALL, 60);
 			response.setEntity(fileRepresentation);
 
 			String fileName = file.getName();
-			
-			RequestUtils.getHttpServletResponse(response)
-			.setHeader(
+
+			RequestUtils.getHttpServletResponse(response).setHeader(
 					"Content-Disposition",
-					"attachment; filename=\"" + fileName
-							+ "\";");
-			
+					"attachment; filename=\"" + fileName + "\";");
 		} catch (NumberFormatException nfe) {
 			response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST, nfe
 					.getMessage());
 			nfe.printStackTrace();
 			log.error(nfe);
 		} catch (FileNotFoundException fnfe) {
-			response.setStatus(Status.SERVER_ERROR_INTERNAL, 
-							"No diff file found");
+			response.setStatus(Status.SERVER_ERROR_INTERNAL,
+					"No diff file found");
 			fnfe.printStackTrace();
 			log.error(fnfe);
 		} catch (Exception e) {
@@ -83,13 +86,9 @@ public class DiffDownloadRestlet extends AbstractBaseRestlet {
 			e.printStackTrace();
 			log.error(e);
 		}
-		
 	}
-
 
 	public void setDiffService(DiffService diffService) {
 		this.diffService = diffService;
 	}
-
-
 }
--- src/java/org/ncbo/stanford/view/rest/restlet/diff/DiffRestlet.java
+++ src/java/org/ncbo/stanford/view/rest/restlet/diff/DiffRestlet.java
@@ -1,13 +1,16 @@
 package org.ncbo.stanford.view.rest.restlet.diff;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.commons.logging.*;
-import org.ncbo.stanford.service.diff.*;
-import org.ncbo.stanford.util.*;
-import org.ncbo.stanford.view.rest.restlet.*;
-import org.ncbo.stanford.view.rest.restlet.ontology.*;
-import org.restlet.data.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ncbo.stanford.service.diff.DiffService;
+import org.ncbo.stanford.util.MessageUtils;
+import org.ncbo.stanford.view.rest.restlet.AbstractBaseRestlet;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
 
 /**
  * @author Natasha Noy
@@ -15,8 +18,7 @@ import org.restlet.data.*;
 public class DiffRestlet extends AbstractBaseRestlet {
 
 	private DiffService diffService;
-	private static final Log log = LogFactory
-			.getLog(OntologyDownloadRestlet.class);
+	private static final Log log = LogFactory.getLog(DiffRestlet.class);
 
 	/**
 	 * Handle GET calls here
@@ -26,26 +28,27 @@ public class DiffRestlet extends AbstractBaseRestlet {
 	 */
 	@Override
 	protected void getRequest(Request request, Response response) {
-		getDiffsForOntology (request, response);
+		getDiffsForOntology(request, response);
 	}
-	
 
 	/**
-	 * Returns a list of diffs that exist, as a list of pairs of ontology version ids:
-	 * v1-v2, v2-v3, ...
+	 * Returns a list of diffs that exist, as a list of pairs of ontology
+	 * version ids: v1-v2, v2-v3, ...
 	 * 
 	 * @param request
 	 * @param response
 	 */
-	private  void getDiffsForOntology (Request request, Response response) {
-		List<ArrayList<String>> versionList = getListOfAllDiffs (request, response);
-		
-		//generate response XML
+	private void getDiffsForOntology(Request request, Response response) {
+		List<ArrayList<String>> versionList = getListOfAllDiffs(request,
+				response);
+
+		// generate response XML
 		xmlSerializationService.generateXMLResponse(request, response,
 				versionList);
 	}
-	
-	private List<ArrayList<String>> getListOfAllDiffs (Request request, Response response) {		
+
+	private List<ArrayList<String>> getListOfAllDiffs(Request request,
+			Response response) {
 		List<ArrayList<String>> versionList = null;
 		String ontologyId = (String) request.getAttributes().get(
 				MessageUtils.getMessage("entity.ontologyid"));
@@ -53,16 +56,14 @@ public class DiffRestlet extends AbstractBaseRestlet {
 			Integer intId = Integer.parseInt(ontologyId);
 			versionList = diffService.getAllDiffsForOntology(intId);
 			response.setStatus(Status.SUCCESS_OK);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			response.setStatus(Status.SERVER_ERROR_INTERNAL, e.getMessage());
 			e.printStackTrace();
 			log.error(e);
 		}
+
 		return versionList;
 	}
-
-
 
 	public void setDiffService(DiffService diffService) {
 		this.diffService = diffService;
