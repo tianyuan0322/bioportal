531ea5018160caf35c7ee5ec3e4d8236544c623e
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-07-16 01:49:30 +0000

    implemented Logging data retrieval REST service

---
+++ src/java/org/ncbo/stanford/view/rest/restlet/logging/UsageLoggingRestlet.java
@@ -0,0 +1,55 @@
+package org.ncbo.stanford.view.rest.restlet.logging;
+
+import java.util.List;
+
+import org.ncbo.stanford.bean.logging.UsageLoggingBean;
+import org.ncbo.stanford.service.logging.UsageLoggingService;
+import org.ncbo.stanford.util.helper.BeanHelper;
+import org.ncbo.stanford.view.rest.restlet.AbstractBaseRestlet;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+
+public class UsageLoggingRestlet extends AbstractBaseRestlet {
+
+	private UsageLoggingService usageLoggingService;
+	private static final String NO_PARAMETERS_EXCEPTION = "This service requires appropriate query parameters to operate";
+
+	/**
+	 * Handle GET calls here
+	 */
+	@Override
+	public void getRequest(Request request, Response response) {
+		extractUsage(request, response);
+	}
+
+	@Override
+	protected boolean logRequests() {
+		return false;
+	}
+
+	private void extractUsage(Request request, Response response) {
+		List<UsageLoggingBean> usageData = null;
+		UsageLoggingBean usageBean = BeanHelper
+				.populateUsageLoggingBeanFromRequestForDataExtraction(request);
+
+		if (usageBean.isEmpty()) {
+			response.setStatus(Status.SERVER_ERROR_INTERNAL,
+					NO_PARAMETERS_EXCEPTION);
+		} else {
+			usageData = usageLoggingService.extractUsage(usageBean);
+		}
+
+		// generate response XML
+		xmlSerializationService.generateXMLResponse(request, response,
+				usageData);
+	}
+
+	/**
+	 * @param usageLoggingService
+	 *            the usageLoggingService to set
+	 */
+	public void setUsageLoggingService(UsageLoggingService usageLoggingService) {
+		this.usageLoggingService = usageLoggingService;
+	}
+}
