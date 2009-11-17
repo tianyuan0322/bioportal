7077b446c4d9f64fddf6954377851283f6ba8873
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-11-17 20:39:09 +0000

    added InvalidInputException to provide a uniform invalid parameter input exception handling

---
+++ src/java/org/ncbo/stanford/exception/InvalidInputException.java
@@ -0,0 +1,33 @@
+package org.ncbo.stanford.exception;
+
+public class InvalidInputException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5777897520002877516L;
+
+	public static final String DEFAULT_MESSAGE = "Invalid input";
+
+	/**
+	 * 
+	 */
+	public InvalidInputException() {
+		super(DEFAULT_MESSAGE);
+	}
+
+	public InvalidInputException(String msg) {
+		super(msg);
+	}
+
+	public InvalidInputException(String msg, Exception e) {
+		super(msg, e);
+	}
+
+	public InvalidInputException(Throwable t) {
+		super(t);
+	}
+
+	public InvalidInputException(String msg, Throwable t) {
+		super(msg, t);
+	}
+}
