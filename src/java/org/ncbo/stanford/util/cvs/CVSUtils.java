b6015df34f79064d35079ba3e87692464790e7d8
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2009-12-11 23:49:22 +0000

    fixed the repository file path calculation to be consistent for both CVS and SVN repositories

--- src/java/org/ncbo/stanford/util/cvs/CVSUtils.java
+++ src/java/org/ncbo/stanford/util/cvs/CVSUtils.java
@@ -240,7 +240,7 @@ public class CVSUtils {
 			GregorianCalendar gCal = new GregorianCalendar();
 			gCal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal
 					.get(Calendar.DAY_OF_MONTH));
-			files.put(entry.getName(), new CVSFile(entry.getLocalPathName(),
+			files.put(entry.getName(), new CVSFile(entry.getFullPathName(),
 					gCal, entry.getVersion()));
 		}
 
