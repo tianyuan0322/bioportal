f0b8def243f06f569a9ec5309def42f8f5b5ae9d
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2010-04-08 19:48:26 +0000

    1. fixed a bug with InstanceCount 2. fixed a bug that cause discrepancy between getRootConcept and getAllConcepts

--- src/java/org/ncbo/stanford/manager/retrieval/impl/OntologyRetrievalManagerProtegeImpl.java
+++ src/java/org/ncbo/stanford/manager/retrieval/impl/OntologyRetrievalManagerProtegeImpl.java
@@ -218,7 +218,7 @@ public class OntologyRetrievalManagerProtegeImpl extends
 
 		if (kb instanceof OWLModel) {
 			// RDF/OWL format
-			allClasses.addAll(((OWLModel) kb).getUserDefinedOWLNamedClasses());
+			allClasses.addAll(((OWLModel) kb).getUserDefinedRDFSNamedClasses());
 		} else {
 			// Protege format
 			allClasses.addAll(kb.getClses());
@@ -551,10 +551,6 @@ public class OntologyRetrievalManagerProtegeImpl extends
 		addDefinitions(cls, definitionSlot, classBean);
 		addAuthors(cls, authorSlot, classBean);
 
-		// TODO: need to check
-		// set instance count
-		// classBean.setInstanceCount(cls.getInstanceCount());
-
 		recursionMap.put(cls, classBean);
 
 		// add properties
@@ -608,10 +604,10 @@ public class OntologyRetrievalManagerProtegeImpl extends
 		}
 
 		classBean.addRelation(ApplicationConstants.CHILD_COUNT, subclasses
-				.size());
+				.size());		
 		classBean.addRelation(ApplicationConstants.INSTANCE_COUNT, cls
-				.getInstanceCount());
-
+				.getDirectInstanceCount());
+		
 		if (recursive) {
 			classBean.addRelation(ApplicationConstants.SUB_CLASS,
 					convertClasses(subclasses, false, synonymSlot,
