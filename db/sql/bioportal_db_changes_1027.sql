3c84bf074b27b000bf5f217f02b4ee675e7f51ac
paul <paul@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2010-06-04 21:45:32 +0000

    Remove data from table (should be empty to start).

--- db/sql/bioportal_db_changes_1027.sql
+++ db/sql/bioportal_db_changes_1027.sql
@@ -8,5 +8,3 @@ CREATE TABLE `ncbo_user_subscriptions` (
   `ontology_id` varchar(11) DEFAULT 'NULL',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-insert  into `ncbo_user_subscriptions`(`id`,`user_id`,`ontology_id`) values (1,38578,'1083');
