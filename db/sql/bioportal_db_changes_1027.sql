c1a007132796f7ba731e3fe39e641b36ea22111b
paul <paul@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2010-06-26 01:15:59 +0000

    Backporting notification service changes to tag 1027 (Fix table structure in tag 1027 db migration).

--- db/sql/bioportal_db_changes_1027.sql
+++ db/sql/bioportal_db_changes_1027.sql
@@ -3,8 +3,8 @@ USE `bioportal`;
 DROP TABLE IF EXISTS `ncbo_user_subscriptions`;
 
 CREATE TABLE `ncbo_user_subscriptions` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `ontology_id` varchar(11) DEFAULT 'NULL',
+  `ontology_id` varchar(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
