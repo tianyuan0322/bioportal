66aae7998fb322d8d1c283e39f670f5a65c4d4a7
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2010-06-30 00:48:12 +0000

    added new bean, NcboLNotificationTypeDAO

---
+++ src/java/org/ncbo/stanford/domain/custom/dao/CustomNcboLNotificationTypeDAO.java
@@ -0,0 +1,14 @@
+package org.ncbo.stanford.domain.custom.dao;
+
+import org.ncbo.stanford.domain.generated.NcboLNotificationTypeDAO;
+
+public class CustomNcboLNotificationTypeDAO extends NcboLNotificationTypeDAO {
+
+	/**
+	 * 
+	 */
+	public CustomNcboLNotificationTypeDAO() {
+		super();
+	}
+
+}
