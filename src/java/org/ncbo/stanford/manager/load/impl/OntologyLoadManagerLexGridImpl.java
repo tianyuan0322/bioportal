32666fcb20bf2ca16abf8fe541d95676c18110ba
natashafn <natashafn@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2010-08-26 18:29:45 +0000

    removed EPL license statement

--- src/java/org/ncbo/stanford/manager/load/impl/OntologyLoadManagerLexGridImpl.java
+++ src/java/org/ncbo/stanford/manager/load/impl/OntologyLoadManagerLexGridImpl.java
@@ -1,21 +1,3 @@
-/*
- * The contents of this file are licensed under the Eclipse Public License,
- * Version 1.0 (the "License");
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at -
- *
- *      http://www.eclipse.org/legal/epl-v10.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * The initial developer of the original code is Stanford University. Portions
- * created by Stanford University are Copyright (c) 2005-2007.  All Rights Reserved.
- *
- */
 package org.ncbo.stanford.manager.load.impl;
 
 /**
