01974cb69f498d5151f4955947c74636b9a51f45
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2010-11-30 21:00:58 +0000

    Upgraded Lucene from 2.4.1 to 2.9.3 ([#2808] Upgrade Lucene library to 2.9.3). The new version requires a compatibility version parameter to be passed in to various Lucene classes.

--- src/java/org/ncbo/stanford/service/search/impl/QuerySearchServiceImpl.java
+++ src/java/org/ncbo/stanford/service/search/impl/QuerySearchServiceImpl.java
@@ -37,34 +37,40 @@ public class QuerySearchServiceImpl extends AbstractSearchService implements
 	
 //	public static void main(String[] args) {
 //		try {
-////			String expr = "DOID:1909";
-////			String expr = "blood";
-////			String expr = "Blue_Nevus-Like_Melanoma";
-////			String expr = "Interferon-Alfa_Lu-177-Monoclonal-Antibody-CC49_Pa";
-////			String expr = "Swiss_Albinos_City_of_Hope_Med_Ctr";
+//			String expr = "DOID:1909";
+//			String expr = "blood";
+//			String expr = "Blue_Nevus-Like_Melanoma";
+//			String expr = "Interferon-Alfa_Lu-177-Monoclonal-Antibody-CC49_Pa";
+//			String expr = "Swiss_Albinos_City_of_Hope_Med_Ctr";
 //			String expr = "Can of vul and vag";
-////			String expr = "lun";
-////			String expr = "algorith";
-////			String expr = "monadic                    	Quality of an object*";
-////			String expr = "CHEBI:16069";
-////			String expr = "blood-vein";
-////			String expr = "Interferon-Alfa_Lu-177-Monoclonal-Antibody-CC49";
-////			String expr = "*Clarke's nu*";
-////			String expr = "multiply";
+//			String expr = "lun";
+//			String expr = "algorith";
+//			String expr = "positron emission tomography";
+//			String expr = "predominately round";
+//			String expr = "monadic                    	Quality of an object*";
+//			String expr = "CHEBI:16069";
+//			String expr = "blood-vein";
+//			String expr = "Interferon-Alfa_Lu-177-Monoclonal-Antibody-CC49";
+//			String expr = "*Clarke's nu*";
+//			String expr = "multiply";
 //			Collection<Integer> ontologyIds = new ArrayList<Integer>(0);
-////			ontologyIds.add(1353);
-////			ontologyIds.add(1104);
-//			// ontologyIds.add(1070);
-//			// ontologyIds.add(1107);
-////			ontologyIds.add(1321); //Nemo
-//
+//			ontologyIds.add(1353);
+//			ontologyIds.add(1104);
+//			ontologyIds.add(1057);
+			// ontologyIds.add(1070);
+			// ontologyIds.add(1107);
+//			ontologyIds.add(1321); //Nemo
+
 //			boolean includeProperties = false;
 //			boolean isExactMatch = false;
-//			Integer maxNumHits = 250;
+//			Integer maxNumHits = 2250;
 //
 //			String indexPath = "/apps/bmir.apps/bioportal_resources/searchindex";
 //			QuerySearchServiceImpl ss = new QuerySearchServiceImpl();
-//			ss.setAnalyzer(new StandardAnalyzer());
+//			
+//			Version ver = Version.LUCENE_24;
+//			ss.setLuceneVersion(ver);
+//			ss.setAnalyzer(new StandardAnalyzer(ver));
 //			ss.setIndexPath(indexPath);
 //
 //			Query q = ss.generateLuceneSearchQuery(ontologyIds, expr,
@@ -73,17 +79,18 @@ public class QuerySearchServiceImpl extends AbstractSearchService implements
 //
 //			long start = System.currentTimeMillis();
 //			SearchResultListBean results = ss.runQuery(q, maxNumHits, null);
-//			long stop = System.currentTimeMillis();
-//
+//			long stop = System.currentTimeMillis();			
 //			System.out.println("Excecution Time: " + (double) (stop - start)
-//					/ 1000 + " seconds.");			
+//					/ 1000 + " seconds.");
 //			
+//			System.out.println("Num Hits: " + results.size());
+//			System.out.println(Jestr.str(results));
 //		} catch (Exception e) {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
 //	}
-//	
+	
 //	 public static void main(String[] args) {
 //		try {
 //			IndexSearcher searcher = new IndexSearcher(
@@ -317,7 +324,7 @@ public class QuerySearchServiceImpl extends AbstractSearchService implements
 	private void addContentsClauseContains(String expr, BooleanQuery query)
 			throws IOException {
 		try {
-			PrefixQuery q = new PrefixQuery(searcher.getIndexReader(), analyzer);
+			PrefixQuery q = new PrefixQuery(luceneVersion, searcher.getIndexReader(), analyzer);
 			q.parsePrefixQuery(SearchIndexBean.CONTENTS_FIELD_LABEL, expr);
 			// q.parseStartsWithPrefixQuery((PrefixQuery.isMultiWord(expr)) ?
 			// SearchIndexBean.LITERAL_CONTENTS_FIELD_LABEL :
