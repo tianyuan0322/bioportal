35bd9962579f3fea5f9d19b75fff7d657b8fea0b
gprakash <gprakash@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2010-12-20 13:56:53 +0000

    Code For  [#2569] Allow batch uploading of mappings as CSV or RDF

--- src/java/org/ncbo/stanford/view/rest/restlet/mapping/MappingUploadRestlet.java
+++ src/java/org/ncbo/stanford/view/rest/restlet/mapping/MappingUploadRestlet.java
@@ -1,40 +1,41 @@
 package org.ncbo.stanford.view.rest.restlet.mapping;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.List;
-
-import org.ncbo.stanford.bean.OntologyBean;
-import org.ncbo.stanford.bean.concept.ClassBean;
-import org.ncbo.stanford.bean.mapping.OneToOneMappingBean;
-import org.ncbo.stanford.bean.mapping.upload.UploadedMappingBean;
-
-import org.ncbo.stanford.enumeration.MappingSourceEnum;
-import org.ncbo.stanford.exception.ConceptNotFoundException;
-import org.ncbo.stanford.exception.InvalidInputException;
-import org.ncbo.stanford.service.concept.ConceptService;
-import org.ncbo.stanford.service.ontology.OntologyService;
-import org.ncbo.stanford.manager.mapping.MappingManager;
-import org.ncbo.stanford.util.MessageUtils;
-import org.ncbo.stanford.util.RequestUtils;
-import org.ncbo.stanford.util.constants.ApplicationConstants;
-
-import org.ncbo.stanford.view.rest.restlet.AbstractBaseRestlet;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
+import org.ncbo.stanford.bean.OntologyBean;
+import org.ncbo.stanford.bean.concept.ClassBean;
+import org.ncbo.stanford.bean.mapping.OneToOneMappingBean;
+import org.ncbo.stanford.bean.mapping.upload.UploadedMappingBean;
+import org.ncbo.stanford.exception.ConceptNotFoundException;
+import org.ncbo.stanford.exception.InvalidInputException;
+import org.ncbo.stanford.exception.MappingMissingException;
+import org.ncbo.stanford.manager.mapping.MappingManager;
+import org.ncbo.stanford.service.concept.ConceptService;
+import org.ncbo.stanford.service.mapping.MappingService;
+import org.ncbo.stanford.service.ontology.OntologyService;
+import org.ncbo.stanford.util.MessageUtils;
+import org.ncbo.stanford.util.RequestUtils;
+import org.ncbo.stanford.util.constants.ApplicationConstants;
+import org.ncbo.stanford.view.rest.restlet.AbstractBaseRestlet;
+import org.openrdf.model.URI;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
-
 import org.restlet.data.Status;
 
 /**
@@ -43,9 +44,158 @@ import org.restlet.data.Status;
  */
 public class MappingUploadRestlet extends AbstractBaseRestlet {
 	private static final Log log = LogFactory.getLog(MappingUploadRestlet.class);
+	//private static URI mappingId;
 	private OntologyService ontologyService;
 	private MappingManager mappingManager;
 	private ConceptService conceptService;
+	private MappingService mappingService;
+	
+
+	Map<String ,String> mapForMapping=new HashMap<String ,String>();
+	
+
+	@Override
+	public void postRequest(Request request, Response response) {
+
+		filesForMapping(request, response);
+
+	}
+
+	/**
+	 * Return to the response uploading files
+	 * 
+	 * @param request
+	 *            response
+	 * @throws FileUploadException 
+	 */
+	private void filesForMapping(Request request, Response response) {
+		HttpServletRequest httpServletRequest = RequestUtils
+				.getHttpServletRequest(request);
+		String fileName;
+		OneToOneMappingBean oneToOneMappingBean = null;
+
+		UploadedMappingBean uploadedMappingBean = null;
+		OntologyBean ontologyBean = null;
+
+		ClassBean concept = null;
+		// List for UploadedMappingBean
+		List<UploadedMappingBean> listForMappingBean = new ArrayList<UploadedMappingBean>();
+		List<URI> listOfId = new ArrayList<URI>();
+		
+		try {
+			// Condition for the HTTP request is encoded in multipart format
+			if (ServletFileUpload.isMultipartContent(httpServletRequest)) {
+				// Parsing the form data contained in the HTTP request.
+				ServletFileUpload servletFileUpload = new ServletFileUpload(
+						new DiskFileItemFactory());
+				// Creating a list for containing a list of file items
+				List listForUploadFileItems = servletFileUpload
+						.parseRequest(httpServletRequest);
+				Iterator iteratorForFile = listForUploadFileItems.iterator();
+				// Using the Iterator for iterating a file inside List
+				while (iteratorForFile.hasNext()) {
+					// Passing the File inside FileItem
+					FileItem fileItem = (FileItem) iteratorForFile.next();
+					fileName = fileItem.getName();
+					// Taking the InputStream
+					InputStream inputStream = fileItem.getInputStream();
+					// Creating a ByteArrayOutputStream Object
+					ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+					// Taking the Size of FileItems
+					int sizeOfFile = (int) fileItem.getSize();
+					// Creating a Array of Byte
+					byte buffer[] = new byte[sizeOfFile];
+					// Initialising a variable
+					int bytesRead = 0;
+					// read the input
+					while ((bytesRead = inputStream.read(buffer, 0, sizeOfFile)) != -1) {
+						byteArrayOutputStream.write(buffer, 0, bytesRead);
+					}
+					// assign it to string
+					String data = new String(byteArrayOutputStream
+							.toByteArray());
+					// Condition For File type it's csv or rdf
+					if (fileName.endsWith(ApplicationConstants.CSVFile)) {
+						// Parsing the CSV file and stores inside List
+						listForMappingBean = mappingManager.parseCSVFile(data);
+					}
+					// Iterating the UploadedMappingBean from the list
+					for (int i = 1; i < listForMappingBean.size(); i++) {
+						uploadedMappingBean = listForMappingBean.get(i);
+						// OntologyIdInt from the UploadedOntologyBean
+						Integer ontologyIdInt = Integer.parseInt(uploadedMappingBean.getSourceOntologyId());
+						// OntologyVersionIdInt from the UploadedOntologyBean
+						Integer ontologyVersionIdInt = Integer.parseInt(uploadedMappingBean.getSourceCreatedInOntologyVersion());
+						// ConceptId from the UploadedOntologyBean
+						String conceptId = uploadedMappingBean.getSource();
+						// Condition for validating ontologies using ontologyIdInt
+						if (ontologyIdInt != null) {
+							ontologyBean = ontologyService.findLatestOntologyOrViewVersion(ontologyIdInt);
+						}
+						// Condition for validating ontologies using ontologyVersionIdInt
+						else if (ontologyVersionIdInt != null) {
+							ontologyBean = ontologyService.findOntologyOrView(ontologyVersionIdInt);
+						}
+						// Condition for validating the existence of a concept
+						if (conceptId != null)
+							concept = conceptService.findConcept(ontologyBean.getId(), conceptId, null, false, false);
+						//Condition for OntologyBean for null Values
+						if (ontologyBean == null) {
+							throw new InvalidInputException(
+									MessageUtils
+											.getMessage("msg.error.ontologyversionidinvalid"));
+						}
+						//Condition for ConceptBean for null values
+						if (concept == null) {
+							throw new ConceptNotFoundException(MessageUtils.getMessage("msg.error.conceptNotFound"));
+						}
+					
+						//Calling the createMappingForUploadFile 
+						oneToOneMappingBean=mappingManager.createMappingForUploadedFile(uploadedMappingBean);
+						OneToOneMappingBean retrievedMappingBean=mappingService.getMapping(oneToOneMappingBean.getId());
+						//Condition for checking the mapping is success or not
+						if(retrievedMappingBean != null  && retrievedMappingBean.toString().isEmpty()){
+							listOfId.add(retrievedMappingBean.getId());
+						}else{
+							throw new MappingMissingException();
+						}
+					}
+				}
+			}
+			
+		}catch(MappingMissingException mappingMissingException){
+			mappingMissingException.getMessage();
+		}catch(FileUploadException fileUploadException){
+			fileUploadException.getMessage();
+		}catch(InputMismatchException inputOutputException){
+			inputOutputException.getMessage();
+		}catch(IOException ioException){
+			ioException.getMessage();
+		}catch (InvalidInputException e) {
+			response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST, e.getMessage());
+		} catch (ConceptNotFoundException cnfe) {
+			response
+					.setStatus(Status.CLIENT_ERROR_NOT_FOUND, cnfe.getMessage());
+
+		}catch (Exception e) {
+			response.setStatus(Status.SERVER_ERROR_INTERNAL, mapForMapping.toString());
+			e.printStackTrace();
+			log.error(e);
+		} 
+		finally {
+			xmlSerializationService
+					.generateXMLResponse(request, response, listOfId);
+		}
+
+	}
+	
+	/**
+	 * 
+	 * @param mappingService
+	 */
+	public void setMappingService(MappingService mappingService) {
+		this.mappingService = mappingService;
+	}
 
 	/**
 	 * 
@@ -70,132 +220,6 @@ public class MappingUploadRestlet extends AbstractBaseRestlet {
 	public void setOntologyService(OntologyService ontologyService) {
 		this.ontologyService = ontologyService;
 	}
-
-	@Override
-	public void postRequest(Request request, Response response) {
-
-		filesForMapping(request, response);
-
-	}
-
-	/**
-	 * Return to the response uploading files
-	 * 
-	 * @param request
-	 *            response
-	 */
-	private void filesForMapping(Request request, Response response) {
-		HttpServletRequest httpServletRequest = RequestUtils
-				.getHttpServletRequest(request);
-		String fileName;
-		OneToOneMappingBean oneToOneMappingBean = null;
-
-		UploadedMappingBean uploadedMappingBean = null;
-		OntologyBean ontologyBean = null;
-
-		ClassBean concept = null;
-		// List for UploadedMappingBean
-		List<UploadedMappingBean> listForMappingBean = new ArrayList<UploadedMappingBean>();
-
-		try {
-			// Condition for the HTTP request is encoded in multipart format
-			if (ServletFileUpload.isMultipartContent(httpServletRequest)) {
-				// Parsing the form data contained in the HTTP request.
-				ServletFileUpload servletFileUpload = new ServletFileUpload(
-						new DiskFileItemFactory());
-				// Creating a list for containing a list of file items
-				List listForUploadFileItems = servletFileUpload
-						.parseRequest(httpServletRequest);
-				Iterator iteratorForFile = listForUploadFileItems.iterator();
-				// Using the Iterator for iterating a file inside List
-				while (iteratorForFile.hasNext()) {
-					// Passing the File inside FileItem
-					FileItem fileItem = (FileItem) iteratorForFile.next();
-					fileName = fileItem.getName();
-					// Taking the InputStream
-					InputStream inputStream = fileItem.getInputStream();
-					// Creating a ByteArrayOutputStream Object
-					ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-					// Taking the Size of FileItems
-					int sizeOfFile = (int) fileItem.getSize();
-
-					// Creating a Array of Byte
-					byte buffer[] = new byte[sizeOfFile];
-					// Initialising a variable
-					int bytesRead = 0;
-					// read the input
-					while ((bytesRead = inputStream.read(buffer, 0, sizeOfFile)) != -1) {
-						byteArrayOutputStream.write(buffer, 0, bytesRead);
-					}
-					// assign it to string
-					String data = new String(byteArrayOutputStream
-							.toByteArray());
-					// Condition For File type it's csv or rdf
-					if (fileName.endsWith(ApplicationConstants.CSVFile)) {
-						// Parsing the CSV file and stores inside List
-						listForMappingBean = mappingManager.parseCSVFile(data);
-					}
-					// Iterating the UploadedMappingBean from the list
-					for (int i = 1; i < listForMappingBean.size(); i++) {
-						uploadedMappingBean = listForMappingBean.get(i);
-						// OntologyIdInt from the UploadedOntologyBean
-						Integer ontologyIdInt = Integer
-								.parseInt(uploadedMappingBean
-										.getSourceOntologyId());
-						// OntologyVersionIdInt from the UploadedOntologyBean
-						Integer ontologyVersionIdInt = Integer
-								.parseInt(uploadedMappingBean
-										.getSourceCreatedInOntologyVersion());
-						// ConceptId from the UploadedOntologyBean
-						String conceptId = uploadedMappingBean.getSource();
-						// Condition for validating ontologies using
-						// ontologyIdInt
-
-						if (ontologyIdInt != null) {
-							ontologyBean = ontologyService
-									.findLatestOntologyOrViewVersion(ontologyIdInt);
-						}
-						// Condition for validating ontologies using
-						// ontologyVersionIdInt
-						else if (ontologyVersionIdInt != null) {
-							ontologyBean = ontologyService
-									.findOntologyOrView(ontologyVersionIdInt);
-						}
-						// Condition for validating the existence of a concept
-						if (conceptId != null)
-							concept = conceptService.findConcept(ontologyBean
-									.getId(), conceptId, null, false, false);
-
-						if (ontologyBean == null) {
-							throw new InvalidInputException(
-									MessageUtils
-											.getMessage("msg.error.ontologyversionidinvalid"));
-						}
-						if (concept == null) {
-							throw new ConceptNotFoundException(MessageUtils
-									.getMessage("msg.error.conceptNotFound"));
-						}
-
-						oneToOneMappingBean = mappingManager
-								.createMappingForUploadedFile(uploadedMappingBean);
-
-					}
-				}
-			}
-		} catch (InvalidInputException e) {
-			response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST, e.getMessage());
-		} catch (ConceptNotFoundException cnfe) {
-			response
-					.setStatus(Status.CLIENT_ERROR_NOT_FOUND, cnfe.getMessage());
-
-		} catch (Exception e) {
-			response.setStatus(Status.SERVER_ERROR_INTERNAL, e.getMessage());
-			e.printStackTrace();
-			log.error(e);
-		} finally {
-			xmlSerializationService
-					.generateStatusXMLResponse(request, response);
-		}
-
-	}
+	
+	
 }
