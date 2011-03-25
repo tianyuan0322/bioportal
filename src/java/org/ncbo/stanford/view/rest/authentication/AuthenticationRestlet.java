f6f056aea8fba6695ffcb5d4d7d84354bdcb08b1
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2011-03-25 20:37:23 +0000

    implemented authentication entry point

--- src/java/org/ncbo/stanford/view/rest/authentication/AuthenticationRestlet.java
+++ src/java/org/ncbo/stanford/view/rest/authentication/AuthenticationRestlet.java
@@ -5,26 +5,23 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ncbo.stanford.bean.UserBean;
-import org.ncbo.stanford.enumeration.ErrorTypeEnum;
 import org.ncbo.stanford.exception.AuthenticationException;
 import org.ncbo.stanford.exception.BPRuntimeException;
-import org.ncbo.stanford.exception.UserNotFoundException;
 import org.ncbo.stanford.service.session.RESTfulSession;
 import org.ncbo.stanford.service.session.SessionService;
-import org.ncbo.stanford.service.user.UserService;
 import org.ncbo.stanford.util.RequestUtils;
+import org.ncbo.stanford.util.constants.ApplicationConstants;
 import org.ncbo.stanford.util.helper.StringHelper;
+import org.ncbo.stanford.util.security.SecurityContextHolder;
+import org.ncbo.stanford.util.security.authentication.AuthenticationService;
 import org.ncbo.stanford.view.rest.restlet.AbstractBaseRestlet;
 import org.ncbo.stanford.view.util.constants.RequestParamConstants;
-import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
-import org.springframework.security.context.SecurityContextHolder;
 
 /**
- * Handles Acegi form based authentication. Simulates the the Acegi's
- * authenticationProcessingFilter in RESTful environment.
+ * Handles form based authentication with username and password.
  * 
  * @author Michael Dorf
  * 
@@ -35,7 +32,7 @@ public final class AuthenticationRestlet extends AbstractBaseRestlet {
 			.getLog(AuthenticationRestlet.class);
 
 	private SessionService sessionService;
-	private UserService userService;
+	private AuthenticationService authenticationService;
 
 	@Override
 	public void handle(Request request, Response response) {
@@ -49,8 +46,8 @@ public final class AuthenticationRestlet extends AbstractBaseRestlet {
 	 * @param response
 	 */
 	private void authenticate(Request request, Response response) {
-		String accessedResource = request.getResourceRef().getPath();
-		RESTfulSession appSession = null;
+		RESTfulSession userSession = null;
+		String apiKey = null;
 
 		try {
 			HttpServletRequest httpServletRequest = RequestUtils
@@ -61,33 +58,37 @@ public final class AuthenticationRestlet extends AbstractBaseRestlet {
 			final String appApiKey = obtainApiKey(httpServletRequest);
 
 			if (StringHelper.isNullOrNullString(appApiKey)
-					|| (appSession = sessionService.get(appApiKey)) == null) {
+					|| sessionService.get(appApiKey) == null) {
 				throw new AuthenticationException(
 						"This application did not pass proper credentials to authenticate users.");
 			}
 
-			UserBean userBean = userService.findUser(username);
-
-			if (userBean == null) {
-				log
-						.error("There is no user in the system corresponding to username: "
-								+ username);
-				throw new AuthenticationException(
-						"Invalid credentials supplied");
-			}
-
+			UserBean user = authenticationService.authenticate(username,
+					password);
+			apiKey = user.getApiKey();
+			sessionService.invalidate(apiKey);
+			userSession = sessionService.createNewSession(apiKey);
+			userSession.setAttribute(ApplicationConstants.SECURITY_CONTEXT_KEY,
+					new SecurityContextHolder(apiKey, appApiKey, user));
 		} catch (BPRuntimeException re) {
-///			sessionService.invalidate()
+			if (apiKey != null) {
+				sessionService.invalidate(apiKey);
+			}
 			re.printStackTrace();
 			log.error(re);
 			response.setStatus(Status.SERVER_ERROR_INTERNAL, re.getMessage());
 		} catch (AuthenticationException ae) {
 			response.setStatus(Status.CLIENT_ERROR_FORBIDDEN, ae.getMessage());
 		} catch (Exception e) {
-///			sessionService.invalidate()
+			if (apiKey != null) {
+				sessionService.invalidate(apiKey);
+			}
 			e.printStackTrace();
 			log.error(e);
 			response.setStatus(Status.SERVER_ERROR_INTERNAL, e.getMessage());
+		} finally {
+			xmlSerializationService.generateXMLResponse(request, response,
+					userSession);
 		}
 	}
 
@@ -112,10 +113,11 @@ public final class AuthenticationRestlet extends AbstractBaseRestlet {
 	}
 
 	/**
-	 * @param userService
-	 *            the userService to set
+	 * @param authenticationService
+	 *            the authenticationService to set
 	 */
-	public void setUserService(UserService userService) {
-		this.userService = userService;
+	public void setAuthenticationService(
+			AuthenticationService authenticationService) {
+		this.authenticationService = authenticationService;
 	}
 }
