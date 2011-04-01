c90295a3a8ca4c1ad8b10da67248e7765da7ea09
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2011-04-01 21:50:30 +0000

    acl wrappers for custom output

---
+++ src/java/org/ncbo/stanford/bean/acl/OntologyAcl.java
@@ -0,0 +1,11 @@
+package org.ncbo.stanford.bean.acl;
+
+import java.util.HashMap;
+
+public class OntologyAcl extends HashMap<Integer, Boolean> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9014462233676954596L;
+}
---
+++ src/java/org/ncbo/stanford/bean/acl/UserAcl.java
@@ -0,0 +1,11 @@
+package org.ncbo.stanford.bean.acl;
+
+import java.util.HashMap;
+
+public class UserAcl extends HashMap<Integer, Boolean> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3140353834321018542L;
+}
