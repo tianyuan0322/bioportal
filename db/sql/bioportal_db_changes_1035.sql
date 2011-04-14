5b1fd1e1a998bee9494ef2edce36f17d0e8a930f
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2011-04-14 00:41:51 +0000

    added OBS user

--- db/sql/bioportal_db_changes_1035.sql
+++ db/sql/bioportal_db_changes_1035.sql
@@ -14,3 +14,12 @@ VALUES ('temp-admin-user', 'fcc74490-5a25-11e0-9a6e-005056aa215e', 'jczJAOPkgEuT
 	
 INSERT INTO ncbo_user_role (user_id, role_id)
 SELECT id, 2824 FROM ncbo_user WHERE username = 'temp-admin-user' AND api_key = 'fcc74490-5a25-11e0-9a6e-005056aa215e';
+
+-- A user for OBS Application. Temporarily, grant this user admin privileges
+INSERT INTO ncbo_user (username, api_key, PASSWORD, email, firstname, lastname)
+VALUES ('obs-user', '0de68a1e-662f-11e0-9d7b-005056aa3316', 'wgnDm3+21SHhwrJ22/vJV9Co2MM4h7vZlg3luF9l83k2te+sZ8tYSQTbG5Pw4wCi',
+	'obs-user@bioontology.org', 'OBS', 'User');
+	
+INSERT INTO ncbo_user_role (user_id, role_id)
+SELECT id, 2824 FROM ncbo_user WHERE username = 'obs-user' AND api_key = '0de68a1e-662f-11e0-9d7b-005056aa3316';
+	
