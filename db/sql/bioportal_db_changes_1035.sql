68c783b2c82719c72dc0d2bf28b251bd8d27b7a1
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2011-04-14 22:10:59 +0000

    added user deletion statments

--- db/sql/bioportal_db_changes_1035.sql
+++ db/sql/bioportal_db_changes_1035.sql
@@ -8,6 +8,12 @@ SELECT LOWER(application_name), application_id, 'MaT9mL6DdWI6s471OFTiJUEZlZz6Ycx
 FROM ncbo_admin_application;
 
 -- A temporary admin user to fallback on during the security grace period
+DELETE FROM ncbo_user_role WHERE user_id = (
+	SELECT id FROM ncbo_user WHERE username = 'temp-admin-user' AND api_key = 'fcc74490-5a25-11e0-9a6e-005056aa215e'
+);
+
+DELETE FROM ncbo_user WHERE username = 'temp-admin-user' AND api_key = 'fcc74490-5a25-11e0-9a6e-005056aa215e';
+
 INSERT INTO ncbo_user (username, api_key, PASSWORD, email, firstname, lastname)
 VALUES ('temp-admin-user', 'fcc74490-5a25-11e0-9a6e-005056aa215e', 'jczJAOPkgEuT6mxRJUMlvi1GNmfK9oZZq/Dce0D3Ka4Ove6NWmroWbNb/1IB0gWj',
 	'temp-admin-user@bioontology.org', 'Temporary', 'Administrator');
@@ -16,6 +22,12 @@ INSERT INTO ncbo_user_role (user_id, role_id)
 SELECT id, 2824 FROM ncbo_user WHERE username = 'temp-admin-user' AND api_key = 'fcc74490-5a25-11e0-9a6e-005056aa215e';
 
 -- A user for OBS Application. Temporarily, grant this user admin privileges
+DELETE FROM ncbo_user_role WHERE user_id = (
+	SELECT id FROM ncbo_user WHERE username = 'obs-user' AND api_key = '0de68a1e-662f-11e0-9d7b-005056aa3316'
+);
+
+DELETE FROM ncbo_user WHERE username = 'obs-user' AND api_key = '0de68a1e-662f-11e0-9d7b-005056aa3316';
+
 INSERT INTO ncbo_user (username, api_key, PASSWORD, email, firstname, lastname)
 VALUES ('obs-user', '0de68a1e-662f-11e0-9d7b-005056aa3316', 'wgnDm3+21SHhwrJ22/vJV9Co2MM4h7vZlg3luF9l83k2te+sZ8tYSQTbG5Pw4wCi',
 	'obs-user@bioontology.org', 'OBS', 'User');
