b35500ba8b64c6e8ab9128254f37eb5808b32563
paul <paul@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2011-04-30 01:04:30 +0000

    Enables metrics integrity check.

--- src/java/org/ncbo/stanford/service/loader/fixer/impl/OntologyIntegrityFixerServiceImpl.java
+++ src/java/org/ncbo/stanford/service/loader/fixer/impl/OntologyIntegrityFixerServiceImpl.java
@@ -8,17 +8,20 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ncbo.stanford.bean.OntologyBean;
+import org.ncbo.stanford.bean.OntologyMetricsBean;
 import org.ncbo.stanford.bean.search.OntologyHitBean;
+import org.ncbo.stanford.exception.MetadataException;
 import org.ncbo.stanford.manager.metadata.OntologyMetadataManager;
 import org.ncbo.stanford.service.loader.fixer.OntologyIntegrityFixerService;
+import org.ncbo.stanford.service.metrics.MetricsService;
 import org.ncbo.stanford.service.ontology.AbstractOntologyService;
 import org.ncbo.stanford.service.search.IndexSearchService;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 
+ *
  * @author Michael Dorf
- * 
+ *
  *         Class responsible for checking fixing the ontology integrity. Fixes
  *         dependent artifacts such as search index or metrics that may be out
  *         of sync with the latest ontology version
@@ -32,6 +35,7 @@ public class OntologyIntegrityFixerServiceImpl extends AbstractOntologyService
 
 	private OntologyMetadataManager ontologyMetadataManager;
 	private IndexSearchService indexSearchService;
+	private MetricsService metricsService;
 
 	/**
 	 * Goes through all latest versions of parsed ontologies and fixes dependent
@@ -48,11 +52,21 @@ public class OntologyIntegrityFixerServiceImpl extends AbstractOntologyService
 			if (log.isInfoEnabled()) {
 				log.info("Running search index fixer...");
 			}
+
 			fixSearchIndex(ontologies);
-			
+
 			if (log.isInfoEnabled()) {
 				log.info("Search index fixer run completed...");
 			}
+
+			// fix metrics
+			if (log.isInfoEnabled())
+				log.info("Running metrics fixer...");
+
+			fixMetrics(ontologies);
+
+			if (log.isInfoEnabled())
+				log.info("Metrics fixer run completed...");
 		} catch (Exception e) {
 			log.error(e);
 			e.printStackTrace();
@@ -62,7 +76,7 @@ public class OntologyIntegrityFixerServiceImpl extends AbstractOntologyService
 	/**
 	 * Rebuilds index for "problem" ontologies - those that are either not in
 	 * the index or have a wrong version in the index
-	 * 
+	 *
 	 * @param ontologies
 	 */
 	public void fixSearchIndex(List<OntologyBean> ontologies) {
@@ -110,6 +124,51 @@ public class OntologyIntegrityFixerServiceImpl extends AbstractOntologyService
 		}
 	}
 
+	private void fixMetrics(List<OntologyBean> ontologies) {
+		Map<Integer, OntologyBean> problemOntologies = new HashMap<Integer, OntologyBean>(
+				0);
+
+		for (OntologyBean ontology : ontologies) {
+			try {
+				OntologyMetricsBean metricsBean = metricsService
+						.getOntologyMetrics(ontology);
+
+				if (metricsBean == null || (metricsBean != null && metricsBean.getMaximumDepth() == null) ) {
+					problemOntologies.put(ontology.getId(), ontology);
+				}
+			} catch (MetadataException e) {
+				problemOntologies.put(ontology.getId(), ontology);
+			} catch (Exception e) {
+				addErrorOntology(errorOntologies, ontology.getId().toString(),
+						ontology, e.getMessage());
+				log.error(e);
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			if (log.isInfoEnabled()) {
+				StringBuffer sb = new StringBuffer();
+				sb
+						.append("The following ontologies were found to be missing metrics and are being re-calculated:\n\n");
+
+				for (Map.Entry<Integer, OntologyBean> entry : problemOntologies
+						.entrySet()) {
+					sb.append(entry.getValue());
+					sb.append("\n\n");
+				}
+				log.info(sb.toString());
+			}
+
+			metricsService.extractOntologyMetrics(new ArrayList<Integer>(
+					problemOntologies.keySet()));
+		} catch (Exception e) {
+			log.error("Unable to fix search index due to the following error: "
+					+ e);
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * @param ontologyMetadataManager
 	 *            the ontologyMetadataManager to set
@@ -125,5 +184,13 @@ public class OntologyIntegrityFixerServiceImpl extends AbstractOntologyService
 	 */
 	public void setIndexSearchService(IndexSearchService indexSearchService) {
 		this.indexSearchService = indexSearchService;
+	}
+
+	/**
+	 * @param metricsService
+	 *            the metricsService to set
+	 */
+	public void setMetricsService(MetricsService metricsService) {
+		this.metricsService = metricsService;
 	}
 }
--- src/java/org/ncbo/stanford/service/metrics/impl/MetricsServiceImpl.java
+++ src/java/org/ncbo/stanford/service/metrics/impl/MetricsServiceImpl.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.ncbo.stanford.service.metrics.impl;
 
