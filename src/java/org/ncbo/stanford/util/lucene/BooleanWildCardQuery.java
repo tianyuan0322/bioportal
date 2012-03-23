61fc15a387fe390a4f8bfe8327debf18736bfff0
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2012-03-23 20:44:52 +0000

    implemented tracker [#3701] Include boolean functionality in Search Web service and UI

--- src/java/org/ncbo/stanford/util/lucene/BooleanWildCardQuery.java
+++ src/java/org/ncbo/stanford/util/lucene/BooleanWildCardQuery.java
@@ -1,8 +1,6 @@
 package org.ncbo.stanford.util.lucene;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -14,19 +12,15 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.MultiPhraseQuery;
 import org.apache.lucene.search.MultiTermQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.NIOFSDirectory;
 import org.apache.lucene.util.Version;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
@@ -58,60 +52,36 @@ public class BooleanWildCardQuery extends BooleanQuery {
 		this.reader = reader;
 		this.analyzer = analyzer;
 	}
-	
-	public static void main(String[] args) {
-		String expr;
-//		expr = "cas (blo+od del phi) -clo -(cat mo) 		         (artery       vibe)   (nil) ser 	gul";
-//		expr = "(heart lung) vess";	
-//		expr = "(blood nose) -dis";
-		expr = "hear attac -Anxiety";
-		
-		Version luceneVersion = Version.LUCENE_24;
-		Analyzer analyzer = new StandardAnalyzer(luceneVersion, Collections.emptySet());
-		String indexPath = "/apps/bmir.apps/bioportal_resources/searchindex";
 
-		try {
-			FSDirectory indexDir = NIOFSDirectory.open(new File(indexPath));
-			IndexSearcher searcher = new IndexSearcher(indexDir, true);
-			
-			BooleanWildCardQuery q = new BooleanWildCardQuery(luceneVersion, searcher.getIndexReader(), analyzer);
-			q.parseBooleanWildCardQuery("contents", expr);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	
+	/*
+	 * public static void main(String[] args) { String expr; expr =
+	 * "cas (blo+od del phi) -clo -(cat mo) 		         (artery       vibe)   (nil) ser 	gul"
+	 * ; expr = "(heart lung) vess"; expr = "(blood nose) -dis"; expr =
+	 * "hear attac -Anxiety";
+	 * 
+	 * Version luceneVersion = Version.LUCENE_24; Analyzer analyzer = new
+	 * StandardAnalyzer(luceneVersion, Collections.emptySet()); String indexPath
+	 * = "/apps/bmir.apps/bioportal_resources/searchindex";
+	 * 
+	 * try { FSDirectory indexDir = NIOFSDirectory.open(new File(indexPath));
+	 * IndexSearcher searcher = new IndexSearcher(indexDir, true);
+	 * 
+	 * BooleanWildCardQuery q = new BooleanWildCardQuery(luceneVersion,
+	 * searcher.getIndexReader(), analyzer);
+	 * q.parseBooleanWildCardQuery("contents", expr); } catch (Exception e) {
+	 * e.printStackTrace(); } }
+	 */	
 	public void parseBooleanWildCardQuery(String field, String expr) throws Exception {
-
-		System.out.println("Orig:" + expr);
-		
 		expr = parseExpression(expr);
-
-		System.out.println("Pars:" + expr);
-		
 		
 		if (expr.length() > 0) {
-	
-			
-			
-			
-			
-		
-			
-			
-			
 			StringTokenizer st = new StringTokenizer(expr);
 
 			while (st.hasMoreTokens()) {
 				MultiPhraseQuery mpq = new MultiPhraseQuery();
-				
-				
 				String termSetStr = st.nextToken();
 				String[] termSet = termSetStr.split(IN_PAREN_DELIMITER);
-				List<Term> clauseTerms = new ArrayList<Term>(0);
-				
-				
+				List<Term> clauseTerms = new ArrayList<Term>(0);			
 				
 				if (termSet[0].matches("^[-+].+")) {
 					termSet[0] = termSet[0].substring(1);
@@ -129,7 +99,7 @@ public class BooleanWildCardQuery extends BooleanQuery {
 					clauseTerms.addAll(Arrays.asList(expandedTerms));
 				}
 				
-				Term[] allTerms = clauseTerms.toArray(new Term[0]);				
+				Term[] allTerms = clauseTerms.toArray(new Term[clauseTerms.size()]);				
 				mpq.add(allTerms);
 				
 				switch (termSetStr.charAt(0)) {
@@ -137,19 +107,16 @@ public class BooleanWildCardQuery extends BooleanQuery {
 					add(mpq, BooleanClause.Occur.MUST_NOT);
 					break;
 				default:
-//					add(mpq, BooleanClause.Occur.SHOULD);
 					add(mpq, BooleanClause.Occur.MUST);
 				}
 			}
-		}
-		
-		
-		System.out.println(this.toString().trim());
-		
+		}		
 	}
 
 	private String parseExpression(String expr) {
 		expr = expr.replaceAll(SPACES_PATTERN, " ").toLowerCase();
+		expr = expr.replace("\"", "");
+
 		// words in parens
 		Pattern inParens = Pattern.compile("\\([^\\(\\)]+\\)");
 		Matcher matcherParen = inParens.matcher(expr);
@@ -166,7 +133,6 @@ public class BooleanWildCardQuery extends BooleanQuery {
 	
 	private Term[] expand(String field, String prefix) throws Exception {
 		QueryParser parser = new QueryParser(luceneVersion, field, analyzer);
-
 		Query queryExact = parser.parse(prefix + WILDCARD_CHAR);
 
 		if (queryExact instanceof org.apache.lucene.search.PrefixQuery) {
