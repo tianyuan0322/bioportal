9e16c7d2577e4c42c06ce800b391b8e3c70fc3ba
mdorf <mdorf@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2012-04-04 18:09:37 +0000

    fixed tracker [#3902] Data truncation: Data too long for column 'request_url'

--- db/sql/bioportal_db_changes_1047.sql
+++ db/sql/bioportal_db_changes_1047.sql
@@ -1,3 +1,6 @@
 USE `bioportal`;
 
 ALTER TABLE `ncbo_usage_log` MODIFY COLUMN `request_url` VARCHAR(2048) CHARACTER SET utf8 COLLATE utf8_bin;
+
+ALTER TABLE `ncbo_usage_log` MODIFY COLUMN `search_query` VARCHAR(2048) CHARACTER SET utf8 COLLATE utf8_bin;
+
