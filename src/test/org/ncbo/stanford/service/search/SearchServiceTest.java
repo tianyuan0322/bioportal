cdf5e3b929c762745ba8af6404f8b57dee656c01
paul <paul@41167b3f-aa3d-0410-aeb3-e59713d2cf71>
2012-04-11 19:49:08 +0000

    Fix search tests for use with new recordTypes parameter

--- src/test/org/ncbo/stanford/service/search/SearchServiceTest.java
+++ src/test/org/ncbo/stanford/service/search/SearchServiceTest.java
@@ -88,7 +88,7 @@ public class SearchServiceTest extends AbstractBioPortalTest {
 			Integer maxNumHits = 250;
 
 			Query q = queryService.generateLuceneSearchQuery(ontologyIds,
-					objectTypes, expr, includeProperties, isExactMatch);
+					objectTypes, null, expr, includeProperties, isExactMatch);
 			System.out.println("q: " + q);
 
 			long start = System.currentTimeMillis();
@@ -116,7 +116,7 @@ public class SearchServiceTest extends AbstractBioPortalTest {
 
 		try {
 			Integer maxNumHits = 50;
-			Query query = queryService.generateLuceneSearchQuery(null, null,
+			Query query = queryService.generateLuceneSearchQuery(null, null, null,
 					"cell", true, false);
 			Page<SearchBean> results = queryService.executeQuery(query,
 					maxNumHits, null, null, false);
@@ -139,7 +139,7 @@ public class SearchServiceTest extends AbstractBioPortalTest {
 
 		try {
 			Integer maxNumHits = 98;
-			Query query = queryService.generateLuceneSearchQuery(null, null,
+			Query query = queryService.generateLuceneSearchQuery(null, null, null,
 					"lead", true, false);
 			Page<SearchBean> results = queryService.executeQuery(query,
 					maxNumHits, null, null, false);
@@ -166,7 +166,7 @@ public class SearchServiceTest extends AbstractBioPortalTest {
 				.println("SearchServiceTest: searchAllOntologies().......................BEGIN");
 
 		Integer maxNumHits = 50;
-		Query query = queryService.generateLuceneSearchQuery(null, null,
+		Query query = queryService.generateLuceneSearchQuery(null, null, null,
 				"blood", true, false);
 		Page<SearchBean> results = queryService.executeQuery(query, 3, 6,
 				maxNumHits, null, null, false);
